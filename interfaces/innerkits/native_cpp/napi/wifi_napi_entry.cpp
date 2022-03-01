cf3413660983c40bbc454abb8b9054432da41f8d
zhangfeng <hw.zhangfeng@huawei.com>
2022-03-01 14:45:08 +0000

    update p2p function name
    Signed-off-by: zhangfeng <hw.zhangfeng@huawei.com>

--- interfaces/innerkits/native_cpp/napi/wifi_napi_entry.cpp
+++ interfaces/innerkits/native_cpp/napi/wifi_napi_entry.cpp
@@ -62,7 +62,7 @@ static napi_value Init(napi_env env, napi_value exports) {
         DECLARE_NAPI_FUNCTION("getStations", GetStations),
         DECLARE_NAPI_FUNCTION("getP2pLinkedInfo", GetP2pLinkedInfo),
         DECLARE_NAPI_FUNCTION("getCurrentGroup", GetCurrentGroup),
-        DECLARE_NAPI_FUNCTION("getP2pDevices", GetP2pDevices),
+        DECLARE_NAPI_FUNCTION("getP2pPeerDevices", GetP2pDevices),
         DECLARE_NAPI_FUNCTION("createGroup", CreateGroup),
         DECLARE_NAPI_FUNCTION("removeGroup", RemoveGroup),
         DECLARE_NAPI_FUNCTION("p2pConnect", P2pConnect),

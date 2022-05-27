f5d5f72118844c28182f0f85eec1472e008f3b7c
y00316381 <yanxiaotao@huawei.com>
2022-05-27 21:07:02 +0800

    bugfix from master
    Signed-off-by: y00316381 <yanxiaotao@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
@@ -1642,7 +1642,7 @@ bool ScanService::GetHiddenNetworkSsidList(std::vector<std::string> &hiddenNetwo
         }
     }
 
-    WIFI_LOGI("Find %{public}u hidden NetworkSsid.\n", hiddenNetworkSsid.size());
+    WIFI_LOGI("Find %{public}d hidden NetworkSsid.\n", (int)hiddenNetworkSsid.size());
     return true;
 }
 

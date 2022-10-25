a25c508a2aa51157c2c85fd0c4c86a48afccd727
yanxiaotao@huawei.com <yanxiaotao@huawei.com>
2022-10-25 10:21:42 +0800

    add error code for wifi 1025
    Signed-off-by: yanxiaotao@huawei.com <yanxiaotao@huawei.com>

--- wifi/frameworks/js/napi/src/wifi_napi_errcode.cpp
+++ wifi/frameworks/js/napi/src/wifi_napi_errcode.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-#include "wifi_manager_napi_errcode.h"
+#include "wifi_napi_errcode.h"
 #include <map>
 #include "wifi_logger.h"
 #include "wifi_errcode.h"

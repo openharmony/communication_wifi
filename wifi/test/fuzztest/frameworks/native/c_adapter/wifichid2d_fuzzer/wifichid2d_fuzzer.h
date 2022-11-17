59eb25e907d54661150a3c87bcaaf1d1f48e2ac1
yiyajun <yiyajun@huawei.com>
2022-11-17 20:54:14 +0800

    add wifi_c_hid2d fuzz test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

---
+++ wifi/test/fuzztest/frameworks/native/c_adapter/wifichid2d_fuzzer/wifichid2d_fuzzer.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef WIFI_C_HID2D__H
+#define WIFI_C_HID2D__H
+
+#define FUZZ_PROJECT_NAME "wifichid2d_fuzzer"
+#define MACLEN 6
+#define IPLEN 4
+#define VALUE 10
+#define NAMELEN 32
+#define DATA_MAX_BYTES
+
+void Hid2dRequestGcIpTest(const uint8_t* data, size_t size);
+void Hid2dSharedlinkIncreaseTest(void);
+void Hid2dSharedlinkDecreaseTest(void);
+void Hid2dCreateGroupTest(const uint8_t* data, size_t size);
+void Hid2dRemoveGcGroupTest(const uint8_t* data, size_t size);
+void Hid2dConnectTest(const uint8_t* data, size_t size);
+void Hid2dConfigIPAddrTest(const uint8_t* data, size_t size);
+void Hid2dReleaseIPAddrTest(const uint8_t* data, size_t size);
+void Hid2dGetRecommendChannelTest(const uint8_t* data, size_t size);
+void Hid2dGetChannelListFor5GTest(const uint8_t* data, size_t size);
+void Hid2dGetSelfWifiCfgInfoTest(const uint8_t* data, size_t size);
+void Hid2dSetPeerWifiCfgInfoTest(const uint8_t* data, size_t size);
+void Hid2dIsWideBandwidthSupportedTest(void);
+void Hid2dSetUpperSceneTest(const uint8_t* data, size_t size);
+
+#endif

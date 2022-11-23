c38d9229badb4f877828d5d750a4032f9e4d9f83
sunjinqiang <17319862417@163.com>
2022-11-23 11:30:04 +0000

    增加TDD用例
    Signed-off-by: sunjinqiang <17319862417@163.com>

---
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/wifi_hid2d_service_utils_test.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "wifi_hid2d_service_utils.h"
+#include "mock_wifi_hid2d_service_utils.h"
+
+using ::testing::Return;
+using ::testing::ext::TestSize;
+
+namespace OHOS {
+namespace Wifi {
+class WifiHid2dServiceUtilsTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {}
+    static void TearDownTestCase()
+    {}
+    virtual void SetUp()
+    {
+        pIpPool.reset(new IpPool);
+        pSharedLinkManager.reset(new SharedLinkManager);
+    }
+    virtual void TearDown()
+    {
+        pIpPool.reset();
+        pSharedLinkManager.reset();
+
+    }
+
+public:
+    std::unique_ptr<IpPool> pIpPool;
+    std::unique_ptr<SharedLinkManager> pSharedLinkManager;
+    WifiP2pGroupManager groupManager;
+    WifiP2pDeviceManager deviceManager;
+};
+
+HWTEST_F(WifiHid2dServiceUtilsTest, InitIpPool, TestSize.Level1)
+{
+    std::string& serverIp;
+    EXPECT_EQ(pIpPool->InitIpPool(serverIp), true);
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, GetIp, TestSize.Level1)
+{
+    std::string& gcMac;
+    pIpPool->GetIp(gcMac);
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, ReleaseIp, TestSize.Level1)
+{
+    std::string& gcMac;
+    pIpPool->ReleaseIp(gcMac);
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, ReleaseIpPool, TestSize.Level1)
+{
+    std::string& gcMac;
+    pIpPool->ReleaseIpPool();
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, IncreaseSharedLink, TestSize.Level1)
+{
+    pSharedLinkManager->IncreaseSharedLink();
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, DecreaseSharedLink, TestSize.Level1)
+{
+    pSharedLinkManager->DecreaseSharedLink();
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, SetSharedLinkCount, TestSize.Level1)
+{
+    int count = 1;
+    pSharedLinkManager->SetSharedLinkCount(count);
+}
+HWTEST_F(WifiHid2dServiceUtilsTest, GetSharedLinkCount, TestSize.Level1)
+{
+    pSharedLinkManager->GetSharedLinkCount();
+}
+}
+}
+
+

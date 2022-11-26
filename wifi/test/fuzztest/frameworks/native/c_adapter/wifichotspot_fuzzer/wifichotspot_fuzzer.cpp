666a380fa7f74dc8145eaf22d04275b28713f63d
yiyajun <yiyajun@huawei.com>
2022-11-26 08:43:23 +0000

    update wifi/test/fuzztest/frameworks/native/c_adapter/wifichotspot_fuzzer/wifichotspot_fuzzer.cpp.
    Signed-off-by: yiyajun <yiyajun@huawei.com>

--- wifi/test/fuzztest/frameworks/native/c_adapter/wifichotspot_fuzzer/wifichotspot_fuzzer.cpp
+++ wifi/test/fuzztest/frameworks/native/c_adapter/wifichotspot_fuzzer/wifichotspot_fuzzer.cpp
@@ -23,7 +23,6 @@
 static void SetHotspotConfigTest(const uint8_t* data, size_t size)
 {
     HotspotConfig config;
-    int index = 0;
 
     if (size >= sizeof(HotspotConfig)) {
         if (memcpy_s(config.ssid, WIFI_MAX_SSID_LEN, data, WIFI_MAX_SSID_LEN - 1) != EOK) {
@@ -33,6 +32,7 @@ static void SetHotspotConfigTest(const uint8_t* data, size_t size)
         if (memcpy_s(config.preSharedKey, WIFI_MAX_KEY_LEN, data, WIFI_MAX_KEY_LEN - 1) != EOK) {
             return;
         }
+        int index = 0;
         config.securityType = static_cast<int>(data[index++]);
         config.band = static_cast<int>(data[index++]);
         config.channelNum = static_cast<int>(data[index++]);
@@ -44,7 +44,6 @@ static void SetHotspotConfigTest(const uint8_t* data, size_t size)
 static void GetHotspotConfigTest(const uint8_t* data, size_t size)
 {
     HotspotConfig result;
-    int index = 0;
 
     if (size >= sizeof(HotspotConfig)) {
         if (memcpy_s(result.ssid, WIFI_MAX_SSID_LEN, data, WIFI_MAX_SSID_LEN - 1) != EOK) {
@@ -54,6 +53,7 @@ static void GetHotspotConfigTest(const uint8_t* data, size_t size)
         if (memcpy_s(result.preSharedKey, WIFI_MAX_KEY_LEN, data, WIFI_MAX_KEY_LEN - 1) != EOK) {
             return;
         }
+        int index = 0;
         result.securityType = static_cast<int>(data[index++]);
         result.band = static_cast<int>(data[index++]);
         result.channelNum = static_cast<int>(data[index++]);
@@ -66,12 +66,12 @@ static void GetStationListTest(const uint8_t* data, size_t size)
 {
     StationInfo result;
     unsigned int mSize = 0;
-    int index = 0;
 
     if (size >= sizeof(StationInfo)) {
         if (memcpy_s(result.macAddress, WIFI_MAC_LEN, data, WIFI_MAC_LEN) != EOK) {
             return;
         }
+        int index = 0;
         result.ipAddress = OHOS::Wifi::U32_AT(data);
         result.disconnectedReason = static_cast<unsigned short>(data[index++]);
         mSize = static_cast<unsigned int>(data[index++]);
@@ -81,10 +81,10 @@ static void GetStationListTest(const uint8_t* data, size_t size)
 
 static void DisassociateStaTest(const uint8_t* data, size_t size)
 {
-    int index = 0;
     unsigned char mac = 0;
     int macLen = 0;
     if (size >= TWO) {
+        int index = 0;
         mac = data[index++];
         macLen = static_cast<unsigned int>(data[index++]);
     }

a53d4c700c0da6829f1765888da6e47fdd4bd5cb
yiyajun <yiyajun@huawei.com>
2022-11-28 15:14:33 +0800

    add fuzz test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

--- wifi/test/fuzztest/frameworks/native/c_adapter/wificdevice_fuzzer/wificdevice_fuzzer.cpp
+++ wifi/test/fuzztest/frameworks/native/c_adapter/wificdevice_fuzzer/wificdevice_fuzzer.cpp
@@ -120,6 +120,151 @@ static void GetSignalLevelTest(const uint8_t* data, size_t size)
     }
     GetSignalLevel(rssi, band);
 }
+//
+static void GetScanInfoListTest(const uint8_t* data, size_t size)
+{
+    WifiScanInfo result;
+    int mSize;
+    if (size >= sizeof(WifiScanInfo)) {
+        if (memcpy_s(result.ssid, WIFI_MAX_SSID_LEN, data, WIFI_MAX_SSID_LEN - 1) != EOK) {
+            return;
+        }
+
+        if (memcpy_s(result.bssid, WIFI_MAC_LEN, data + WIFI_MAX_SSID_LEN, WIFI_MAC_LEN) != EOK) {
+            return;
+        }
+        int index = 0;
+        result.securityType = static_cast<int>(data[index++]);
+        result.rssi = static_cast<int>(data[index++]);
+        result.band = static_cast<int>(data[index++]);
+        result.frequency = static_cast<int>(data[index++]);
+        result.channelWidth = static_cast<WifiChannelWidth>(static_cast<int>(data[index++]) % WIDTH_INVALID);
+        result.centerFrequency0 = static_cast<int>(data[index++]);
+        result.centerFrequency1 = static_cast<int>(data[index++]);
+        result.timestamp = static_cast<int64_t>(OHOS::Wifi::U32_AT(data));
+        mSize = static_cast<unsigned int>(data[0]);
+    }
+    (void)GetScanInfoList(&result, &size);
+}
+
+static void GetDeviceConfigsTest(const uint8_t* data, size_t size)
+{
+    WifiDeviceConfig result;
+    unsigned int mSize;
+    if (size >= sizeof(WifiDeviceConfig)) {
+        if (memcpy_s(result.ssid, WIFI_MAX_SSID_LEN, data, WIFI_MAX_SSID_LEN - 1) != EOK) {
+            return;
+        }
+
+        if (memcpy_s(result.bssid, WIFI_MAC_LEN, data + WIFI_MAX_SSID_LEN, WIFI_MAC_LEN) != EOK) {
+            return;
+        }
+
+        if (memcpy_s(result.preSharedKey, WIFI_MAX_KEY_LEN, data + WIFI_MAX_SSID_LEN, WIFI_MAX_KEY_LEN - 1) != EOK) {
+            return;
+        }
+        int index = 0;
+        result.securityType = static_cast<int>(data[index++]);
+        result.netId = static_cast<int>(data[index++]);
+        result.freq = static_cast<unsigned int>(data[index++]);
+        result.wapiPskType = static_cast<int>(data[index++]);
+        result.ipType = static_cast<IpType>(static_cast<int>(data[index++]) % UNKNOWN);
+        result.staticIp.ipAddress = static_cast<unsigned int>(data[index++]);
+        result.staticIp.gateway = static_cast<unsigned int>(data[index++]);
+        result.staticIp.netmask = static_cast<unsigned int>(data[index++]);
+        result.isHiddenSsid = static_cast<int>(data[index++]);
+        mSize = static_cast<unsigned int>(data[0]);
+    }
+    (void)GetDeviceConfigs(&result, &size);
+}
+
+static void ConnectToDeviceTest(const uint8_t* data, size_t size)
+{
+    WifiDeviceConfig config;
+    if (size >= sizeof(WifiDeviceConfig)) {
+        if (memcpy_s(config.ssid, WIFI_MAX_SSID_LEN, data, WIFI_MAX_SSID_LEN - 1) != EOK) {
+            return;
+        }
+
+        if (memcpy_s(config.bssid, WIFI_MAC_LEN, data + WIFI_MAX_SSID_LEN, WIFI_MAC_LEN) != EOK) {
+            return;
+        }
+
+        if (memcpy_s(config.preSharedKey, WIFI_MAX_KEY_LEN, data + WIFI_MAX_SSID_LEN, WIFI_MAX_KEY_LEN - 1) != EOK) {
+            return;
+        }
+        int index = 0;
+        config.securityType = static_cast<int>(data[index++]);
+        config.netId = static_cast<int>(data[index++]);
+        config.freq = static_cast<unsigned int>(data[index++]);
+        config.wapiPskType = static_cast<int>(data[index++]);
+        config.ipType = static_cast<IpType>(static_cast<int>(data[index++]) % UNKNOWN);
+        config.staticIp.ipAddress = static_cast<unsigned int>(data[index++]);
+        config.staticIp.gateway = static_cast<unsigned int>(data[index++]);
+        config.staticIp.netmask = static_cast<unsigned int>(data[index++]);
+        config.isHiddenSsid = static_cast<int>(data[index++]);
+    }
+    (void)ConnectToDevice(&config);
+}
+
+static void GetLinkedInfoTest(const uint8_t* data, size_t size)
+{
+    WifiLinkedInfo result;
+    if(size >= sizeof(WifiLinkedInfo)) {
+        if (memcpy_s(result.ssid, WIFI_MAX_SSID_LEN, data, WIFI_MAX_SSID_LEN - 1) != EOK) {
+            return;
+        }
+
+        if (memcpy_s(result.bssid, WIFI_MAC_LEN, data + WIFI_MAX_SSID_LEN, WIFI_MAC_LEN) != EOK) {
+            return;
+        }
+        int index = 0;
+        result.rssi = static_cast<int>(data[index++]);
+        result.band = static_cast<int>(data[index++]);
+        result.rssi = static_cast<int>(data[index++]);
+        result.frequency = static_cast<int>(data[index++]);
+        result.connState = static_cast<WifiConnState>(static_cast<int>(data[index++]) % (WIFI_CONNECTED + 1));
+        result.disconnectedReason = static_cast<unsigned short>(data[index++]);
+        result.ipAddress = static_cast<unsigned int>(data[index++]);
+
+    }
+    (void)GetLinkedInfo(&result);
+}
+
+static void GetDeviceMacAddressTest(const uint8_t* data, size_t size)
+{
+    unsigned char result;
+    if (size > 0) {
+        result = static_cast<unsigned char>(data[0]);
+    }
+    (void)GetDeviceMacAddress(&result);
+}
+
+static void GetIpInfoTest(const uint8_t* data, size_t size)
+{
+    IpInfo info;
+    if (size >= sizeof(IpInfo)) {
+        int index = 0;
+        info.ipAddress = static_cast<unsigned int>(data[index++]);
+        info.netMask = static_cast<unsigned int>(data[index++]);
+        info.netGate = static_cast<unsigned int>(data[index++]);
+        info.dns1 = static_cast<unsigned int>(data[index++]);
+        info.dns2 = static_cast<unsigned int>(data[index++]);
+        info.serverAddress = static_cast<unsigned int>(data[index++]);
+        info.leaseDuration = static_cast<int>(data[index++]);
+    }
+    (void)GetIpInfo(&info);
+}
+
+static void SetLowLatencyModeTest(const uint8_t* data, size_t size)
+{
+    int enabled;
+    if (size > 0) {
+        enabled = static_cast<int>(data[0]);
+    }
+    (void)SetLowLatencyMode(int enabled);
+}
+
 namespace OHOS {
 namespace Wifi {
     bool WifiCDeviceFuzzerTest(const uint8_t* data, size_t size)
@@ -135,6 +280,14 @@ namespace Wifi {
         AddDeviceConfigTest(data, size);
         AdvanceScanTest(data, size);
         GetSignalLevelTest(data, size);
+        (void)IsWifiActive();
+        GetScanInfoListTest(data, size);
+        GetDeviceConfigsTest(data, size);
+        ConnectToDeviceTest(data, size);
+        GetLinkedInfoTest(data, size);
+        GetDeviceMacAddressTest(data, size);
+        GetIpInfoTest(data, size);
+        SetLowLatencyModeTest(data, size);
         return true;
     }
 }  // namespace Wifi

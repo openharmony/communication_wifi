4b28ebd352c313b94c971a7d408816b310222122
yanxiaotao@huawei.com <yanxiaotao@huawei.com>
2022-12-07 23:15:18 +0800

    get supported freqs when sta/scan is inited @2022-12-07
    Signed-off-by: yanxiaotao@huawei.com <yanxiaotao@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
@@ -103,13 +103,16 @@ bool ScanService::InitScanService(const IScanSerivceCallbacks &scanSerivceCallba
         WIFI_LOGE("GetSupportFrequencies failed.\n");
     }
 
-    std::vector<int32_t> supp2Gfreqs(freqs2G.begin(), freqs2G.end());
-    std::vector<int32_t> supp5Gfreqs(freqs5G.begin(), freqs5G.end());
     ChannelsTable ChanTbs;
-    ChanTbs[BandType::BAND_2GHZ] = supp2Gfreqs;
-    ChanTbs[BandType::BAND_5GHZ] = supp5Gfreqs;
-    if (WifiSettings::GetInstance().SetValidChannels(ChanTbs)) {
-        WIFI_LOGE("%{public}s, fail to SetValidChannels", __func__);
+    (void)WifiSettings::GetInstance().GetValidChannels(ChanTbs);
+    if (ChanTbs[BandType::BAND_2GHZ].size() == 0) {
+        std::vector<int32_t> supp2Gfreqs(freqs2G.begin(), freqs2G.end());
+        std::vector<int32_t> supp5Gfreqs(freqs5G.begin(), freqs5G.end());
+        ChanTbs[BandType::BAND_2GHZ] = supp2Gfreqs;
+        ChanTbs[BandType::BAND_5GHZ] = supp5Gfreqs;
+        if (WifiSettings::GetInstance().SetValidChannels(ChanTbs)) {
+            WIFI_LOGE("%{public}s, fail to SetValidChannels", __func__);
+        }
     }
     pScanMonitor->SetScanStateMachine(pScanStateMachine);
     pScanStateMachine->SendMessage(static_cast<int>(CMD_SCAN_PREPARE));

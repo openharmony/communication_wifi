bf2285635b3a08f3ba69da30f38b6eaa2d811bc4
fengye <fengye10@huawei.com>
2022-12-09 10:11:42 +0000

    fix bug
    Signed-off-by: fengye <fengye10@huawei.com>

--- wifi/frameworks/js/napi/src/wifi_napi_entry.cpp
+++ wifi/frameworks/js/napi/src/wifi_napi_entry.cpp
@@ -22,75 +22,6 @@
 namespace OHOS {
 namespace Wifi {
 #ifndef ENABLE_NAPI_COMPATIBLE
-/*
- * Module initialization function
- */
-static napi_value Init(napi_env env, napi_value exports) {
-    napi_value PropertyValueInit(env, exports);
-    napi_property_descriptor desc[] = {
-        DECLARE_NAPI_FUNCTION("enableWifi", EnableWifi),
-        DECLARE_NAPI_FUNCTION("disableWifi", DisableWifi),
-        DECLARE_NAPI_FUNCTION("isWifiActive", IsWifiActive),
-        DECLARE_NAPI_FUNCTION("scan", Scan),
-        DECLARE_NAPI_FUNCTION("getScanInfos", GetScanInfos),
-        DECLARE_NAPI_FUNCTION("getScanInfosSync", GetScanResults),
-        DECLARE_NAPI_FUNCTION("getScanResults", GetScanInfos),
-        DECLARE_NAPI_FUNCTION("getScanResultsSync", GetScanResults),
-        DECLARE_NAPI_FUNCTION("addDeviceConfig", AddDeviceConfig),
-        DECLARE_NAPI_FUNCTION("addUntrustedConfig", AddUntrustedConfig),
-        DECLARE_NAPI_FUNCTION("removeUntrustedConfig", RemoveUntrustedConfig),
-        DECLARE_NAPI_FUNCTION("addCandidateConfig", AddCandidateConfig),
-        DECLARE_NAPI_FUNCTION("removeCandidateConfig", RemoveCandidateConfig),
-        DECLARE_NAPI_FUNCTION("connectToCandidateConfig", ConnectToCandidateConfig),
-        DECLARE_NAPI_FUNCTION("getCandidateConfigs", GetCandidateConfigs),
-        DECLARE_NAPI_FUNCTION("connectToNetwork", ConnectToNetwork),
-        DECLARE_NAPI_FUNCTION("connectToDevice", ConnectToDevice),
-        DECLARE_NAPI_FUNCTION("isConnected", IsConnected),
-        DECLARE_NAPI_FUNCTION("disconnect", Disconnect),
-        DECLARE_NAPI_FUNCTION("getSignalLevel", GetSignalLevel),
-        DECLARE_NAPI_FUNCTION("reconnect", ReConnect),
-        DECLARE_NAPI_FUNCTION("reassociate", ReAssociate),
-        DECLARE_NAPI_FUNCTION("getIpInfo", GetIpInfo),
-        DECLARE_NAPI_FUNCTION("getLinkedInfo", GetLinkedInfo),
-        DECLARE_NAPI_FUNCTION("removeDevice", RemoveDevice),
-        DECLARE_NAPI_FUNCTION("removeAllNetwork", RemoveAllNetwork),
-        DECLARE_NAPI_FUNCTION("disableNetwork", DisableNetwork),
-        DECLARE_NAPI_FUNCTION("getCountryCode", GetCountryCode),
-        DECLARE_NAPI_FUNCTION("getDeviceConfigs", GetDeviceConfigs),
-        DECLARE_NAPI_FUNCTION("updateNetwork", UpdateNetwork),
-        DECLARE_NAPI_FUNCTION("getSupportedFeatures", GetSupportedFeatures),
-        DECLARE_NAPI_FUNCTION("isFeatureSupported", IsFeatureSupported),
-        DECLARE_NAPI_FUNCTION("getDeviceMacAddress", GetDeviceMacAddress),
-        DECLARE_NAPI_FUNCTION("isHotspotActive", IsHotspotActive),
-        DECLARE_NAPI_FUNCTION("isHotspotDualBandSupported", IsHotspotDualBandSupported),
-        DECLARE_NAPI_FUNCTION("enableHotspot", EnableHotspot),
-        DECLARE_NAPI_FUNCTION("disableHotspot", DisableHotspot),
-        DECLARE_NAPI_FUNCTION("setHotspotConfig", SetHotspotConfig),
-        DECLARE_NAPI_FUNCTION("getHotspotConfig", GetHotspotConfig),
-        DECLARE_NAPI_FUNCTION("getStations", GetStations),
-        DECLARE_NAPI_FUNCTION("addBlockList", AddBlockList),
-        DECLARE_NAPI_FUNCTION("delBlockList", DelBlockList),
-        DECLARE_NAPI_FUNCTION("getP2pLinkedInfo", GetP2pLinkedInfo),
-        DECLARE_NAPI_FUNCTION("getCurrentGroup", GetCurrentGroup),
-        DECLARE_NAPI_FUNCTION("getP2pPeerDevices", GetP2pDevices),
-        DECLARE_NAPI_FUNCTION("getP2pLocalDevice", GetP2pLocalDevice),
-        DECLARE_NAPI_FUNCTION("createGroup", CreateGroup),
-        DECLARE_NAPI_FUNCTION("removeGroup", RemoveGroup),
-        DECLARE_NAPI_FUNCTION("p2pConnect", P2pConnect),
-        DECLARE_NAPI_FUNCTION("p2pCancelConnect", P2pCancelConnect),
-        DECLARE_NAPI_FUNCTION("p2pDisconnect", P2pCancelConnect),
-        DECLARE_NAPI_FUNCTION("startDiscoverDevices", StartDiscoverDevices),
-        DECLARE_NAPI_FUNCTION("stopDiscoverDevices", StopDiscoverDevices),
-        DECLARE_NAPI_FUNCTION("deletePersistentGroup", DeletePersistentGroup),
-        DECLARE_NAPI_FUNCTION("getP2pGroups", GetP2pGroups),
-        DECLARE_NAPI_FUNCTION("setDeviceName", SetDeviceName),
-        DECLARE_NAPI_FUNCTION("on", On),
-        DECLARE_NAPI_FUNCTION("off", Off),
-    };
-
-    NAPI_CALL(env, napi_define_properties(env, exports, sizeof(desc) / sizeof(napi_property_descriptor), desc));
-    return exports;
-}
 
 static napi_value SecurityTypeInit(napi_env env)
 {
@@ -256,6 +187,76 @@ static napi_value PropertyValueInit(napi_env env, napi_value exports)
         DECLARE_NAPI_PROPERTY("GroupOwnerBand", groupOwnerBandObj)
     };
     napi_define_properties(env, exports, sizeof(exportFuncs) / sizeof(*exportFuncs), exportFuncs);
+    return exports;
+}
+
+/*
+ * Module initialization function
+ */
+static napi_value Init(napi_env env, napi_value exports) {
+    PropertyValueInit(env, exports);
+    napi_property_descriptor desc[] = {
+        DECLARE_NAPI_FUNCTION("enableWifi", EnableWifi),
+        DECLARE_NAPI_FUNCTION("disableWifi", DisableWifi),
+        DECLARE_NAPI_FUNCTION("isWifiActive", IsWifiActive),
+        DECLARE_NAPI_FUNCTION("scan", Scan),
+        DECLARE_NAPI_FUNCTION("getScanInfos", GetScanInfos),
+        DECLARE_NAPI_FUNCTION("getScanInfosSync", GetScanResults),
+        DECLARE_NAPI_FUNCTION("getScanResults", GetScanInfos),
+        DECLARE_NAPI_FUNCTION("getScanResultsSync", GetScanResults),
+        DECLARE_NAPI_FUNCTION("addDeviceConfig", AddDeviceConfig),
+        DECLARE_NAPI_FUNCTION("addUntrustedConfig", AddUntrustedConfig),
+        DECLARE_NAPI_FUNCTION("removeUntrustedConfig", RemoveUntrustedConfig),
+        DECLARE_NAPI_FUNCTION("addCandidateConfig", AddCandidateConfig),
+        DECLARE_NAPI_FUNCTION("removeCandidateConfig", RemoveCandidateConfig),
+        DECLARE_NAPI_FUNCTION("connectToCandidateConfig", ConnectToCandidateConfig),
+        DECLARE_NAPI_FUNCTION("getCandidateConfigs", GetCandidateConfigs),
+        DECLARE_NAPI_FUNCTION("connectToNetwork", ConnectToNetwork),
+        DECLARE_NAPI_FUNCTION("connectToDevice", ConnectToDevice),
+        DECLARE_NAPI_FUNCTION("isConnected", IsConnected),
+        DECLARE_NAPI_FUNCTION("disconnect", Disconnect),
+        DECLARE_NAPI_FUNCTION("getSignalLevel", GetSignalLevel),
+        DECLARE_NAPI_FUNCTION("reconnect", ReConnect),
+        DECLARE_NAPI_FUNCTION("reassociate", ReAssociate),
+        DECLARE_NAPI_FUNCTION("getIpInfo", GetIpInfo),
+        DECLARE_NAPI_FUNCTION("getLinkedInfo", GetLinkedInfo),
+        DECLARE_NAPI_FUNCTION("removeDevice", RemoveDevice),
+        DECLARE_NAPI_FUNCTION("removeAllNetwork", RemoveAllNetwork),
+        DECLARE_NAPI_FUNCTION("disableNetwork", DisableNetwork),
+        DECLARE_NAPI_FUNCTION("getCountryCode", GetCountryCode),
+        DECLARE_NAPI_FUNCTION("getDeviceConfigs", GetDeviceConfigs),
+        DECLARE_NAPI_FUNCTION("updateNetwork", UpdateNetwork),
+        DECLARE_NAPI_FUNCTION("getSupportedFeatures", GetSupportedFeatures),
+        DECLARE_NAPI_FUNCTION("isFeatureSupported", IsFeatureSupported),
+        DECLARE_NAPI_FUNCTION("getDeviceMacAddress", GetDeviceMacAddress),
+        DECLARE_NAPI_FUNCTION("isHotspotActive", IsHotspotActive),
+        DECLARE_NAPI_FUNCTION("isHotspotDualBandSupported", IsHotspotDualBandSupported),
+        DECLARE_NAPI_FUNCTION("enableHotspot", EnableHotspot),
+        DECLARE_NAPI_FUNCTION("disableHotspot", DisableHotspot),
+        DECLARE_NAPI_FUNCTION("setHotspotConfig", SetHotspotConfig),
+        DECLARE_NAPI_FUNCTION("getHotspotConfig", GetHotspotConfig),
+        DECLARE_NAPI_FUNCTION("getStations", GetStations),
+        DECLARE_NAPI_FUNCTION("addBlockList", AddBlockList),
+        DECLARE_NAPI_FUNCTION("delBlockList", DelBlockList),
+        DECLARE_NAPI_FUNCTION("getP2pLinkedInfo", GetP2pLinkedInfo),
+        DECLARE_NAPI_FUNCTION("getCurrentGroup", GetCurrentGroup),
+        DECLARE_NAPI_FUNCTION("getP2pPeerDevices", GetP2pDevices),
+        DECLARE_NAPI_FUNCTION("getP2pLocalDevice", GetP2pLocalDevice),
+        DECLARE_NAPI_FUNCTION("createGroup", CreateGroup),
+        DECLARE_NAPI_FUNCTION("removeGroup", RemoveGroup),
+        DECLARE_NAPI_FUNCTION("p2pConnect", P2pConnect),
+        DECLARE_NAPI_FUNCTION("p2pCancelConnect", P2pCancelConnect),
+        DECLARE_NAPI_FUNCTION("p2pDisconnect", P2pCancelConnect),
+        DECLARE_NAPI_FUNCTION("startDiscoverDevices", StartDiscoverDevices),
+        DECLARE_NAPI_FUNCTION("stopDiscoverDevices", StopDiscoverDevices),
+        DECLARE_NAPI_FUNCTION("deletePersistentGroup", DeletePersistentGroup),
+        DECLARE_NAPI_FUNCTION("getP2pGroups", GetP2pGroups),
+        DECLARE_NAPI_FUNCTION("setDeviceName", SetDeviceName),
+        DECLARE_NAPI_FUNCTION("on", On),
+        DECLARE_NAPI_FUNCTION("off", Off),
+    };
+
+    NAPI_CALL(env, napi_define_properties(env, exports, sizeof(desc) / sizeof(napi_property_descriptor), desc));
     return exports;
 }
 

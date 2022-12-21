1d087bf47da59f05e45105d04b45bca58853562c
sunjinqiang <17319862417@163.com>
2022-12-21 07:35:23 +0000

    update wifi/test/wifi_standard/native/wifi_c_hid2d_test.cpp.
    Signed-off-by: sunjinqiang <17319862417@163.com>

--- wifi/test/wifi_standard/native/wifi_c_hid2d_test.cpp
+++ wifi/test/wifi_standard/native/wifi_c_hid2d_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,8 +17,6 @@
 
 #include "../../../interfaces/kits/c/wifi_hid2d.h"
 #include "../../../interfaces/kits/c/wifi_device.h"
-
-
 
 using ::testing::_;
 using ::testing::Return;

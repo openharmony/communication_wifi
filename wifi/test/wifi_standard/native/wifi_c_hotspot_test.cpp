a661be1013f2f3fa1e688f6f3bf35edc9f575869
sunjinqiang <17319862417@163.com>
2022-12-21 07:36:03 +0000

    update wifi/test/wifi_standard/native/wifi_c_hotspot_test.cpp.
    Signed-off-by: sunjinqiang <17319862417@163.com>

--- wifi/test/wifi_standard/native/wifi_c_hotspot_test.cpp
+++ wifi/test/wifi_standard/native/wifi_c_hotspot_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,8 +18,6 @@
 #include "../../../interfaces/kits/c/wifi_hotspot.h"
 #include "../../../interfaces/kits/c/wifi_hotspot_config.h"
 #include "../../../interfaces/kits/c/wifi_device_config.h"
-
-
 
 using ::testing::_;
 using ::testing::Return;

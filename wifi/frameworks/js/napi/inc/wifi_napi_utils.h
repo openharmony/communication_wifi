44d39dc664f8b10477783964717974e955fffb61
fengye <fengye10@huawei.com>
2022-12-26 09:50:28 +0000

    fix
    Signed-off-by: fengye <fengye10@huawei.com>

--- wifi/frameworks/js/napi/inc/wifi_napi_utils.h
+++ wifi/frameworks/js/napi/inc/wifi_napi_utils.h
@@ -95,6 +95,8 @@ napi_status SetValueInt64(const napi_env& env, const char* fieldStr, const int64
 napi_status SetValueBool(const napi_env& env, const char* fieldStr, const bool boolValue, napi_value& result);
 napi_value DoAsyncWork(const napi_env& env, AsyncContext *asyncContext,
     const size_t argc, const napi_value *argv, const size_t nonCallbackArgNum);
+void SetNamedPropertyByInteger(napi_env, napi_value dstObj, int32_t objName, const char *propName);
+
 enum class SecTypeJs {
     /** Invalid security type */
     SEC_TYPE_INVALID = 0,

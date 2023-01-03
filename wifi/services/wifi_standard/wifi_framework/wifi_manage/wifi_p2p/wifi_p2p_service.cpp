648ea60c707f0c90bd6472a086bc0b4c9b23fda8
xiaosi <xiaosi2@huawei.com>
2023-01-03 16:27:46 +0800

    wifi_p2p_service GetP2pRecommendChannel
    Signed-off-by: xiaosi <xiaosi2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/wifi_p2p_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/wifi_p2p_service.cpp
@@ -346,8 +346,10 @@ int WifiP2pService::GetP2pRecommendChannel(void)
     WifiSettings::GetInstance().GetLinkedInfo(linkedInfo);
     if (linkedInfo.connState == CONNECTED) {
         channel = FrequencyToChannel(linkedInfo.frequency);
-        WIFI_LOGI("Recommend linked channel: %{public}d", channel);
-        return channel;
+        if (linkedInfo.band == static_cast<int>(BandType::BAND_5GHZ)) {
+            WIFI_LOGI("Recommend linked channel: %{public}d", channel);
+            return channel;
+        }
     }
 
     ChannelsTable channels;
@@ -369,8 +371,9 @@ int WifiP2pService::GetP2pRecommendChannel(void)
         WIFI_LOGI("Recommend 5G channel: %{public}d", channel);
         return channel;
     }
-    WIFI_LOGI("Recommend 2G channel: %{public}d", COMMON_USING_2G_CHANNEL);
-    return COMMON_USING_2G_CHANNEL;
+    channel = (channel == 0) ? COMMON_USING_2G_CHANNEL : channel;
+    WIFI_LOGI("Recommend 2G channel: %{public}d", channel);
+    return channel;
 }
 
 ErrCode WifiP2pService::Hid2dSetUpperScene(const std::string& ifName, const Hid2dUpperScene& scene)

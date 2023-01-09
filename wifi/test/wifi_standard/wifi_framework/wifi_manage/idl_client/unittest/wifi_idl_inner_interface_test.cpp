fbf5d2c715495870baaa9d421f3939c7fda314a2
yiyajun <yiyajun@huawei.com>
2023-01-09 16:46:25 +0800

    add tdd test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/wifi_idl_inner_interface_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/wifi_idl_inner_interface_test.cpp
@@ -25,7 +25,7 @@ namespace OHOS {
 namespace Wifi {
 constexpr int LENTH = 16;
 constexpr int LENTH1 = 11;
-class wifiidlinnerinterfacetest : public testing::Test {
+class WifiIdlInnerInterfaceTest : public testing::Test {
 public:
     static void SetUpTestCase()
     {}
@@ -37,7 +37,7 @@ public:
     {}
 };
 
-HWTEST_F(wifiidlinnerinterfacetest, OnApStaJoinOrLeaveTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnApStaJoinOrLeaveTest, TestSize.Level1)
 {
     CStationInfo* info = nullptr;
     int id = 1;
@@ -50,14 +50,14 @@ HWTEST_F(wifiidlinnerinterfacetest, OnApStaJoinOrLeaveTest, TestSize.Level1)
     OnApStaJoinOrLeave(&infomation, id);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnApEnableOrDisableTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnApEnableOrDisableTest, TestSize.Level1)
 {
     int status = 1;
     int id = 1;
     OnApEnableOrDisable(status, id);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnConnectChangedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnConnectChangedTest, TestSize.Level1)
 {
     int status = 1;
     int networkId = 1;
@@ -65,7 +65,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnConnectChangedTest, TestSize.Level1)
     OnConnectChanged(status, networkId, mac);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnBssidChangedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnBssidChangedTest, TestSize.Level1)
 {
     char *rea = nullptr;
     char *bss = nullptr;
@@ -76,7 +76,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnBssidChangedTest, TestSize.Level1)
     OnBssidChanged(reason, bssid);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnWpaStateChangedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnWpaStateChangedTest, TestSize.Level1)
 {
     int status = 1;
     OnWpaStateChanged(status);
@@ -87,7 +87,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnWpaStateChangedTest, TestSize.Level1)
     OnWpsTimeOut(status);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pDeviceFoundTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pDeviceFoundTest, TestSize.Level1)
 {
     P2pDeviceInfo* info = nullptr;
     OnP2pDeviceFound(info);
@@ -99,7 +99,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pDeviceFoundTest, TestSize.Level1)
     OnP2pDeviceFound(&information);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pDeviceLostTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pDeviceLostTest, TestSize.Level1)
 {
     char *p2pDevic = nullptr;
     OnP2pDeviceLost(p2pDevic);
@@ -107,7 +107,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pDeviceLostTest, TestSize.Level1)
     OnP2pDeviceLost(p2pDeviceAddress);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGoNegotiationRequestTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGoNegotiationRequestTest, TestSize.Level1)
 {
     char *srcAdd = nullptr;
     short passwordId = 1;
@@ -116,18 +116,18 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pGoNegotiationRequestTest, TestSize.Leve
     OnP2pGoNegotiationRequest(srcAddress, passwordId);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGoNegotiationSuccessTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGoNegotiationSuccessTest, TestSize.Level1)
 {
     OnP2pGoNegotiationSuccess();
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGoNegotiationFailureTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGoNegotiationFailureTest, TestSize.Level1)
 {
     int status = 1;
     OnP2pGoNegotiationFailure(status);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pInvitationResultTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pInvitationResultTest, TestSize.Level1)
 {
     char *bss = nullptr;
     int status = 1;
@@ -136,7 +136,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pInvitationResultTest, TestSize.Level1)
     OnP2pInvitationResult(bssid, status);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pInvitationReceivedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pInvitationReceivedTest, TestSize.Level1)
 {
     P2pInvitationInfo *info = nullptr;
     OnP2pInvitationReceived(info);
@@ -147,12 +147,12 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pInvitationReceivedTest, TestSize.Level1
     OnP2pInvitationReceived(&information);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupFormationSuccessTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGroupFormationSuccessTest, TestSize.Level1)
 {
     OnP2pGroupFormationSuccess();
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupFormationFailureTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGroupFormationFailureTest, TestSize.Level1)
 {
     char *failure = nullptr;
     OnP2pGroupFormationFailure(failure);
@@ -160,7 +160,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupFormationFailureTest, TestSize.Lev
     OnP2pGroupFormationFailure(failureReason);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupStartedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGroupStartedTest, TestSize.Level1)
 {
     P2pGroupInfo *group = nullptr;
     OnP2pGroupStarted(group);
@@ -171,7 +171,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupStartedTest, TestSize.Level1)
     OnP2pGroupStarted(&groupInfo);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupRemovedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pGroupRemovedTest, TestSize.Level1)
 {
     char *groupIf = nullptr;
     int isGo = 1;
@@ -180,7 +180,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pGroupRemovedTest, TestSize.Level1)
     OnP2pGroupRemoved(groupIfName, isGo);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pProvisionDiscoveryTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pProvisionDiscoveryTest, TestSize.Level1)
 {
     char *p2pDeviceAdd = nullptr;
     OnP2pProvisionDiscoveryPbcRequest(p2pDeviceAdd);
@@ -192,7 +192,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pProvisionDiscoveryTest, TestSize.Level1
     OnP2pProvisionDiscoveryEnterPin(p2pDeviceAddress);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pProvisionDiscoveryShowPinTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pProvisionDiscoveryShowPinTest, TestSize.Level1)
 {
     char *p2pDeviceAddress = nullptr;
     char *generatedPin = nullptr;
@@ -203,17 +203,17 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pProvisionDiscoveryShowPinTest, TestSize
     OnP2pProvisionDiscoveryShowPin(p2pDeviceAdd, pin);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pProvisionDiscoveryFailureTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pProvisionDiscoveryFailureTest, TestSize.Level1)
 {
     OnP2pProvisionDiscoveryFailure();
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pFindStoppedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pFindStoppedTest, TestSize.Level1)
 {
     OnP2pFindStopped();
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pServiceDiscoveryResponseTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pServiceDiscoveryResponseTest, TestSize.Level1)
 {
     char *srcAdd = nullptr;
     short updateIndicator = 1;
@@ -224,7 +224,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pServiceDiscoveryResponseTest, TestSize.
     OnP2pServiceDiscoveryResponse(srcAddress, updateIndicator, tlvs, tlvsLength);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pStaDeauthorizedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pStaDeauthorizedTest, TestSize.Level1)
 {
     char *p2pDeviceAddress = nullptr;
     OnP2pStaDeauthorized(p2pDeviceAddress);
@@ -234,12 +234,12 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pStaDeauthorizedTest, TestSize.Level1)
     OnP2pStaAuthorized(p2pDeviceAdd);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pConnectSupplicantFailedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pConnectSupplicantFailedTest, TestSize.Level1)
 {
     OnP2pConnectSupplicantFailed();
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pServDiscReqTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pServDiscReqTest, TestSize.Level1)
 {
     P2pServDiscReqInfo *info = nullptr;
     OnP2pServDiscReq(info);
@@ -251,7 +251,7 @@ HWTEST_F(wifiidlinnerinterfacetest, OnP2pServDiscReqTest, TestSize.Level1)
     OnP2pServDiscReq(&infomation);
 }
 
-HWTEST_F(wifiidlinnerinterfacetest, OnP2pIfaceCreatedTest, TestSize.Level1)
+HWTEST_F(WifiIdlInnerInterfaceTest, OnP2pIfaceCreatedTest, TestSize.Level1)
 {
     char ifName[] = "TV";
     int isGo = 1;

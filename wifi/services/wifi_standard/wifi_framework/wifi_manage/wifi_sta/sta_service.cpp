6da377b517a8321b966d3f8f66afafe683b4372c
xiaosi <xiaosi2@huawei.com>
2023-01-11 10:11:42 +0800

    systemApp & channel
    Signed-off-by: xiaosi <xiaosi2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp
@@ -60,7 +60,6 @@ ErrCode StaService::InitStaService(const StaServiceCallback &callbacks)
         WIFI_LOGE("Alloc pStaStateMachine failed.\n");
         return WIFI_OPT_FAILED;
     }
-
     if (pStaStateMachine->InitStaStateMachine() != WIFI_OPT_SUCCESS) {
         WIFI_LOGE("InitStaStateMachine failed.\n");
         return WIFI_OPT_FAILED;
@@ -73,12 +72,10 @@ ErrCode StaService::InitStaService(const StaServiceCallback &callbacks)
         WIFI_LOGE("Alloc pStaMonitor failed.\n");
         return WIFI_OPT_FAILED;
     }
-
     if (pStaMonitor->InitStaMonitor() != WIFI_OPT_SUCCESS) {
         WIFI_LOGE("InitStaMonitor failed.\n");
         return WIFI_OPT_FAILED;
     }
-
     pStaMonitor->SetStateMachine(pStaStateMachine);
 
     ChannelsTable chanTbs;

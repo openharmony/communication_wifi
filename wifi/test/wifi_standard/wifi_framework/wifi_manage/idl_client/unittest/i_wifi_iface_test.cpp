16524b69b753cf627f85acb5b421b61d24bf830b
yiyajun <yiyajun@huawei.com>
2023-01-12 20:09:24 +0800

    add tdd test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/i_wifi_iface_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/i_wifi_iface_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,17 +12,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-
+#include <gtest/gtest.h>
 #include "i_wifi_iface.h"
 
 using namespace testing::ext;
 
 namespace OHOS {
 namespace Wifi {
-
 constexpr int LENTH = 5;
-
 class IWifiIfaceTest : public testing::Test {
 public:
     static void SetUpTestCase()

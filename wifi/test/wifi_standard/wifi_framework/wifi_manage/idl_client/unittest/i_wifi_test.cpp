7bcbf6f6471b0b7a09ef417728e1c3080e456bec
yiyajun <yiyajun@huawei.com>
2023-01-12 19:07:54 +0800

    add tdd test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

---
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/i_wifi_test.cpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include "i_wifi.h"
+
+using namespace testing::ext;
+
+namespace OHOS {
+namespace Wifi {
+
+constexpr int LENTH = 5;
+
+class IWifiTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {}
+    static void TearDownTestCase()
+    {}
+    virtual void SetUp()
+    {}
+    virtual void TearDown()
+    {}
+};
+
+HWTEST_F(IWifiTest, GetWifiChipTest, TestSize.Level1)
+{
+    uint8_t id = 1;;
+    IWifiChip chip;
+    GetWifiChip(id, &chip);
+}
+
+HWTEST_F(IWifiTest, GetWifiChipIdsTest, TestSize.Level1)
+{
+    uint8_t ids = 1;;
+    int32_t size = 1;
+    GetWifiChip(&ids, &size);
+}
+
+HWTEST_F(IWifiTest, StopTest, TestSize.Level1)
+{
+    Stop();
+}
+
+HWTEST_F(IWifiTest, NotifyClearTest, TestSize.Level1)
+{
+    NotifyClear();
+}
+
+HWTEST_F(IWifiTest, OnTransactTest, TestSize.Level1)
+{
+    Context context;
+    context.szRead = "12";
+    OnTransact(&context);
+}
+}  // namespace Wifi
+}  // namespace OHOS

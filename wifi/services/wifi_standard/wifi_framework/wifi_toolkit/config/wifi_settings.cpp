8f75035feb6123ae5c57ffad636ac5a12225a808
张绍成 <zhangshaocheng2@huawei.com>
2023-02-03 10:53:03 +0800

    设置热点默认自动关闭时间为10分钟
    Signed-off-by: 张绍成 <zhangshaocheng2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
@@ -46,7 +46,7 @@ WifiSettings::WifiSettings()
       mPowerSavingModeState(MODE_STATE_CLOSE),
       mFreezeModeState(MODE_STATE_CLOSE),
       mNoChargerPlugModeState(MODE_STATE_CLOSE),
-      mHotspotIdleTimeout(10 * 60000),
+      mHotspotIdleTimeout(HOSSPOT_IDLE_TIMEOUT_INTERVAL_MS),
       explicitGroup(false)
 {
     mHotspotState[0] = static_cast<int>(ApState::AP_STATE_CLOSED);

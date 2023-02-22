9836a1f6314608fb14c358b07a886f7b5b64556e
yiyajun <yiyajun@huawei.com>
2023-02-22 11:33:42 +0800

    add tdd test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

---
+++ wifi/test/wifi_standard/native/wifi_device_callback_stub_test.cpp
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "wifi_device_callback_stub.h"
+#include "wifi_logger.h"
+
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::DoAll;
+using ::testing::Eq;
+using ::testing::Return;
+using ::testing::SetArgReferee;
+using ::testing::StrEq;
+using ::testing::TypedEq;
+using ::testing::ext::TestSize;
+
+DEFINE_WIFILOG_LABEL("WifiDeviceCallBackStubTest");
+namespace OHOS {
+namespace Wifi {
+class WifiDeviceCallBackStubTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    virtual void SetUp() {
+        pWifiDeviceCallBackStub = std::make_unique<WifiDeviceCallBackStub>();
+    }
+
+    virtual void TearDown() {
+        pWifiDeviceCallBackStub.reset();
+    }
+
+public:
+    std::unique_ptr<WifiDeviceCallBackStub> pWifiDeviceCallBackStub;
+};
+
+class IWifiDeviceCallBackMock : public IWifiDeviceCallBack {
+public:
+    IWifiDeviceCallBackMock() {
+        WIFI_LOGI("IWifiDeviceCallBackMock");
+    }
+
+    virtual ~IWifiDeviceCallBackMock() {
+        WIFI_LOGI("~IWifiDeviceCallBackMock");
+    }
+
+public:
+    void OnWifiStateChanged(int state) override {
+        WIFI_LOGI("OnWifiStateChanged test");
+    }
+
+    void OnWifiConnectionChanged(int state, const WifiLinkedInfo &info) override {
+        WIFI_LOGI("OnWifiConnectionChanged test");
+    }
+
+    void OnWifiRssiChanged(int rssi) override {
+        WIFI_LOGI("OnWifiRssiChanged test");
+    }
+
+    void OnWifiWpsStateChanged(int state, const std::string &pinCode) override {
+        WIFI_LOGI("OnWifiWpsStateChanged test");
+    }
+
+    void OnStreamChanged(int direction) override {
+        WIFI_LOGI("OnStreamChanged test");
+    }
+
+    void OnDeviceConfigChanged(ConfigChange value) override {
+        WIFI_LOGI("OnDeviceConfigChanged test");
+    }
+
+    OHOS::sptr<OHOS::IRemoteObject> AsObject() override {
+        return nullptr;
+    }
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest1, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest1 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    data.WriteInt32(1);
+    pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option);
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest2, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest2 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest2, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest2 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest3, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest3 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest4, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest4 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest5, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest5 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest6, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest6 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest7, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest7 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    sptr<IWifiDeviceCallBack> callBack = new (std::nothrow) IWifiDeviceCallBackMock();
+    pWifiDeviceCallBackStub->RegisterUserCallBack(callBack);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option) == 0);
+    delete callBack;
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, OnRemoteRequestTest8, TestSize.Level1)
+{
+    WIFI_LOGI("OnRemoteRequestTest8 enter");
+    uint32_t code = 0;
+    MessageParcel data;
+    MessageParcel reply;
+    MessageOption option;
+    pWifiDeviceCallBackStub->OnRemoteRequest(code, data, reply, option);
+}
+
+HWTEST_F(WifiDeviceCallBackStubTest, IsRemoteDiedTest, TestSize.Level1)
+{
+    WIFI_LOGI("IsRemoteDiedTest enter");
+    bool val = false;
+    pWifiDeviceCallBackStub->SetRemoteDied(val);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->IsRemoteDied() == false);
+    val = true;
+    pWifiDeviceCallBackStub->SetRemoteDied(val);
+    EXPECT_TRUE(pWifiDeviceCallBackStub->IsRemoteDied() == true);
+}
+} // WIFI
+} // OHOS

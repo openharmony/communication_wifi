26d303e9d4157bf344c707c3f300ac9df34b79df
yiyajun <yiyajun@huawei.com>
2023-02-25 15:46:56 +0800

    add tdd test
    Signed-off-by: yiyajun <yiyajun@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/i_wifi_iface_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/i_wifi_iface_test.cpp
@@ -13,6 +13,8 @@
  * limitations under the License.
  */
 #include <gtest/gtest.h>
+#include "i_wifi_chip.h"
+#include "i_wifi_hotspot_iface.h"
 #include "i_wifi_iface.h"
 
 using namespace testing::ext;
@@ -20,6 +22,8 @@ using namespace testing::ext;
 namespace OHOS {
 namespace Wifi {
 constexpr int LENTH = 5;
+constexpr int LENMAC = 17;
+
 class IWifiIfaceTest : public testing::Test {
 public:
     static void SetUpTestCase()
@@ -30,6 +34,10 @@ public:
     {}
     virtual void TearDown()
     {}
+
+    static void onStaJoinOrLeaveTest(const CStationInfo *info, int id)
+    {
+    }
 };
 
 HWTEST_F(IWifiIfaceTest, GetNameTest, TestSize.Level1)
@@ -45,5 +53,118 @@ HWTEST_F(IWifiIfaceTest, GetTypeTest, TestSize.Level1)
     GetType(&type);
 }
 
+HWTEST_F(IWifiIfaceTest, StartSoftApTest, TestSize.Level1)
+{
+    int id = 0;
+    StartSoftAp(id);
+}
+
+HWTEST_F(IWifiIfaceTest, StopSoftApTest, TestSize.Level1)
+{
+    int id = 0;
+    StopSoftAp(id);
+}
+
+HWTEST_F(IWifiIfaceTest, SetHostapdConfigTest, TestSize.Level1)
+{
+    HostapdConfig config;
+    int id = 0;
+    SetHostapdConfig(&config, id);
+}
+
+HWTEST_F(IWifiIfaceTest, GetStaInfosTest, TestSize.Level1)
+{
+    char infos[] = "GetStaInfosTest";
+    int32_t size = 1;
+    int id = 0;
+    GetStaInfos(infos, &size, id);
+}
+
+HWTEST_F(IWifiIfaceTest, SetMacFilterTest, TestSize.Level1)
+{
+    unsigned char mac[] = "00:00:00:00:00:00";
+    int lenMac = LENMAC;
+    int id = 0;
+    SetMacFilter(mac, lenMac, id);
+}
+
+HWTEST_F(IWifiIfaceTest, DelMacFilterTest, TestSize.Level1)
+{
+    unsigned char mac[] = "00:00:00:00:00:00";
+    int lenMac = LENMAC;
+    int id = 0;
+    DelMacFilter(mac, lenMac, id);
+}
+
+HWTEST_F(IWifiIfaceTest, DisassociateStaTest, TestSize.Level1)
+{
+    unsigned char mac[] = "00:00:00:00:00:00";
+    int lenMac = LENMAC;
+    int id = 0;
+    DisassociateSta(mac, lenMac, id);
+}
+
+HWTEST_F(IWifiIfaceTest, GetValidFrequenciesForBandTest, TestSize.Level1)
+{
+    int32_t band = 1;
+    int frequencies = 1;
+    int32_t size = 1;
+    int id = 0;
+    GetValidFrequenciesForBand(band, &frequencies, &size, id);
+}
+
+HWTEST_F(IWifiIfaceTest, SetCountryCodeTest, TestSize.Level1)
+{
+    const char code;
+    int id = 0;
+    SetCountryCode(&code, id);
+}
+
+HWTEST_F(IWifiIfaceTest, RegisterAsscociatedEventTest, TestSize.Level1)
+{
+    IWifiApEventCallback callback;
+    int id = 0;
+    RegisterAsscociatedEvent(callback, id)
+    callback.onStaJoinOrLeave = onStaJoinOrLeaveTest;
+    RegisterAsscociatedEvent(callback, id);
+}
+
+HWTEST_F(IWifiIfaceTest, WpaSetPowerModelTest, TestSize.Level1)
+{
+    const int model = 1;
+    int id = 0;
+    WpaSetPowerModel(model, id);
+}
+
+HWTEST_F(IWifiIfaceTest, WpaGetPowerModelTest, TestSize.Level1)
+{
+    int model = 1;
+    int id = 0;
+    WpaGetPowerModel(&model, id);
+}
+
+HWTEST_F(IWifiIfaceTest, IsChipSupportCsaTest, TestSize.Level1)
+{
+    bool isSupport = false;
+    IsChipSupportCsa(&isSupport);
+}
+
+HWTEST_F(IWifiIfaceTest, IsChipSupportRadarDetectTest, TestSize.Level1)
+{
+    bool isSupport = false;
+    IsChipSupportRadarDetect(&isSupport);
+}
+
+HWTEST_F(IWifiIfaceTest, IsChipSupportDfsChannelTest, TestSize.Level1)
+{
+    bool isSupport = false;
+    IsChipSupportDfsChannel(&isSupport);
+}
+
+HWTEST_F(IWifiIfaceTest, IsChipSupportIndoorChannelTest, TestSize.Level1)
+{
+    bool isSupport = false;
+    IsChipSupportIndoorChannel(&isSupport);
+}
 }  // namespace Wifi
 }  // namespace OHOS

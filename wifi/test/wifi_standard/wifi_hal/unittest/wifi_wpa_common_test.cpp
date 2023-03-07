8a55dc8084ff0e589164dc55946e71e82f61a6b5
sunjingqiang <17319862417@163.com>
2023-03-07 19:22:08 +0800

    TDD用例更新
    Signed-off-by: sunjingqiang <17319862417@163.com>

---
+++ wifi/test/wifi_standard/wifi_hal/unittest/wifi_wpa_common_test.cpp
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "securec.h"
+#include "wifi_hal_common_func.h"
+
+using ::testing::_;
+using ::testing::AtLeast;
+using ::testing::Eq;
+using ::testing::ext::TestSize;
+
+namespace OHOS {
+namespace Wifi {
+class WifiWpaCommonTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+};
+
+HWTEST_F(WifiHalCommonTest, Hex2DecTest, TestSize.Level1)
+{
+    char str[] = "0z1259";
+    char src[] = "0a1259";
+    char srf[] = "0xaAfF29";
+    EXPECT_EQ(Hex2Dec(nullptr), 0);
+    EXPECT_EQ(Hex2Dec(str), 0);
+    EXPECT_EQ(Hex2Dec(src), 0);
+    EXPECT_EQ(Hex2Dec(srf), 0);
+}
+
+HWTEST_F(WifiHalCommonTest, TrimQuotationMarkTest, TestSize.Level1)
+{
+    char str[] = "A1s62";
+	TrimQuotationMark(nullptr, 'A');
+	TrimQuotationMark(str, 'A');
+}
+
+HWTEST_F(WifiHalCommonTest, InitWpaCtrlTest, TestSize.Level1)
+{
+	WpaCtrl *pCtrl = nullptr;
+    char str[] = "A1s62";
+	InitWpaCtrl(pCtrl, str);
+	pCtrl->pRecv->s = 1;
+	InitWpaCtrl(pCtrl, nullptr);
+}
+} // namespace Wifi
+} // namespace OHOS
+

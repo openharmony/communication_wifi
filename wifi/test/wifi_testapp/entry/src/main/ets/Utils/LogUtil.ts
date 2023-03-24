8afcdeb4607c1db4ca8ab80991bbd944567899ad
wangshi <wangshi@kaihong.com>
2023-03-24 09:11:57 +0800

    增加wifitestapp，增加wifiapi测试内容
    Signed-off-by: wangshi <wangshi@kaihong.com>

---
+++ wifi/test/wifi_testapp/entry/src/main/ets/Utils/LogUtil.ts
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2022 Shenzhen Kaihong Digital Industry Development Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import BaseModel from './BaseModel';
+
+let LogLevel = {
+    /**
+       * debug
+       */
+    DEBUG: 3,
+
+    /**
+       * info
+       */
+    INFO: 4,
+
+    /**
+       * warn
+       */
+    WARN: 5,
+
+    /**
+       * error
+       */
+    ERROR: 6,
+
+    /**
+       * fatal
+       */
+    FATAL: 7,
+};
+
+const LOG_LEVEL = LogLevel.INFO
+
+/**
+ *  log package tool class
+ */
+export class LogUtil extends BaseModel{
+    debug(msg): void {
+        console.info(msg)
+        if (LogLevel.DEBUG >= LOG_LEVEL) {
+            console.info(msg);
+        }
+    }
+
+    log(msg): void {
+        console.info(msg)
+        if (LogLevel.INFO >= LOG_LEVEL) {
+            console.info(msg);
+        }
+    }
+
+    info(msg): void {
+        console.info(msg)
+        if (LogLevel.INFO >= LOG_LEVEL) {
+            console.info(msg);
+        }
+    }
+
+    warn(msg): void {
+        console.info(msg)
+        if (LogLevel.WARN >= LOG_LEVEL) {
+            console.warn(msg);
+        }
+    }
+
+    error(msg): void {
+        if (LogLevel.ERROR >= LOG_LEVEL) {
+            console.error(msg);
+        }
+    }
+}
+
+let mLogUtil = new LogUtil();
+export default mLogUtil as LogUtil
+;

dca2a494d3873b525f3cf7fda27e75c67eed0482
HW-cjd <chengjiadong@huawei.com>
2023-07-13 15:36:48 +0800

    wifi重启bug修改
    Signed-off-by: HW-cjd <chengjiadong@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_config_file_spec.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_config_file_spec.cpp
@@ -783,7 +783,7 @@ template<>
 void ClearTClass<WifiConfig>(WifiConfig &item)
 {
     item.scanAlwaysSwitch = false;
-    item.operatorWifiType = static_cast<int>(OperatorWifiType::INITIAL_TYPE);
+    item.staAirplaneMode = static_cast<int>(OperatorWifiType::INITIAL_TYPE);
     item.canOpenStaWhenAirplane = false;
     item.staLastState = false;
     item.savedDeviceAppraisalPriority = PRIORITY_1;
@@ -825,8 +825,8 @@ static int SetWifiConfigValueFirst(WifiConfig &item, const std::string &key, con
 {
     if (key == "scanAlwaysSwitch") {
         item.scanAlwaysSwitch = (std::stoi(value) != 0); /* 0 -> false 1 -> true */
-    } else if (key == "operatorWifiType") {
-        item.operatorWifiType = std::stoi(value);
+    } else if (key == "staAirplaneMode") {
+        item.staAirplaneMode = std::stoi(value);
     } else if (key == "canOpenStaWhenAirplane") {
         item.canOpenStaWhenAirplane = (std::stoi(value) != 0);
     } else if (key == "staLastState") {
@@ -938,7 +938,7 @@ std::string OutTClassString<WifiConfig>(WifiConfig &item)
     std::ostringstream ss;
     ss << "    " <<"<WifiConfig>" << std::endl;
     ss << "    " <<"scanAlwaysSwitch=" << item.scanAlwaysSwitch << std::endl; /* bool false->0 true->1 */
-    ss << "    " <<"operatorWifiType=" << item.operatorWifiType << std::endl;
+    ss << "    " <<"staAirplaneMode=" << item.staAirplaneMode << std::endl;
     ss << "    " <<"canOpenStaWhenAirplane=" << item.canOpenStaWhenAirplane << std::endl;
     ss << "    " <<"staLastState=" << item.staLastState << std::endl;
     ss << "    " <<"savedDeviceAppraisalPriority=" << item.savedDeviceAppraisalPriority << std::endl;

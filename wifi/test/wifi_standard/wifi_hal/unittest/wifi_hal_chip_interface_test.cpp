c829683205f2da4cd71706d4c3e165be3d6c572a
sunjingqiang <17319862417@163.com>
2023-07-19 16:26:08 +0800

    TDD
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/test/wifi_standard/wifi_hal/unittest/wifi_hal_chip_interface_test.cpp
+++ wifi/test/wifi_standard/wifi_hal/unittest/wifi_hal_chip_interface_test.cpp
@@ -98,66 +98,105 @@ HWTEST_F(WifiHalChipInterfaceTest, RequestFirmwareDebugDumpTest, TestSize.Level1
     int size = 32;
     EXPECT_TRUE(RequestFirmwareDebugDump(bytes, &size) == WIFI_HAL_NOT_SUPPORT);
 }
-
+/**
+ * @tc.name: GetIsChipSupportDbdcTest
+ * @tc.desc: GetIsChipSupportDbdc()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, GetIsChipSupportDbdcTest, TestSize.Level1)
 {
     int support = 0;
     EXPECT_TRUE(GetIsChipSupportDbdc(NULL) == WIFI_HAL_FAILED);
     EXPECT_TRUE(GetIsChipSupportDbdc(&support) == WIFI_HAL_SUCCESS);
 }
-
+/**
+ * @tc.name: GetIsChipSupportCsaTest
+ * @tc.desc: GetIsChipSupportCsa()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, GetIsChipSupportCsaTest, TestSize.Level1)
 {
     int support = 0;
     EXPECT_TRUE(GetIsChipSupportCsa(NULL) == WIFI_HAL_FAILED);
     EXPECT_TRUE(GetIsChipSupportCsa(&support) == WIFI_HAL_SUCCESS);
 }
-
+/**
+ * @tc.name: GetIsChipSupportRadarDetectTest
+ * @tc.desc: GetIsChipSupportRadarDetect()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, GetIsChipSupportRadarDetectTest, TestSize.Level1)
 {
     int support = 0;
     EXPECT_TRUE(GetIsChipSupportRadarDetect(NULL) == WIFI_HAL_FAILED);
     EXPECT_TRUE(GetIsChipSupportRadarDetect(&support) == WIFI_HAL_SUCCESS);
 }
-
+/**
+ * @tc.name: GetIsChipSupportDfsChannelTest
+ * @tc.desc: GetIsChipSupportDfsChannel()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, GetIsChipSupportDfsChannelTest, TestSize.Level1)
 {
     int support = 0;
     EXPECT_TRUE(GetIsChipSupportDfsChannel(NULL) == WIFI_HAL_FAILED);
     EXPECT_TRUE(GetIsChipSupportDfsChannel(&support) == WIFI_HAL_SUCCESS);
 }
-
+/**
+ * @tc.name: GetIsChipSupportIndoorChannelTest
+ * @tc.desc: GetIsChipSupportIndoorChannel()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, GetIsChipSupportIndoorChannelTest, TestSize.Level1)
 {
     int support = 0;
     EXPECT_TRUE(GetIsChipSupportIndoorChannel(NULL) == WIFI_HAL_FAILED);
     EXPECT_TRUE(GetIsChipSupportIndoorChannel(&support) == WIFI_HAL_SUCCESS);
 }
-
+/**
+ * @tc.name: GetIfaceTest
+ * @tc.desc: GetIface()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, GetIfaceTest, TestSize.Level1)
 {
     char ifname[] = "wlan0";
     EXPECT_TRUE(GetIface(NULL, NULL) == WIFI_HAL_FAILED);
     EXPECT_TRUE(GetIface(ifname, NULL) == WIFI_HAL_FAILED);
 }
-
-
+/**
+ * @tc.name: ConvertErrorCodeTest
+ * @tc.desc: ConvertErrorCode()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest,ConvertErrorCodeTest, TestSize.Level1)
 {
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_SUCCESS), WIFI_HAL_SUCCESS);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_UNKNOWN), WIFI_HAL_VENDOR_UNKNOWN);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_UNINITIALIZED), WIFI_HAL_VENDOR_UNINITIALIZED);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_NOT_SUPPORTED), WIFI_HAL_NOT_SUPPORT);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_NOT_AVAILABLE), WIFI_HAL_VENDOR_NOT_AVAILABLE);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_INVALID_ARGS), WIFI_HAL_VENDOR_INVALID_ARGS);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_INVALID_REQUEST_ID), WIFI_HAL_VENDOR_INVALID_REQUEST_ID);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_TIMED_OUT), WIFI_HAL_VENDOR_TIMED_OUT);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_TOO_MANY_REQUESTS), WIFI_HAL_VENDOR_TOO_MANY_REQUESTS);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_OUT_OF_MEMORY), WIFI_HAL_VENDOR_OUT_OF_MEMORY);
-    EXPECT_EQ(ConvertErrorCode(HAL_VENDOR_ERROR_BUSY), WIFI_HAL_VENDOR_BUSY);
-    EXPECT_EQ(ConvertErrorCode(WIFI_HAL_INVALID_PARAM), WIFI_HAL_SUCCESS);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_SUCCESS), WIFI_HAL_SUCCESS);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_UNKNOWN), WIFI_HAL_VENDOR_UNKNOWN);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_UNINITIALIZED), WIFI_HAL_VENDOR_UNINITIALIZED);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_NOT_SUPPORTED), WIFI_HAL_NOT_SUPPORT);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_NOT_AVAILABLE), WIFI_HAL_VENDOR_NOT_AVAILABLE);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_INVALID_ARGS), WIFI_HAL_VENDOR_INVALID_ARGS);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_INVALID_REQUEST_ID), WIFI_HAL_VENDOR_INVALID_REQUEST_ID);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_TIMED_OUT), WIFI_HAL_VENDOR_TIMED_OUT);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_TOO_MANY_REQUESTS), WIFI_HAL_VENDOR_TOO_MANY_REQUESTS);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_OUT_OF_MEMORY), WIFI_HAL_VENDOR_OUT_OF_MEMORY);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::HAL_VENDOR_ERROR_BUSY), WIFI_HAL_VENDOR_BUSY);
+    EXPECT_EQ(ConvertErrorCode(HalVendorError::WIFI_HAL_INVALID_PARAM), WIFI_HAL_SUCCESS);
 }
-
+/**
+ * @tc.name: InitDefaultHalVendorFuncTest
+ * @tc.desc: InitDefaultHalVendorFunc()
+ * @tc.type: FUNC
+ * @tc.require: issue
+*/
 HWTEST_F(WifiHalChipInterfaceTest, InitDefaultHalVendorFuncTest, TestSize.Level1)
 {
     EXPECT_TRUE(InitDefaultHalVendorFunc(NULL) == HAL_VENDOR_ERROR_UNKNOWN);

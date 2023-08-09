209ce989d986e5b47f723a5713eeaf33e637bf82
CodeAdo <baliguan163@163.com>
2023-08-09 14:49:53 +0800

    native c event添加部分tdd用例
    Signed-off-by: CodeAdo <baliguan163@163.com>

--- wifi/test/wifi_standard/native/wifi_c_event_test.cpp
+++ wifi/test/wifi_standard/native/wifi_c_event_test.cpp
@@ -17,9 +17,11 @@
 #include <gtest/gtest.h>
 #include "../../../interfaces/kits/c/wifi_device.h"
 #include "../../../interfaces/kits/c/wifi_scan_info.h"
+#include "wifi_logger.h"
 
 using ::testing::Return;
 using ::testing::ext::TestSize;
+DEFINE_WIFILOG_LABEL("WifiCEventStubTest");
 
 namespace OHOS {
 namespace Wifi {
@@ -29,13 +31,79 @@ public:
     static void TearDownTestCase() {};
     virtual void SetUp() {};
     virtual void TearDown() {};
+
+    /* Connection state change */
+    static void OnWifiConnectionChanged(int state, WifiLinkedInfo *info)
+    {
+        WIFI_LOGE("OnWifiConnectionChanged enter!");
+    }
+    /* Scan state change */
+    static void OnWifiScanStateChanged(int state, int size)
+    {
+        WIFI_LOGE("OnWifiScanStateChanged enter!");
+    }
+    /* Hotspot state change */
+    static void OnHotspotStateChanged(int state)
+    {
+        WIFI_LOGE("OnHotspotStateChanged enter!");
+    }
+    /* Station connected */
+    static void OnHotspotStaJoin(StationInfo *info)
+    {
+        WIFI_LOGE("OnHotspotStaJoin enter!");
+    }
+    /* Station disconnected */
+    static void OnHotspotStaLeave(StationInfo *info)
+    {
+        WIFI_LOGE("OnHotspotStaLeave enter!");
+    }
+    /* Device config change */
+    static void OnDeviceConfigChange(ConfigChange state)
+    {
+        WIFI_LOGE("OnDeviceConfigChange enter!");
+    }
+
+    void RegisterWifiEventErrorTest()
+    {
+        WIFI_LOGE("RegisterWifiEventErrorTest enter!");
+        WifiEvent *event = nullptr;
+        RegisterWifiEvent(event);
+        EXPECT_EQ(RegisterWifiEvent(event), ERROR_WIFI_UNKNOWN);
+    }
+    void UnRegisterWifiEventTest()
+    {
+        WIFI_LOGE("UnRegisterWifiEventTest enter!");
+        WifiEvent *event = nullptr;
+        EXPECT_EQ(UnRegisterWifiEvent(event), WIFI_SUCCESS);
+    }
+    void RegisterWifiEventSuccessTest()
+    {
+        WIFI_LOGE("RegisterWifiEventSuccessTest enter!");
+        WifiEvent event;
+        event.OnWifiConnectionChanged = OnWifiConnectionChanged;
+        event.OnWifiScanStateChanged = OnWifiScanStateChanged;
+        event.OnHotspotStateChanged = OnHotspotStateChanged;
+        event.OnHotspotStaJoin = OnHotspotStaJoin;
+        event.OnHotspotStaLeave = OnHotspotStaLeave;
+        event.OnDeviceConfigChange = OnDeviceConfigChange;
+        EXPECT_EQ(RegisterWifiEvent(&event), WIFI_SUCCESS);
+        EXPECT_EQ(UnRegisterWifiEvent(&event), WIFI_SUCCESS);
+    }
 };
+
+HWTEST_F(WifiEventTest, RegisterWifiEventErrorTest, TestSize.Level1)
+{
+    RegisterWifiEventErrorTest();
+}
 
 HWTEST_F(WifiEventTest, UnRegisterWifiEventTest, TestSize.Level1)
 {
-    WifiEvent *event = nullptr;
-    RegisterWifiEvent(event);
-    EXPECT_EQ(UnRegisterWifiEvent(event), WIFI_SUCCESS);
+    UnRegisterWifiEventTest();
+}
+
+HWTEST_F(WifiEventTest, RegisterWifiEventSuccessTest, TestSize.Level1)
+{
+    RegisterWifiEventSuccessTest();
 }
 }  // namespace Wifi
 }  // namespace OHOS

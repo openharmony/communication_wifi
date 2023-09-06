0311cef0d7e25318ce91c97e5d97e7eba85f6183
sunjinqiang <17319862417@163.com>
2023-09-06 03:37:55 +0000

    update wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/http_request_test.cpp.
    Signed-off-by: sunjinqiang <17319862417@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/http_request_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/http_request_test.cpp
@@ -149,18 +149,5 @@ HWTEST_F(HttpRequestTest, HttpDataTransmit_001, TestSize.Level1)
     pHttpRequest->httpHead = "Connection: Keep-Alive\r\n";
     EXPECT_TRUE(pHttpRequest->HttpDataTransmit(iSockFd) == -1);
 }
-/**
- * @tc.name: GetHostThread_001
- * @tc.desc: test GetHostThread error
- * @tc.type: FUNC
- * @tc.require: issue
-*/
-HWTEST_F(HttpRequestTest, GetHostThread_001, TestSize.Level1)
-{
-    WIFI_LOGI("HttpDataTransmit_001");
-    int iSockFd = 1;
-    pHttpRequest->httpHead = "Connection: Keep-Alive\r\n";
-    EXPECT_TRUE(pHttpRequest->GetHostThread(iSockFd) == -1);
-}
 }  // namespace Wifi
 }  // namespace OHOS

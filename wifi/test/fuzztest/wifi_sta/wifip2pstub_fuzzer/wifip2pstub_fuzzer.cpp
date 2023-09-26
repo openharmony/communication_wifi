0562b10ceb2280dd76c25028fe8b60e45c71819e
sunjingqiang <17319862417@163.com>
2023-09-26 16:53:44 +0800

    fuzz
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/test/fuzztest/wifi_sta/wifip2pstub_fuzzer/wifip2pstub_fuzzer.cpp
+++ wifi/test/fuzztest/wifi_sta/wifip2pstub_fuzzer/wifip2pstub_fuzzer.cpp
@@ -29,7 +29,7 @@
 
 namespace OHOS {
 namespace Wifi {
-constexpr size_t U32_AT_SIZE_ZERO = 0;
+constexpr size_t U32_AT_SIZE_ZERO = 4;
 constexpr size_t MAP_P2P_NUMS = 41;
 const std::u16string FORMMGR_INTERFACE_TOKEN = u"ohos.wifi.IWifiP2pService";
 sptr<WifiP2pStub> pWifiP2pStub = WifiP2pServiceImpl::GetInstance();

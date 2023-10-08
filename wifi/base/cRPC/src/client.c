1f53d67e231f93568390e267ea6d3c336abd12a8
xiejinzhou <xiejinzhou@huawei.com>
2023-10-08 09:56:15 +0800

    add p2p enhance
    Signed-off-by: xiejinzhou <xiejinzhou@huawei.com>

--- wifi/base/cRPC/src/client.c
+++ wifi/base/cRPC/src/client.c
@@ -28,6 +28,158 @@
 #undef LOG_TAG
 #define LOG_TAG "WifiRpcClient"
 
+const int FD_CHECK_TIMEOUT = 1000; /* poll wait time, units: ms */
+const int CLIENT_STATE_IDLE = 0;
+const int CLIENT_STATE_DEAL_REPLY = 1;
+const int CLIENT_STATE_EXIT = 2;
+
+#define TMP_BUFF_SIZE 16
+
+static void *RpcClientThreadDeal(void *arg);
+
+static char *RpcClientReadMsg(RpcClient *client)
+{
+    if (client == NULL) {
+        return NULL;
+    }
+
+    char *buff = ContextGetReadRecord(client->context);
+    while (buff == NULL && client->threadRunFlag) {
+        int ret = WaitFdEvent(client->context->fd, READ_EVENT, FD_CHECK_TIMEOUT);
+        if (ret < 0) {
+            LOGE("wait server reply message failed!");
+            client->threadRunFlag = 0;
+            return NULL;
+        } else if (ret == 0) {
+            continue;
+        }
+        ret = ContextReadNet(client->context);
+        if (ret < 0) {
+            LOGE("read server reply message failed!");
+            client->threadRunFlag = 0;
+            return NULL;
+        }
+        buff = ContextGetReadRecord(client->context);
+    }
+    if (!client->threadRunFlag) {
+        if (buff != NULL) {
+            free(buff);
+            buff = NULL;
+        }
+        return NULL;
+    }
+    return buff;
+}
+
+static void RpcClientDealReadMsg(RpcClient *client, char *buff)
+{
+    if (client == NULL) {
+        if (buff != NULL) {
+            free(buff);
+            buff = NULL;
+        }
+        return;
+    }
+
+    char szTmp[TMP_BUFF_SIZE] = {0};
+    if (snprintf_s(szTmp, sizeof(szTmp), sizeof(szTmp) - 1, "N%c", client->context->cSplit) < 0) {
+        if (buff != NULL) {
+            free(buff);
+            buff = NULL;
+        }
+        return;
+    }
+    if (strncmp(buff, szTmp, strlen(szTmp)) == 0) { /* deal reply message */
+        pthread_mutex_lock(&client->mutex);
+        client->waitReply = CLIENT_STATE_DEAL_REPLY;
+        client->context->oneProcess = buff;
+        client->context->nPos = strlen(szTmp);
+        client->context->nSize = strlen(buff);
+        pthread_cond_signal(&client->condW);
+        pthread_mutex_unlock(&client->mutex);
+    } else { /* deal callback message */
+        pthread_mutex_lock(&client->mutex);
+        while (client->waitReply == CLIENT_STATE_DEAL_REPLY) {
+            pthread_cond_wait(&client->condW, &client->mutex);
+        }
+        pthread_mutex_unlock(&client->mutex);
+        client->context->oneProcess = buff;
+        client->context->nPos = strlen(szTmp);
+        client->context->nSize = strlen(buff);
+        OnTransact(client->context);
+        free(buff);
+        buff = NULL;
+    }
+    return;
+}
+
+static void *RpcClientThreadDeal(void *arg)
+{
+    RpcClient *client = (RpcClient *)arg;
+    if (client == NULL) {
+        return NULL;
+    }
+
+    while (client->threadRunFlag) {
+        char *buff = RpcClientReadMsg(client);
+        if (buff == NULL) {
+            continue;
+        }
+        RpcClientDealReadMsg(client, buff);
+    }
+    pthread_mutex_lock(&client->mutex);
+    client->waitReply = CLIENT_STATE_EXIT;
+    pthread_cond_signal(&client->condW);
+    pthread_mutex_unlock(&client->mutex);
+    LOGI("Client read message thread exiting!");
+    return NULL;
+}
+
+RpcClient *CreateRpcClient(const char *path)
+{
+    int fd = ConnectUnixServer(path);
+    if (fd < 0) {
+        return NULL;
+    }
+    SetNonBlock(fd, 1);
+    RpcClient *client = (RpcClient *)calloc(1, sizeof(RpcClient));
+    if (client == NULL) {
+        close(fd);
+        return NULL;
+    }
+    client->context = CreateContext(CONTEXT_BUFFER_MIN_SIZE);
+    if (client->context == NULL) {
+        close(fd);
+        free(client);
+        client = NULL;
+        return NULL;
+    }
+    client->context->fd = fd;
+    client->threadRunFlag = 1;
+    client->threadId = 0;
+    client->waitReply = CLIENT_STATE_IDLE;
+    client->callLockFlag = 0;
+    pthread_mutex_init(&client->mutex, NULL);
+    pthread_cond_init(&client->condW, NULL);
+    pthread_mutex_init(&client->lockMutex, NULL);
+    pthread_cond_init(&client->lockCond, NULL);
+    int ret = pthread_create(&client->threadId, NULL, RpcClientThreadDeal, client);
+    if (ret) {
+        pthread_cond_destroy(&client->condW);
+        pthread_mutex_destroy(&client->mutex);
+        pthread_cond_destroy(&client->lockCond);
+        pthread_mutex_destroy(&client->lockMutex);
+        ReleaseContext(client->context);
+        close(fd);
+        free(client);
+        client = NULL;
+        return NULL;
+    }
+    pthread_setname_np(client->threadId, "RpcClientThread");
+    signal(SIGPIPE, SIG_IGN);
+    return client;
+}
+
 void ReleaseRpcClient(RpcClient *client)
 {
     if (client != NULL) {

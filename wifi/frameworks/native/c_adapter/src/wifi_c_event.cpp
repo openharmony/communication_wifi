6d48d7f249858f6ef993dc6071597a7b0abf6514
yanxiaotao@huawei.com <yanxiaotao@huawei.com>
2023-10-24 16:19:40 +0800

    add debug for cfi check fail.
    Signed-off-by: yanxiaotao@huawei.com <yanxiaotao@huawei.com>

--- wifi/frameworks/native/c_adapter/src/wifi_c_event.cpp
+++ wifi/frameworks/native/c_adapter/src/wifi_c_event.cpp
@@ -260,7 +260,8 @@ NO_SANITIZE("cfi") WifiErrorCode EventManager::RegisterWifiEvents()
     if (mSaStatusListener == nullptr) {
         int32_t ret;
         mSaStatusListener = new OHOS::Wifi::WifiAbilityStatusChange();
-        OHOS::sptr<OHOS::ISystemAbilityManager> samgrProxy = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+        OHOS::sptr<OHOS::ISystemAbilityManager> samgrProxy =
+            OHOS::SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
         ret = samgrProxy->SubscribeSystemAbility(WIFI_DEVICE_ABILITY_ID, mSaStatusListener);
         samgrProxy->SubscribeSystemAbility(WIFI_SCAN_ABILITY_ID, mSaStatusListener);
         samgrProxy->SubscribeSystemAbility(WIFI_HOTSPOT_ABILITY_ID, mSaStatusListener);
@@ -333,7 +334,8 @@ void EventManager::Init()
         int32_t ret;
         WIFI_LOGI("EventManager Listener Init!");
         mSaStatusListener = new OHOS::Wifi::WifiAbilityStatusChange();
-        OHOS::sptr<OHOS::ISystemAbilityManager> samgrProxy = SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
+        OHOS::sptr<OHOS::ISystemAbilityManager> samgrProxy =
+            OHOS::SystemAbilityManagerClient::GetInstance().GetSystemAbilityManager();
         ret = samgrProxy->SubscribeSystemAbility(WIFI_DEVICE_ABILITY_ID, mSaStatusListener);
         samgrProxy->SubscribeSystemAbility(WIFI_SCAN_ABILITY_ID, mSaStatusListener);
         samgrProxy->SubscribeSystemAbility(WIFI_HOTSPOT_ABILITY_ID, mSaStatusListener);

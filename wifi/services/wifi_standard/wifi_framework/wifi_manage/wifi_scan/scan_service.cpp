145ca6bf30ad1f316fc1c83ade1a63d3fc023dde
l30052082 <lihui410@huawei.com>
2023-11-01 18:05:36 +0800

    revert
    Signed-off-by: l30052082 <lihui410@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
@@ -2357,12 +2357,14 @@ ErrCode ScanService::CloseWpa()
 ErrCode ScanService::OpenScanOnly() const
 {
     WIFI_LOGI("Enter ScanService::OpenScanOnly.\n");
+    mScanSerivceCallbacks.OnOpenScanOnlyRes(OperateResState::OPEN_SCAN_ONLY_SUCCEED, m_instId);
     return WIFI_OPT_SUCCESS;
 }
 
 ErrCode ScanService::CloseScanOnly() const
 {
     WIFI_LOGI("Enter ScanService::CloseScanOnly.\n");
+    mScanSerivceCallbacks.OnCloseScanOnlyRes(OperateResState::CLOSE_SCAN_ONLY_SUCCEED, m_instId);
     return WIFI_OPT_SUCCESS;
 }
 

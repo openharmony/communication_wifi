aa18946dbee0cd64ff2a51adc336a07bc4e21806
sunjingqiang <17319862417@163.com>
2023-11-22 17:27:41 +0800

    fuzz
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/wifi_manager_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/wifi_manager_test.cpp
@@ -47,11 +47,52 @@ public:
     WifiManager wifiManager;
 };
 
+HWTEST_F(WifiManagerTest, AutoStartStaService_001, TestSize.Level1)
+{
+    WIFI_LOGE("AutoStartStaService_001 enter!");
+    WifiConfigCenter::GetInstance().SetWifiMidState(WifiOprMidState::CLOSING);
+    EXPECT_EQ(wifiManager.AutoStartStaService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_OPEN_FAIL_WHEN_CLOSING);
+    WifiConfigCenter::GetInstance().SetWifiMidState(WifiOprMidState::RUNNING);
+    EXPECT_EQ(wifiManager.AutoStartStaService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_OPEN_SUCC_WHEN_OPENED);
+}
+
+HWTEST_F(WifiManagerTest, AutoStartStaService_002, TestSize.Level1)
+{
+    WIFI_LOGE("AutoStartStaService_002 enter!");
+    WIFI_LOGE("AutoStartStaService_002 enter!");
+    WifiConfigCenter::GetInstance().SetWifiMidState(WifiOprMidState::CLOSED);
+    ErrCode result = wifiManager.AutoStartStaService(AutoStartOrStopServiceReason::STA_AP_EXCLUSION);
+    WIFI_LOGE("AutoStartStaService_002 result(%{public}d)", result);
+    EXPECT_EQ(result, WIFI_OPT_FAILED);
+}
+
+HWTEST_F(WifiManagerTest, AutoStopStaService_001, TestSize.Level1)
+{
+    WIFI_LOGE("AutoStopStaService_001 enter!");
+    WifiConfigCenter::GetInstance().SetWifiMidState(WifiOprMidState::OPENING);
+    EXPECT_EQ(wifiManager.AutoStopStaService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_CLOSE_FAIL_WHEN_OPENING);
+    WifiConfigCenter::GetInstance().SetWifiMidState(WifiOprMidState::CLOSED);
+    EXPECT_EQ(wifiManager.AutoStopStaService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_CLOSE_SUCC_WHEN_CLOSED);
+}
+
+HWTEST_F(WifiManagerTest, AutoStopStaService_002, TestSize.Level1)
+{
+    WIFI_LOGE("AutoStopStaService_002 enter!");
+    WifiConfigCenter::GetInstance().SetWifiMidState(WifiOprMidState::RUNNING);
+    EXPECT_EQ(wifiManager.AutoStopStaService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_CLOSE_SUCC_WHEN_CLOSED);
+}
+
 HWTEST_F(WifiManagerTest, StartUnloadStaSaTimerTest, TestSize.Level1)
 {
     WIFI_LOGE("StartUnloadStaSaTimerTest enter!");
     wifiManager.StartUnloadStaSaTimer();
 }
+
 
 HWTEST_F(WifiManagerTest, StartUnloadScanSaTimerTest, TestSize.Level1)
 {
@@ -62,27 +103,40 @@ HWTEST_F(WifiManagerTest, StartUnloadScanSaTimerTest, TestSize.Level1)
 HWTEST_F(WifiManagerTest, AutoStartApService_001, TestSize.Level1)
 {
     WIFI_LOGE("AutoStartApService_001 enter!");
-    EXPECT_EQ(wifiManager.SoftapToggled(1, 0), WIFI_OPT_SUCCESS);
+    WifiConfigCenter::GetInstance().SetApMidState(WifiOprMidState::CLOSING, 0);
+    EXPECT_EQ(wifiManager.AutoStartApService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_OPEN_FAIL_WHEN_CLOSING);
+    WifiConfigCenter::GetInstance().SetApMidState(WifiOprMidState::RUNNING, 0);
+    EXPECT_EQ(wifiManager.AutoStartApService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_OPEN_SUCC_WHEN_OPENED);
 }
 
 HWTEST_F(WifiManagerTest, AutoStartApService_002, TestSize.Level1)
 {
     WIFI_LOGE("AutoStartApService_002 enter!");
-    EXPECT_EQ(wifiManager.SoftapToggled(0, 0), WIFI_OPT_SUCCESS);
-    EXPECT_EQ(wifiManager.SoftapToggled(1, 0), WIFI_OPT_SUCCESS);
+    WifiConfigCenter::GetInstance().SetApMidState(WifiOprMidState::CLOSED, 0);
+    EXPECT_EQ(wifiManager.AutoStartApService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_FAILED);
 }
 
 HWTEST_F(WifiManagerTest, AutoStopApService_001, TestSize.Level1)
+
 {
     WIFI_LOGE("AutoStopApService_001 enter!");
-    EXPECT_EQ(wifiManager.SoftapToggled(0, 0), WIFI_OPT_SUCCESS);
+    WifiConfigCenter::GetInstance().SetApMidState(WifiOprMidState::OPENING, 0);
+    EXPECT_EQ(wifiManager.AutoStopApService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_CLOSE_FAIL_WHEN_OPENING);
+    WifiConfigCenter::GetInstance().SetApMidState(WifiOprMidState::CLOSED, 0);
+    EXPECT_EQ(wifiManager.AutoStopApService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_CLOSE_SUCC_WHEN_CLOSED);
 }
 
 HWTEST_F(WifiManagerTest, AutoStopApService_002, TestSize.Level1)
 {
     WIFI_LOGE("AutoStopApService_002 enter!");
-    EXPECT_EQ(wifiManager.SoftapToggled(1, 0), WIFI_OPT_SUCCESS);
-    EXPECT_EQ(wifiManager.SoftapToggled(0, 0), WIFI_OPT_SUCCESS);
+    WifiConfigCenter::GetInstance().SetApMidState(WifiOprMidState::RUNNING, 0);
+    EXPECT_EQ(wifiManager.AutoStopApService(
+        AutoStartOrStopServiceReason::AUTO_START_UPON_STARTUP), WIFI_OPT_CLOSE_SUCC_WHEN_CLOSED);
 }
 
 HWTEST_F(WifiManagerTest, StartUnloadApSaTimerTest, TestSize.Level1)

6accae0652317963509da873f00f4dce613942cf
wujun <2019125625@qq.com>
2023-12-06 17:54:55 +0800

    rx listen
    Signed-off-by: wujun <2019125625@qq.com>

---
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_app_parser.cpp
@@ -0,0 +1,235 @@
+/*
+ * Copyright (C) 2023-2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "wifi_app_parser.h"
+#include <unordered_map>
+#include "wifi_common_def.h"
+#include "wifi_config_file_impl.h"
+#include <algorithm>
+#include "wifi_logger.h"
+
+namespace OHOS {
+namespace Wifi {
+DEFINE_WIFILOG_LABEL("WifiAppXmlParser");
+
+static constexpr auto WIFI_MONITOR_APP_FILE_PATH = CONFIG_ROOR_DIR "/wifi_monitor_apps.xml";
+static constexpr auto XML_TAG_SECTION_HEADER_MONITOR_APP = "MonitorAPP";
+static constexpr auto XML_TAG_SECTION_HEADER_GAME_INFO = "GameInfo";
+static constexpr auto XML_TAG_SECTION_HEADER_APP_WHITE_LIST = "AppWhiteList";
+static constexpr auto XML_TAG_SECTION_HEADER_APP_BLACK_LIST = "AppBlackList";
+static constexpr auto XML_TAG_SECTION_HEADER_CHARIOT_APK = "ChariotApk";
+
+static constexpr auto XML_TAG_SECTION_KEY_GAME_NAME = "gameName";
+static constexpr auto XML_TAG_SECTION_KEY_PACKAGE_NAME = "packageName";
+static constexpr auto XML_TAG_GAME_ID = "mGameId";
+static constexpr auto XML_TAG_SCENE_ID = "mScenceId";
+static constexpr auto XML_TAG_GAME_KQI = "mGameKQI";
+static constexpr auto XML_TAG_GAME_RTT = "mGameRtt";
+static constexpr auto XML_TAG_GAME_ACTION = "mGameAction";
+static constexpr auto XML_TAG_GAME_SPECIAL_INFO_SOURCES = "mGameSpecialInfoSources";
+
+static const std::unordered_map<std::string, AppType> appTypeMap = {
+    { XML_TAG_SECTION_HEADER_GAME_INFO, AppType::GAME_APP },
+    { XML_TAG_SECTION_HEADER_APP_WHITE_LIST, AppType::WHITE_LIST_APP },
+    { XML_TAG_SECTION_HEADER_APP_BLACK_LIST, AppType::BLACK_LIST_APP },
+    { XML_TAG_SECTION_HEADER_CHARIOT_APK, AppType::CHARIOT_APP },
+};
+
+static const std::unordered_map<std::string, GameAppInfoType> gameInfoTypeMap = {
+    { XML_TAG_GAME_ID, GameAppInfoType::GAME_ID },
+    { XML_TAG_SCENE_ID, GameAppInfoType::SCENE_ID },
+    { XML_TAG_GAME_KQI, GameAppInfoType::GAME_KQI },
+    { XML_TAG_GAME_RTT, GameAppInfoType::GAME_RTT },
+    { XML_TAG_GAME_ACTION, GameAppInfoType::GAME_ACTION },
+    { XML_TAG_GAME_SPECIAL_INFO_SOURCES, GameAppInfoType::GAME_SPACIAL_INFO_SOURCES },
+};
+
+AppParser::AppParser()
+{
+    WIFI_LOGI("%{public}s enter", __FUNCTION__);
+    InitAppParser();
+}
+
+AppParser::~AppParser()
+{
+    WIFI_LOGI("%{public}s enter", __FUNCTION__);
+}
+
+AppParser &AppParser::GetInstance()
+{
+    static AppParser instance;
+    return instance;
+}
+
+bool AppParser::IsGameApp(const std::string &bundleName) const
+{
+    return std::any_of(m_gameAppVec.begin(), m_gameAppVec.end(),
+        [bundleName](const GameAppInfo &app) { return app.gameName == bundleName; });
+}
+
+bool AppParser::IsWhiteListApp(const std::string &bundleName) const
+{
+    return std::any_of(m_whiteAppVec.begin(), m_whiteAppVec.end(),
+        [bundleName](const WhiteListAppInfo &app) { return app.packageName == bundleName; });
+}
+
+bool AppParser::IsBlackListApp(const std::string &bundleName) const
+{
+    return std::any_of(m_blackAppVec.begin(), m_blackAppVec.end(),
+        [bundleName](const BlackListAppInfo &app) { return app.packageName == bundleName; });
+}
+
+bool AppParser::IsChariotApp(const std::string &bundleName) const
+{
+    return std::any_of(m_chariotAppVec.begin(), m_chariotAppVec.end(),
+        [bundleName](const ChariotAppInfo &app) { return app.packageName == bundleName; });
+}
+
+void AppParser::InitAppParser()
+{
+    if (!std::filesystem::exists(WIFI_MONITOR_APP_FILE_PATH)) {
+        WIFI_LOGE("%{public}s %{public}s not exists", __FUNCTION__, WIFI_MONITOR_APP_FILE_PATH);
+        return;
+    }
+    bool ret = LoadConfiguration(WIFI_MONITOR_APP_FILE_PATH);
+    if (!ret) {
+        WIFI_LOGE("%{public}s load failed", __FUNCTION__);
+        return;
+    }
+    ret = Parse();
+    if (!ret) {
+        WIFI_LOGE("%{public}s parse failed", __FUNCTION__);
+        return;
+    }
+    WIFI_LOGD("%{public}s, wifi monitor app xml passed successfully", __FUNCTION__);
+}
+
+bool AppParser::ParserInternal(xmlNodePtr node)
+{
+    if (node == nullptr) {
+        WIFI_LOGE("%{public}s node is null", __FUNCTION__);
+        return false;
+    }
+    ParserAppList(node);
+    return false;
+}
+
+void AppParser::ParserAppList(const xmlNodePtr &innode)
+{
+    if (xmlStrcmp(innode->name, BAD_CAST(XML_TAG_SECTION_HEADER_MONITOR_APP)) != 0) {
+        WIFI_LOGE("innode name=%{public}s not equal MonitorAPP", innode->name);
+    }
+    for (xmlNodePtr node = innnode->children; node != nullptr; node = node->next) {
+        switch (GetAppTypeAsInt(node)) {
+            case AppType::GAME_APP:
+                m_gameAppVec.push_back(ParseGameAppInfo(node));
+                break;
+            case AppType::WHITE_LIST_APP:
+                m_whiteAppVec.push_back(ParseWhiteAppInfo(node));
+                break;
+            case AppType::BLACK_LIST_APP:
+                m_blackAppVec.push_back(ParseBlackAppInfo(node));
+                break;
+            case AppType::CHARIOT_APP:
+                m_chariotAppVec.push_back(ParseChariotAppInfo(node));
+
+            default:
+                WIFI_LOGD("app type: %{public}s is not monitored", GetNodeValue(node).c_str());
+                break;
+        }
+    }
+}
+
+GameAppInfo AppParser::ParseGameAppInfo(const xmlNodePtr &innode)
+{
+    GameAppInfo gameAppInfo;
+    std::string gameName =
+        std::string(reinterpret_cast<char *>(xmlGetProp(innode, BAD_CAST(XML_TAG_SECTION_KEY_GAME_NAME))));
+    if (gameName.empty()) {
+        WIFI_LOGW("%{public}s game name is empty", __FUNCTION__);
+    }
+    gameAppInfo.gameName = gameName;
+    for (xmlNodePtr node = innnode->children; node != nullptr; node = node->next) {
+        switch (GetGameAppInfoNameAsInt(node)) {
+            case GameAppInfoType::GAME_ID:
+                gameAppInfo.mGameId = GetStringValue(node);
+                break;
+            case GameAppInfoType::SCENE_ID:
+                gameAppInfo.mSceneId = GetStringValue(node);
+                break;
+            case GameAppInfoType::GAME_KQI:
+                gameAppInfo.mGameKQI = GetStringValue(node);
+                break;
+            case GameAppInfoType::GAME_RTT:
+                gameAppInfo.mGameRtt = GetStringValue(node);
+                break;
+            case GameAppInfoType::GAME_ACTION:
+                gameAppInfo.mGameAction = GetStringValue(node);
+                break;
+            case GameAppInfoType::GAME_SPACIAL_INFO_SOURCES:
+                gameAppInfo.mGameSpacialInfoSources = GetStringValue(node);
+                break;
+            default:
+                WIFI_LOGD("%{public}s invalid game app info: %{public}s", __FUNCTION__, GetStringValue(node).c_str());
+                break;
+        }
+    }
+    return gameAppInfo;
+}
+
+WhiteListAppInfo AppParser::ParseWhiteAppInfo(const xmlNodePtr &innode)
+{
+    WhiteListAppInfo appInfo;
+    appInfo.packageName =
+        std::string(reinterpret_cast<char *>(xmlGetProp(innode, BAD_CAST(XML_TAG_SECTION_KEY_PACKAGE_NAME))));
+    return appInfo;
+}
+
+BlackListAppInfo AppParser::ParseBlackAppInfo(const xmlNodePtr &innode)
+{
+    BlackListAppInfo appInfo;
+    appInfo.packageName =
+        std::string(reinterpret_cast<char *>(xmlGetProp(innode, BAD_CAST(XML_TAG_SECTION_KEY_PACKAGE_NAME))));
+    return appInfo;
+}
+
+ChariotAppInfo AppParser::ParseChariotAppInfo(const xmlNodePtr &innode)
+{
+    ChariotAppInfo appInfo;
+    appInfo.packageName =
+        std::string(reinterpret_cast<char *>(xmlGetProp(innode, BAD_CAST(XML_TAG_SECTION_KEY_PACKAGE_NAME))));
+    return appInfo;
+}
+
+GameAppInfoType AppParser::GetGameAppInfoNameAsInt(const xmlNodePtr &innode)
+{
+    std::string tagName = GetNodeValue(node);
+    if (gameInfoTypeMap.find(tagName) != gameInfoTypeMap.end()) {
+        return gameInfoTypeMap.at(tagName);
+    }
+    WIFI_LOGD("%{public} not find targName:%{public}s in gameInfoTypeMap", __FUNCTION__, tagName.c_str());
+    return GameAppInfoType::INVALID;
+}
+
+AppType AppParser::GetAppTypeAsInt(const xmlNodePtr &innode)
+{
+    std::string tagName = GetNodeValue(node);
+    if (appTypeMap.find(tagName) != appTypeMap.end()) {
+        return appTypeMap.at(tagName);
+    }
+    WIFI_LOGD("%{public} not find targName:%{public}s in appTypeMap", __FUNCTION__, tagName.c_str());
+    return AppType::OTHER_APP;
+}
+} // namespace Wifi
+} // namespace OHOS

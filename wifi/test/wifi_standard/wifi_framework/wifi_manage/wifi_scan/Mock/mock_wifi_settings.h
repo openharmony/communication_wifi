137181c359a1a4ee86378acfe061c27bb16701aa
sunjinqiang <17319862417@163.com>
2023-12-18 05:58:50 +0000

    update wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_scan/Mock/mock_wifi_settings.h.
    Signed-off-by: sunjinqiang <17319862417@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_scan/Mock/mock_wifi_settings.h
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_scan/Mock/mock_wifi_settings.h
@@ -85,7 +85,7 @@ public:
     MOCK_METHOD0(ClearScanInfoList, int());
     MOCK_METHOD1(SetAppPackageName, void(const std::string &name));
     MOCK_METHOD1(SetP2pBusinessType, int(const P2pBusinessType &type));
-    MOCK_METHOD1(GetHid2dUpperScene, int(std::string& ifName, Hid2dUpperScene &scene));
+    MOCK_METHOD2(GetHid2dUpperScene, int(std::string& ifName, Hid2dUpperScene &scene));
     MOCK_METHOD1(GetP2pBusinessType, int(P2pBusinessType &type));
 };
 }  // namespace Wifi

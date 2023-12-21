da33b69843643d7c751f76c8f52a86969d5cc6b8
sunjingqiang <17319862417@163.com>
2023-12-21 15:37:18 +0800

    tdd
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/test/wifi_testapp/hvigorfile.ts
+++ wifi/test/wifi_testapp/hvigorfile.ts
@@ -1,2 +1,6 @@
-// Script for compiling build behavior. It is built in the build plug-in and cannot be modified currently.
+/*
+ * Copyright (C) Huawei Technologies Co., Ltd.2023-2023.All rights reserved.
+ * Description : Script for compiling build behavior. It is built in the build
+ * plug-in and cannot be modified currently.
+*/
 export { appTasks } from '@ohos/hvigor-ohos-plugin';

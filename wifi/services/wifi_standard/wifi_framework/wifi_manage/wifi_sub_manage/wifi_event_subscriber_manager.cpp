e97011d0112f6eebcab9777b574b2ba38292d59b
yanxiaotao <yanxiaotao@huawei.com>
2023-12-25 17:14:35 +0000

    reset subscribered flag when UnSubscribeCommonEvent failed.
    Signed-off-by: yanxiaotao <yanxiaotao@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_event_subscriber_manager.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_event_subscriber_manager.cpp
@@ -413,8 +413,8 @@ void WifiEventSubscriberManager::UnRegisterAirplaneModeEvent()
         WIFI_LOGE("AirplaneModeEvent UnSubscribeCommonEvent() failed");
     } else {
         WIFI_LOGI("AirplaneModeEvent UnSubscribeCommonEvent() OK");
-        isAirplaneModeEventSubscribered = false;
     }
+    isAirplaneModeEventSubscribered = false;
 }
 
 void WifiEventSubscriberManager::RegisterLocationEvent()
@@ -642,9 +642,9 @@ void WifiEventSubscriberManager::UnRegisterPowerStateListener()
     if (!ret) {
         WIFI_LOGE("UnRegisterPowerStateListener, unregister power state callback failed");
     } else {
-        isPowerStateListenerSubscribered = false;
         WIFI_LOGI("UnRegisterPowerStateListener OK!");
     }
+    isPowerStateListenerSubscribered = false;
 }
 #endif
 

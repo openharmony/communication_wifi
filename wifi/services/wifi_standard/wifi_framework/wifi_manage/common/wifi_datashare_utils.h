0567e9523469db91fbafd4898ed698a59af64ff2
yangpengfei <yangpengfei34@huawei.com>
2023-12-27 07:39:58 +0000

    update
    Signed-off-by: yangpengfei <yangpengfei34@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/common/wifi_datashare_utils.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/common/wifi_datashare_utils.h
@@ -38,8 +38,6 @@ constexpr const char *SETTINGS_DATASHARE_KEY_AIRPLANE_MODE = "settings.telephony
 constexpr const char *SETTINGS_DATASHARE_KEY_LOCATION_MODE = "location_switch_enable";
 
 #ifndef OHOS_ARCH_LITE
-constexpr const char *SETTINGS_DATASHARE_URI_SETTINGS_MIGRATE =
-    "datashare:///com.ohos.settingsdata/entry/settingsdata/SETTINGSDATA?Proxy=true&key=settings_migrate_success";
 constexpr const char *SETTINGS_DATASHARE_URI_WIFI_ON =
     "datashare:///com.ohos.settingsdata/entry/settingsdata/SETTINGSDATA?Proxy=true&key=wifi_on";
 constexpr const char *SETTINGS_DATASHARE_KEY_WIFI_ON = "wifi_on";

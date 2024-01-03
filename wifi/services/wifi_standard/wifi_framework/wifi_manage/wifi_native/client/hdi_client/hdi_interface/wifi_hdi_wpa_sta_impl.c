fb4ee7dd1a249257198befe07cd95168b59f2bc8
likuanxin <likuanxin@huawei.com>
2024-01-03 10:27:19 +0800

    解决扫描管控问题
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/hdi_interface/wifi_hdi_wpa_sta_impl.c
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/hdi_interface/wifi_hdi_wpa_sta_impl.c
@@ -79,7 +79,6 @@ static WifiErrorNo RegisterEventCallback()
     if (result != HDF_SUCCESS) {
         pthread_mutex_unlock(&g_hdiCallbackMutex);
         LOGE("RegisterEventCallback: RegisterEventCallback failed result:%{public}d", result);
-        HdiWpaResetGlobalObj(result);
         return WIFI_IDL_OPT_FAILED;
     }
 

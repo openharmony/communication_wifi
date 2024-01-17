a6210c49763b844f205c52b4de08ba2f727ab8f8
huxiaomin <huxiaomin2@huawei.com>
2024-01-17 11:40:55 +0800

    dns添加运行状态检测
    Signed-off-by: huxiaomin <huxiaomin2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/net_helper/sta_network_check.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/net_helper/sta_network_check.cpp
@@ -194,7 +194,7 @@ int StaNetworkCheck::HttpPortalDetection(const std::string &url) __attribute__((
         return -1;
     }
 
-    task->OnSuccess([task, this](const NetStack::HttpClient::HttpClientRequest &request,
+    task->OnSuccess([this](const NetStack::HttpClient::HttpClientRequest &request,
         const NetStack::HttpClient::HttpClientResponse &response) {
         std::string url = request.GetURL();
         int codeNum = response.GetResponseCode();
@@ -352,6 +352,7 @@ ErrCode StaNetworkCheck::InitNetCheckThread()
 void StaNetworkCheck::StopNetCheckThread()
 {
     WIFI_LOGI("enter StopNetCheckThread!\n");
+    dnsChecker.StopDnsCheck();
     isStopNetCheck = true;
 }
 

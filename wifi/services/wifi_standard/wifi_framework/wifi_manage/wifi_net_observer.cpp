33873b647c36094f6d9ecb26ce19700dbed6ad47
huxiaomin <huxiaomin2@huawei.com>
2024-01-26 09:30:57 +0800

    监听网络管理的探测结果
    Signed-off-by: huxiaomin <huxiaomin2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_net_observer.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_net_observer.cpp
@@ -26,7 +26,6 @@ DEFINE_WIFILOG_LABEL("WifiNetObserver");
 namespace OHOS {
 namespace Wifi {
 using namespace NetManagerStandard;
-constexpr int32_t RETRY_MAX_TIMES = 10;
 
 NetStateObserver &NetStateObserver::GetInstance()
 {
@@ -44,108 +43,97 @@ NetStateObserver::~NetStateObserver()
     WIFI_LOGD("~NetStateObserver");
 }
 
-void NetStateObserver::SetNetStateCallback(std::function<void(SystemNetWorkState)> callback)
+void NetStateObserver::SetNetStateCallback(std::function<void(SystemNetWorkState, std::string)> callback)
 {
     m_Callback = callback;
 }
 
 void NetStateObserver::StartNetStateObserver()
 {
-    WIFI_LOGI("StartNetObserver");
-    std::thread th = std::thread([this]() {
-        NetManagerStandard::NetSpecifier netSpecifier;
-        NetManagerStandard::NetAllCapabilities netAllCapabilities;
-        netAllCapabilities.netCaps_.insert(NetManagerStandard::NetCap::NET_CAPABILITY_INTERNET);
-        netSpecifier.ident_ = "";
-        netSpecifier.netCapabilities_ = netAllCapabilities;
-        int32_t retryCount = 0;
-        int32_t ret = 0;
-        do {
-            ret = NetManagerStandard::NetConnClient::GetInstance().RegisterNetConnCallback(this);
-            if (ret == 0) {
-                WIFI_LOGI("StartNetObserver register success");
-                return;
-            }
-            retryCount++;
-            WIFI_LOGI("StartNetObserver retry, ret = %{public}d", ret);
-            sleep(1);
-        } while (retryCount < RETRY_MAX_TIMES);
-        WIFI_LOGI("StartNetObserver failed");
-    });
-    th.detach();
+    int32_t ret = 0;
+    int32_t netId = GetWifiNetId();
+    WIFI_LOGI("StartNetObserver netId:%{public}d", netId);
+    ret = NetManagerStandard::NetConnClient::GetInstance().RegisterNetDetectionCallback(netId, this);
+    if (ret == 0) {
+        WIFI_LOGI("StartNetObserver register success");
+        return;
+    }
+    WIFI_LOGI("StartNetObserver failed ret = %{public}d", ret);
 }
 
 void NetStateObserver::StopNetStateObserver()
 {
-    WIFI_LOGI("StopNetObserver");
     int32_t ret = 0;
-    ret = NetManagerStandard::NetConnClient::GetInstance().UnregisterNetConnCallback(this);
+    int32_t netId = GetWifiNetId();
+    WIFI_LOGI("StopNetObserver netId:%{public}d", netId);
+    ret = NetManagerStandard::NetConnClient::GetInstance().UnRegisterNetDetectionCallback(netId, this);
     if (ret == 0) {
         WIFI_LOGI("StopNetObserver unregister success");
         return;
     }
-    WIFI_LOGE("StopNetObserver unregister failed!");
+    WIFI_LOGI("StopNetObserver failed ret = %{public}d", ret);
 }
 
-int32_t NetStateObserver::NetCapabilitiesChange(sptr<NetManagerStandard::NetHandle> &netHandle,
-    const sptr<NetManagerStandard::NetAllCapabilities> &netAllCap)
+int32_t NetStateObserver::OnNetDetectionResultChanged(
+    NetManagerStandard::NetDetectionResultCode detectionResult, const std::string &urlRedirect)
 {
-    if (netAllCap == nullptr) {
-        return 0;
-    }
-
-    for (auto info : netAllCap->netCaps_) {
-        WIFI_LOGI("netAllCap: %{public}d", info);
-    }
-
-    if (netAllCap->bearerTypes_.count(NetManagerStandard::BEARER_CELLULAR) > 0) {
-        WIFI_LOGI("NetCapabilitiesChange NetBearType BEARER_CELLULAR");
-        if (m_Callback != nullptr) {
-            m_Callback(NETWORK_CELL_WORKING);
+    WIFI_LOGI("OnNetDetectionResultChanged nettype:%{public}d, url:%{public}s", detectionResult, urlRedirect.c_str());
+    switch (detectionResult) {
+        case NetManagerStandard::NET_DETECTION_CAPTIVE_PORTAL: {
+            m_Callback(NETWORK_IS_PORTAL, urlRedirect);
+            break;
         }
-    } else {
-        WIFI_LOGI("NetCapabilitiesChange NetBearType NO CELL");
-        if (m_Callback != nullptr) {
-            m_Callback(NETWORK_CELL_NOWORK);
+        case NetManagerStandard::NET_DETECTION_FAIL: {
+            m_Callback(NETWORK_NOTWORKING, nullptr);
+            break;
+        }
+        case NetManagerStandard::NET_DETECTION_SUCCESS: {
+            m_Callback(NETWORK_IS_WORKING, nullptr);
+            break;
         }
     }
-
     return 0;
 }
 
-SystemNetWorkState NetStateObserver::GetCellNetState()
-{
-    NetHandle netHandle;
-    NetManagerStandard::NetAllCapabilities netAllCap;
-    int32_t result = NetConnClient::GetInstance().GetDefaultNet(netHandle);
-    if (result != NETMANAGER_SUCCESS) {
-        WIFI_LOGE("GetCellNetState GetDefaultNet failed!");
-        return NETWORK_UNKNOWN;
-    }
-    NetConnClient::GetInstance().GetNetCapabilities(netHandle, netAllCap);
-    
-    if (netAllCap.bearerTypes_.count(NetManagerStandard::BEARER_CELLULAR) > 0) {
-        WIFI_LOGI("GetCellNetState is cell working");
-        return NETWORK_CELL_WORKING;
-    }
-    WIFI_LOGI("GetCellNetState is cell no work");
-    return NETWORK_CELL_NOWORK;
-}
-
-int32_t NetStateObserver::GetWifiNetId()
+sptr<NetHandle> NetStateObserver::GetWifiNetworkHandle()
 {
     std::list<sptr<NetHandle>> netList;
     int32_t ret = NetConnClient::GetInstance().GetAllNets(netList);
     if (ret != NETMANAGER_SUCCESS) {
-        WIFI_LOGE("GetAllNets failed!");
-        return 0;
+        WIFI_LOGE("GetAllNets failed ret = %{public}d", ret);
+        return nullptr;
     }
     for (auto iter : netList) {
         NetManagerStandard::NetAllCapabilities netAllCap;
         NetConnClient::GetInstance().GetNetCapabilities(*iter, netAllCap);
         if (netAllCap.bearerTypes_.count(NetManagerStandard::BEARER_WIFI) > 0) {
-            return iter->GetNetId();
+            return iter;
         }
+    }
+    WIFI_LOGE("GetWifiNetworkHandle not find wifi network");
+    return nullptr;
+}
+ 
+int32_t NetStateObserver::StartWifiDetection()
+{
+    sptr<NetHandle> netHandle = GetWifiNetworkHandle();
+    if (netHandle == nullptr) {
+        WIFI_LOGE("StartWifiDetection failed!");
+        return 1;
+    }
+    int32_t res = NetConnClient::GetInstance().NetDetection(*netHandle);
+    if (res != 0) {
+        WIFI_LOGE("StartWifiDetection failed %{public}d", res);
+        return 1;
+    }
+    return 0;
+}
+
+int32_t NetStateObserver::GetWifiNetId()
+{
+    sptr<NetHandle> netHandle = GetWifiNetworkHandle();
+    if (netHandle != nullptr) {
+        return netHandle->GetNetId();
     }
     return 0;
 }
--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_define.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_define.h
@@ -103,9 +103,9 @@ typedef enum EnumStaDnsState {
 } StaDnsState;
 
 typedef enum EnumNetWorkState {
-    NETWORK_UNKNOWN,
-    NETWORK_CELL_WORKING,
-    NETWORK_CELL_NOWORK,
+    NETWORK_NOTWORKING,
+    NETWORK_IS_WORKING,
+    NETWORK_IS_PORTAL,
 }SystemNetWorkState;
 
 using NetStateHandler = std::function<void(StaNetState netState, std::string portalUrl)>;

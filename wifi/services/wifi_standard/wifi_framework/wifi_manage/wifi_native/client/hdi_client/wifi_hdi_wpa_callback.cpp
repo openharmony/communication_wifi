f22ddceb134ef21d95d8748e67bbd1c4d5e674d4
WangYusen <wangyusen8@huawei.com>
2024-02-19 14:29:16 +0800

    debug softAp hdi interface
    Signed-off-by: WangYusen <wangyusen8@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/wifi_hdi_wpa_callback.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/wifi_hdi_wpa_callback.cpp
@@ -220,9 +220,17 @@ int32_t onEventApState(struct IHostapdCallback *self, const struct HdiApCbParm *
     WifiIdlEvent event;
     if (strncmp(apCbParm->content, "AP-ENABLED", strlen("AP-ENABLED")) == 0) {
         event = WIFI_IDL_CBK_CMD_AP_ENABLE;
-    } else if (strncmp(apCbParm->content, "AP-DISABLED", strlen("AP-DISABLED")) == 0 ||
-               strncmp(apCbParm->content, "CRTL-EVENT-TERMINATING", strlen("CRTL-EVENT-TERMINATING")) == 0) {
+    } else if (strncmp(apCbParm->content, "AP-DISABLE", strlen("AP-DISABLE")) == 0) {
         event = WIFI_IDL_CBK_CMD_AP_DISABLE;
+        if (GetExecDisable() == EXEC_DISABLE) {
+            SetExecDisable(0);
+            return 0;
+        }
+    } else if (strncmp(apCbParm->content, "CTRL-EVENT-TERMINATING", strlen("CTRL-EVENT-TERMINATING")) == 0) {
+        event = WIFI_IDL_CBK_CMD_AP_DISABLE;
+    } else if (strncmp(apCbParm->content, "AP-STA-POSSIBLE-PSK-MISMATCH ",
+        strlen("AP-STA-POSSIBLE-PSK-MISMATCH ")) == 0) {
+        event = WIFI_IDL_CBK_CMD_AP_STA_PSK_MISMATCH_EVENT;
     } else {
         return 1;
     }

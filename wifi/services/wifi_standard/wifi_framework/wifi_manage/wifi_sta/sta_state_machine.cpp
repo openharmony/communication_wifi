b7c9562098481464169a79ae8f3422ace9947c93
CoderAdo <duliqun1@h-partners.com>
2024-03-12 02:33:18 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp.
    Signed-off-by: CoderAdo <duliqun1@h-partners.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
@@ -2848,6 +2848,24 @@ void StaStateMachine::DhcpResultNotify::SaveDhcpResult(DhcpResult *dest, DhcpRes
         return;
     }
     LOGI("SaveDhcpResult ok, ipType:%{public}d", dest->iptype);
+    StaStateMachine::DhcpResultNotify::SaveDhcpResultExt(dest, source);
+}
+
+void StaStateMachine::DhcpResultNotify::SaveDhcpResultExt(DhcpResult *dest, DhcpResult *source)
+{
+    if (dest == nullptr || source == nullptr) {
+        LOGE("SaveDhcpResultExt dest or source is nullptr.");
+        return;
+    }
+    if (strcpy_s(dest->strOptLinkIpv6Addr, DHCP_MAX_FILE_BYTES, source->strOptLinkIpv6Addr) != EOK) {
+        LOGE("SaveDhcpResultExt strOptLinkIpv6Addr strcpy_s failed!");
+        return;
+    }
+    if (strcpy_s(dest->strOptRandIpv6Addr, DHCP_MAX_FILE_BYTES, source->strOptRandIpv6Addr) != EOK) {
+        LOGE("SaveDhcpResultExt strOptRandIpv6Addr strcpy_s failed!");
+        return;
+    }
+    LOGI("SaveDhcpResultExt ok, ipType:%{public}d", dest->iptype);
 }
 
 /* ------------------ state machine dhcp callback function ----------------- */

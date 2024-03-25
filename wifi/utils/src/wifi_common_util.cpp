d5c5a600f87f200139e4f3c986603bc92a3ae319
fengye <fengye10@huawei.com>
2024-03-25 09:11:04 +0000

    ::::wifi ffrt
    Signed-off-by: fengye <fengye10@huawei.com>

--- wifi/utils/src/wifi_common_util.cpp
+++ wifi/utils/src/wifi_common_util.cpp
@@ -322,57 +322,6 @@ TimeStats::~TimeStats()
     WIFI_LOGI("[Time stats][end] %{public}s, time cost:%{public}lldus, %{public}lldms, %{public}llds",
         m_desc.c_str(), us, us / TIME_BASE, us / TIME_BASE / TIME_BASE);
 }
-
-WifiTimer *WifiTimer::GetInstance()
-{
-    static WifiTimer instance;
-    return &instance;
-}
-
-WifiTimer::WifiTimer() : timer_(std::make_unique<Utils::Timer>("WifiManagerTimer"))
-{
-    timer_->Setup();
-}
-
-WifiTimer::~WifiTimer()
-{
-    if (timer_) {
-        timer_->Shutdown(true);
-    }
-}
-
-ErrCode WifiTimer::Register(const TimerCallback &callback, uint32_t &outTimerId, uint32_t interval, bool once)
-{
-    if (timer_ == nullptr) {
-        WIFI_LOGE("timer_ is nullptr");
-        return WIFI_OPT_FAILED;
-    }
-
-    uint32_t ret = timer_->Register(callback, interval, once);
-    if (ret == Utils::TIMER_ERR_DEAL_FAILED) {
-        WIFI_LOGE("Register timer failed");
-        return WIFI_OPT_FAILED;
-    }
-
-    outTimerId = ret;
-    return WIFI_OPT_SUCCESS;
-}
-
-void WifiTimer::UnRegister(uint32_t timerId)
-{
-    if (timerId == 0) {
-        WIFI_LOGE("timerId is 0, no register timer");
-        return;
-    }
-
-    if (timer_ == nullptr) {
-        WIFI_LOGE("timer_ is nullptr");
-        return;
-    }
-
-    timer_->Unregister(timerId);
-    return;
-}
 #endif
 
 int FrequencyToChannel(int freq)

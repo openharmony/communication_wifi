82efe8a60430098edbfb6b7be4410b29a7b67b97
sunjingqiang <17319862417@163.com>
2024-03-29 18:51:54 +0800

    64位编译器问题处理
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_auth_center.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_auth_center.cpp
@@ -47,6 +47,9 @@ int WifiAuthCenter::Init()
 
 #ifndef OHOS_ARCH_LITE
 bool WifiAuthCenter::IsSystemAppByToken() {
+#ifdef DTFUZZ_TEST
+    return true;
+#endif
     uint64_t fullTokenId = IPCSkeleton::GetCallingFullTokenID();
     bool isSystemApp = Security::AccessToken::TokenIdKit::IsSystemAppByFullTokenID(fullTokenId);
     uint32_t tokenId = IPCSkeleton::GetCallingTokenID();
@@ -62,6 +65,9 @@ bool WifiAuthCenter::IsSystemAppByToken() {
 }
 bool WifiAuthCenter::IsNativeProcess()
 {
+#ifdef DTFUZZ_TEST
+    return true;
+#endif
     uint32_t tokenId = IPCSkeleton::GetCallingTokenID();
     Security::AccessToken::ATokenTypeEnum callingType =
         Security::AccessToken::AccessTokenKit::GetTokenTypeFlag(tokenId);

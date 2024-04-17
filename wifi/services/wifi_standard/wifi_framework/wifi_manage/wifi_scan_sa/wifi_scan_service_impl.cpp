e02a9eefcc414ddb57a57e0e97b164fb79354194
xiaosi <xiaosi2@huawei.com>
2024-04-17 16:05:21 +0800

    inner api permission
    Signed-off-by: xiaosi <xiaosi2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan_sa/wifi_scan_service_impl.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan_sa/wifi_scan_service_impl.cpp
@@ -89,6 +89,10 @@ WifiScanServiceImpl::~WifiScanServiceImpl()
 ErrCode WifiScanServiceImpl::SetScanControlInfo(const ScanControlInfo &info)
 {
     WIFI_LOGI("WifiScanServiceImpl::SetScanControlInfo");
+    if (WifiPermissionUtils::VerifyGetWifiInfoInternalPermission() == PERMISSION_DENIED) {
+        WIFI_LOGE("SetScanControlInfo:VerifyGetWifiInfoInternalPermission PERMISSION_DENIED!");
+        return WIFI_OPT_PERMISSION_DENIED;
+    }
     if (WifiPermissionUtils::VerifySetWifiInfoPermission() == PERMISSION_DENIED) {
         WIFI_LOGE("SetScanControlInfo:VerifySetWifiInfoPermission PERMISSION_DENIED!");
         return WIFI_OPT_PERMISSION_DENIED;

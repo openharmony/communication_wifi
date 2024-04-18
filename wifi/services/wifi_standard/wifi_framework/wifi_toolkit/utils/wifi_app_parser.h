967d6c2305c253405bf1d8aa82ca589413dd7c50
likuanxin <likuanxin@huawei.com>
2024-04-18 08:01:07 +0000

    wifi_app_parser.h.
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_app_parser.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_app_parser.h
@@ -4,7 +4,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -62,14 +62,20 @@ private:
     ChariotAppInfo ParseChariotAppInfo(const xmlNodePtr &innode);
     HighTempLimitSpeedAppInfo ParseHighTempLimitSpeedAppInfo(const xmlNodePtr &innode);
     AppType GetAppTypeAsInt(const xmlNodePtr &innode);
-
+    bool ReadPackageCloudFilterConfig();
+    bool IsReadCloudConfig();
+    std::string GetLocalFileVersion(const char *appXmlVersionFilePath);
+    std::string GetCloudPushFileVersion(const char *appVersionFilePath);
+    std::string GetCloudPushVersionFilePath();
+    std::string GetCloudPushJsonFilePath();
 private:
     std::vector<LowLatencyAppInfo> m_lowLatencyAppVec {};
     std::vector<WhiteListAppInfo> m_whiteAppVec {};
     std::vector<BlackListAppInfo> m_blackAppVec {};
     std::vector<ChariotAppInfo> m_chariotAppVec {};
     std::vector<HighTempLimitSpeedAppInfo> m_highTempLimitSpeedAppVec {};
-    
+    std::vector<HighTempLimitSpeedAppInfo> m_highTempLimitSpeedAppVecCloudPush {};
+    bool mIshighTempLimitSpeedReadCloudPush = false;
 };
 } // namespace Wifi
 } // namespace OHOS

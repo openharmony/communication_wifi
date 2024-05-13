506547ebe2ad375f00eff03360f59490e189e71f
WangYusen <wangyusen8@huawei.com>
2024-05-13 18:19:54 +0800

    p2p upper layer exception process
    Signed-off-by: WangYusen <wangyusen8@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p_sa/wifi_p2p_service_impl.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p_sa/wifi_p2p_service_impl.cpp
@@ -412,7 +412,8 @@ ErrCode WifiP2pServiceImpl::StopP2pListen()
 
 ErrCode WifiP2pServiceImpl::CreateGroup(const WifiP2pConfig &config)
 {
-    WIFI_LOGI("CreateGroup, network name is [%{public}s]", config.GetGroupName().c_str());
+    int callingUid = GetCallingUid();
+    WIFI_LOGI("Uid %{public}d createGroup, network name is [%{public}s]", callingUid, config.GetGroupName().c_str());
     if (WifiPermissionUtils::VerifyGetWifiInfoPermission() == PERMISSION_DENIED) {
         WIFI_LOGE("CreateGroup:VerifyGetWifiInfoPermission PERMISSION_DENIED!");
         return WIFI_OPT_PERMISSION_DENIED;
@@ -438,6 +439,7 @@ ErrCode WifiP2pServiceImpl::CreateGroup(const WifiP2pConfig &config)
         WIFI_LOGE("Get P2P service failed!");
         return WIFI_OPT_P2P_NOT_OPENED;
     }
+    pService->IncreaseSharedLink(callingUid);
     return pService->CreateGroup(config);
 }
 
@@ -1050,7 +1052,8 @@ ErrCode WifiP2pServiceImpl::Hid2dRequestGcIp(const std::string& gcMac, std::stri
 
 ErrCode WifiP2pServiceImpl::Hid2dSharedlinkIncrease()
 {
-    WIFI_LOGI("Hid2dSharedlinkIncrease");
+    int callingUid = GetCallingUid();
+    WIFI_LOGI("Uid %{public}d Hid2dSharedlinkIncrease", callingUid);
     if (!WifiAuthCenter::IsNativeProcess()) {
         WIFI_LOGE("Hid2dSharedlinkIncrease:NOT NATIVE PROCESS, PERMISSION_DENIED!");
         return WIFI_OPT_PERMISSION_DENIED;
@@ -1070,13 +1073,14 @@ ErrCode WifiP2pServiceImpl::Hid2dSharedlinkIncrease()
         WIFI_LOGE("Get P2P service failed!");
         return WIFI_OPT_FAILED;
     }
-    pService->IncreaseSharedLink();
+    pService->IncreaseSharedLink(callingUid);
     return WIFI_OPT_SUCCESS;
 }
 
 ErrCode WifiP2pServiceImpl::Hid2dSharedlinkDecrease()
 {
-    WIFI_LOGI("Hid2dSharedlinkDecrease");
+    int callingUid = GetCallingUid();
+    WIFI_LOGI("Uid %{public}d Hid2dSharedlinkDecrease", callingUid);
     if (!WifiAuthCenter::IsNativeProcess()) {
         WIFI_LOGE("Hid2dSharedlinkDecrease:NOT NATIVE PROCESS, PERMISSION_DENIED!");
         return WIFI_OPT_PERMISSION_DENIED;
@@ -1090,9 +1094,7 @@ ErrCode WifiP2pServiceImpl::Hid2dSharedlinkDecrease()
         WIFI_LOGE("Get P2P service failed!");
         return WIFI_OPT_P2P_NOT_OPENED;
     }
-    if (pService->GetSharedLinkCount() > 0) {
-        pService->DecreaseSharedLink();
-    }
+    pService->DecreaseSharedLink(callingUid);
     if (pService->GetSharedLinkCount() == 0) {
         WIFI_LOGI("Shared link count == 0, remove group!");
         RemoveGroup();
@@ -1102,7 +1104,8 @@ ErrCode WifiP2pServiceImpl::Hid2dSharedlinkDecrease()
 
 ErrCode WifiP2pServiceImpl::Hid2dCreateGroup(const int frequency, FreqType type)
 {
-    WIFI_LOGI("Hid2dCreateGroup");
+    int callingUid = GetCallingUid();
+    WIFI_LOGI("Uid %{public}d Hid2dCreateGroup", callingUid);
     if (WifiPermissionUtils::VerifyGetWifiInfoPermission() == PERMISSION_DENIED) {
         WIFI_LOGE("CreateGroup:VerifyGetWifiInfoPermission PERMISSION_DENIED!");
         return WIFI_OPT_PERMISSION_DENIED;
@@ -1119,6 +1122,7 @@ ErrCode WifiP2pServiceImpl::Hid2dCreateGroup(const int frequency, FreqType type)
         return WIFI_OPT_P2P_NOT_OPENED;
     }
     WifiSettings::GetInstance().SetP2pBusinessType(P2pBusinessType::P2P_TYPE_HID2D);
+    pService->IncreaseSharedLink(callingUid);
     return pService->Hid2dCreateGroup(frequency, type);
 }
 

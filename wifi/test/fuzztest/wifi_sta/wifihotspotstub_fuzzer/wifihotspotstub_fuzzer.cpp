d4b25ab08d8bc4dec49be10daa05ffdf48d18542
sunjingqiang <17319862417@163.com>
2024-05-22 21:53:43 +0800

    fuzz crash 修改
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/test/fuzztest/wifi_sta/wifihotspotstub_fuzzer/wifihotspotstub_fuzzer.cpp
+++ wifi/test/fuzztest/wifi_sta/wifihotspotstub_fuzzer/wifihotspotstub_fuzzer.cpp
@@ -49,7 +49,7 @@ bool Init()
 {
     if (!g_isInsted) {
         if (WifiManager::GetInstance().Init() < 0) {
-            WIFI_LOGE("WifiManager init failed!");
+            LOGE("WifiManager init failed!");
             return false;
         }
         g_isInsted = true;

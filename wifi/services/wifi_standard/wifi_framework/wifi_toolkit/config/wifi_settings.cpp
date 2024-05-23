fda3db008b347f8bb9bb695017a2d54a89e687ce
张鹏飞 <zhangpengfei123@huawei.com>
2024-05-23 01:54:38 +0000

    海外用户判断 海外用户判断
    Signed-off-by: 张鹏飞 <zhangpengfei123@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
@@ -27,6 +27,7 @@
 #include "wifi_global_func.h"
 #include "wifi_log.h"
 #include "wifi_config_country_freqs.h"
+#include "param_manager.h"
 #include <random>
 #ifdef FEATURE_ENCRYPTION_SUPPORT
 #include "wifi_encryption_util.h"

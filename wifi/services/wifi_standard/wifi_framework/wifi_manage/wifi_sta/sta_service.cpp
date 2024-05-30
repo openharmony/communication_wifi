b2a25d4859a46e29733f1e3ab3879c87941ab775
huxiaomin <huxiaomin2@huawei.com>
2024-05-30 10:03:12 +0800

    修改网络探测时机
    Signed-off-by: huxiaomin <huxiaomin2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp
@@ -796,6 +796,7 @@ void StaService::HandleScreenStatusChanged(int screenState)
         WIFI_LOGE("pStaStateMachine is null!");
         return;
     }
+    pStaStateMachine->SendMessage(WIFI_SCREEN_STATE_CHANGED_NOTIFY_EVENT, screenState);
     if (screenState == MODE_STATE_OPEN) {
         pStaStateMachine->StartDetectTimer(DETECT_TYPE_DEFAULT);
     } else {
@@ -804,7 +805,6 @@ void StaService::HandleScreenStatusChanged(int screenState)
     if (pStaAppAcceleration != nullptr) {
         pStaAppAcceleration->HandleScreenStatusChanged(screenState);
     }
-    pStaStateMachine->SendMessage(WIFI_SCREEN_STATE_CHANGED_NOTIFY_EVENT, screenState);
 #endif
     return;
 }

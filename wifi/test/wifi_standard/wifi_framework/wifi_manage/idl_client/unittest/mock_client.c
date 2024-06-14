efde648bd72ebb3aba9c70e4a0bd8a23822103b1
likuanxin <likuanxin@huawei.com>
2024-06-14 09:25:02 +0800

    解决tdd crash
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/mock_client.c
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/idl_client/unittest/mock_client.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2024-2024. All rights reserved.
+ * Copyright (C) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at

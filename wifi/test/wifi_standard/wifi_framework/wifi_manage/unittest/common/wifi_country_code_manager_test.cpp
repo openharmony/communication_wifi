4c69f7bf8ac61f2ae566c687cf1c18b28a519e06
likuanxin <likuanxin@huawei.com>
2024-06-18 08:02:52 +0000

    wifi_country_code_manager_test.cpp.
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/common/wifi_country_code_manager_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/common/wifi_country_code_manager_test.cpp
@@ -149,6 +149,8 @@ HWTEST_F(WifiCountryCodeManagerTest, UnregisterWifiCountryCodeChangeListenerFail
 HWTEST_F(WifiCountryCodeManagerTest, DealStaOpenResTest, TestSize.Level1)
 {
     WIFI_LOGI("DealStaOpenResTest enter");
+    EXPECT_CALL(WifiSettings::GetInstance(), SetWifiStateOnAirplaneChanged(_)).WillRepeatedly(Return(1));
+    EXPECT_CALL(WifiSettings::GetInstance(), SetLastAirplaneMode(_, _)).WillRepeatedly(Return(1));
     StaServiceCallback cbk = WifiCountryCodeManager::GetInstance().GetStaCallback();
     sleep(1);
     ASSERT_TRUE(cbk.OnStaOpenRes != nullptr);
@@ -158,7 +160,6 @@ HWTEST_F(WifiCountryCodeManagerTest, DealStaOpenResTest, TestSize.Level1)
     sleep(1);
     cbk.OnStaOpenRes(OperateResState::OPEN_WIFI_SUCCEED, 0);
     sleep(1);
-    WifiSettings::GetInstance().SetWifiStateOnAirplaneChanged(1);
     cbk.OnStaOpenRes(OperateResState::OPEN_WIFI_SUCCEED, 0);
     sleep(1);
     cbk.OnStaOpenRes(OperateResState::OPEN_WIFI_DISABLED, 0);
@@ -168,6 +169,7 @@ HWTEST_F(WifiCountryCodeManagerTest, DealStaOpenResTest, TestSize.Level1)
 HWTEST_F(WifiCountryCodeManagerTest, DealStaCloseResTest, TestSize.Level1)
 {
     WIFI_LOGI("DealStaCloseResTest enter");
+    EXPECT_CALL(WifiSettings::GetInstance(), SetWifiStateOnAirplaneChanged(_)).WillRepeatedly(Return(1));
     StaServiceCallback cbk = WifiCountryCodeManager::GetInstance().GetStaCallback();
     sleep(1);
     ASSERT_TRUE(cbk.OnStaOpenRes != nullptr);
@@ -175,8 +177,6 @@ HWTEST_F(WifiCountryCodeManagerTest, DealStaCloseResTest, TestSize.Level1)
     cbk.OnStaOpenRes(OperateResState::OPEN_WIFI_SUCCEED, 0);
     cbk.OnStaCloseRes(OperateResState::CLOSE_WIFI_CLOSING, 0);
     cbk.OnStaCloseRes(OperateResState::CLOSE_WIFI_SUCCEED, 0);
-    WifiSettings::GetInstance().SetWifiStateOnAirplaneChanged(1);
-    WifiConfigCenter::GetInstance().SetP2pMidState(WifiOprMidState::RUNNING);
     cbk.OnStaCloseRes(OperateResState::CLOSE_WIFI_SUCCEED, 0);
     sleep(2);
 }

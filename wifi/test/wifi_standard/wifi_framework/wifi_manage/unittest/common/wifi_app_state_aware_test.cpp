1cdd2df198d36eb6b6e1b405d33dc08b95259e4e
wujun <wujun156@huawei.com>
2024-06-26 09:39:33 +0800

    add
    Signed-off-by: wujun <wujun156@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/common/wifi_app_state_aware_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/common/wifi_app_state_aware_test.cpp
@@ -77,3 +77,103 @@ HWTEST_F(WifiAppStateAwareTest, OnForegroundAppChanged_CallsOnForegroundAppChang
     appStateData->uid = -1;
     callbacks_->OnForegroundAppChanged(*appStateData, 0);
 }
+
+HWTEST_F(WifiAppStateAwareTest, UpdateCurForegroundAppInfo_AddForegroundApp, TestSize.Level1)
+{
+    AppExecFwk::AppStateData appStateData;
+    appStateData.uid = 1;
+    appStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    WifiAppStateAware::GetInstance().curForegroundApps_ = appStateDataList;
+    bool ret = WifiAppStateAware::GetInstance().UpdateCurForegroundAppInfo(appStateData);
+    EXPECT_EQ(WifiAppStateAware::GetInstance().curForegroundApps_.size(), 1);
+    EXPECT_TRUE(ret);
+}
+
+HWTEST_F(WifiAppStateAwareTest, UpdateCurForegroundAppInfo_RemoveForegroundApp, TestSize.Level1)
+{
+    AppExecFwk::AppStateData changeAppStateData;
+    changeAppStateData.uid = 1;
+    changeAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_BACKGROUND);
+
+    AppExecFwk::AppStateData historyAppStateData;
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    historyAppStateData.uid = 1;
+    historyAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    appStateDataList.push_back(historyAppStateData);
+    WifiAppStateAware::GetInstance().curForegroundApps_ = appStateDataList
+    bool ret = WifiAppStateAware::GetInstance().UpdateCurForegroundAppInfo(changeAppStateData);
+    EXPECT_EQ(WifiAppStateAware::GetInstance().curForegroundApps_.size(), 0);
+    EXPECT_TRUE(ret);
+}
+
+HWTEST_F(WifiAppStateAwareTest, UpdateCurForegroundAppInfo_NoNeedUpdate, TestSize.Level1)
+{
+    AppExecFwk::AppStateData changeAppStateData;
+    changeAppStateData.uid = 1;
+    changeAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+
+    AppExecFwk::AppStateData historyAppStateData;
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    historyAppStateData.uid = 1;
+    historyAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    appStateDataList.push_back(historyAppStateData);
+    WifiAppStateAware::GetInstance().curForegroundApps_ = appStateDataList
+    bool ret = WifiAppStateAware::GetInstance().UpdateCurForegroundAppInfo(changeAppStateData);
+    EXPECT_EQ(WifiAppStateAware::GetInstance().curForegroundApps_.size(), 1);
+    EXPECT_FALSE(ret);
+}
+
+HWTEST_F(WifiAppStateAwareTest, HasRecordInCurForegroundApps_True, TestSize.Level1)
+{
+    AppExecFwk::AppStateData historyAppStateData;
+    historyAppStateData.uid = 1;
+    historyAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    appStateDataList.push_back(historyAppStateData);
+
+    AppExecFwk::AppStateData changeAppStateData;
+    changeAppStateData.uid = 1;
+
+    bool ret = WifiAppStateAware::GetInstance().HasRecordInCurForegroundApps(changeAppStateData);
+    EXPECT_TRUE(ret);
+}
+
+HWTEST_F(WifiAppStateAwareTest, HasRecordInCurForegroundApps_False, TestSize.Level1)
+{
+    AppExecFwk::AppStateData historyAppStateData;
+    historyAppStateData.uid = 1;
+    historyAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    appStateDataList.push_back(historyAppStateData);
+
+    AppExecFwk::AppStateData changeAppStateData;
+    changeAppStateData.uid = 2;
+
+    bool ret = WifiAppStateAware::GetInstance().HasRecordInCurForegroundApps(changeAppStateData);
+    EXPECT_FALSE(ret);
+}
+
+HWTEST_F(WifiAppStateAwareTest, IsForegroundApp_True, TestSize.Level1)
+{
+    AppExecFwk::AppStateData historyAppStateData;
+    historyAppStateData.uid = 1;
+    historyAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    appStateDataList.push_back(historyAppStateData);
+
+    bool ret = WifiAppStateAware::GetInstance().IsForegroundApp(1);
+    EXPECT_TRUE(ret);
+}
+
+HWTEST_F(WifiAppStateAwareTest, IsForegroundApp_False, TestSize.Level1)
+{
+    AppExecFwk::AppStateData historyAppStateData;
+    historyAppStateData.uid = 1;
+    historyAppStateData.state = static_cast<int32_t>(AppExecFwk::ApplicationState::APP_STATE_FOREGROUND);
+    std::vector<AppExecFwk::AppStateData> appStateDataList = {};
+    appStateDataList.push_back(historyAppStateData);
+
+    bool ret = WifiAppStateAware::GetInstance().IsForegroundApp(2);
+    EXPECT_FALSE(ret);
+}

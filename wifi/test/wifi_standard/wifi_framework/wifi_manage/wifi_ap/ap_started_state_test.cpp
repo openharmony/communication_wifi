3eb7944238cb79792ed9649c7c3edc50c2c10e3b
likuanxin <likuanxin@huawei.com>
2024-06-27 12:47:56 +0800

    提交sta&Ap tdd
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_started_state_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_started_state_test.cpp
@@ -504,7 +504,7 @@ HWTEST_F(ApStartedState_test, UpdatMacAddressTest, TestSize.Level1)
 {
     std::string ssid = "1234";
     KeyMgmt securityType = KeyMgmt::WPA2_PSK;
-    HotspotConfig curApConfig; 
+    HotspotConfig curApConfig;
     curApConfig.SetSsid("1234");
     curApConfig.SetSecurityType(KeyMgmt::WPA2_PSK);
     EXPECT_CALL(WifiSettings::GetInstance(), GetHotspotConfig(_, 0))

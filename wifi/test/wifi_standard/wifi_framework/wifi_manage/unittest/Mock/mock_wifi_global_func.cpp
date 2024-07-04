b56618039ee037fbab2e1c17c01ac564807fc05e
yangpengfei <yangpengfei34@huawei.com>
2024-07-04 01:20:08 +0000

    update
    Signed-off-by: yangpengfei <yangpengfei34@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/Mock/mock_wifi_global_func.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/unittest/Mock/mock_wifi_global_func.cpp
@@ -26,48 +26,55 @@ constexpr const char* FACTORY_WIFI_COUNTRY_CODE = "const.wifi.country_code.facto
 constexpr const char* OPERATOR_NUMERIC_KEY = "ril.operator.numeric";  // plmn cached
 constexpr const char* WIFI_COUNTRY_CODE_DYNAMIC_UPDATE_KEY = "persist.wifi.country_code.dynamic_update";
 constexpr const char* DEFAULT_REGION_KEY = "const.global.region";
+const int INDEX_ZERO = 0;
+const int INDEX_ONE = 1;
+const int INDEX_TWO = 2;
+const int INDEX_THREE = 3;
+const int INDEX_FOUR = 4;
+const int INDEX_FIVE = 5;
+const int INDEX_SIX = 6;
 
 int GetParamValue(const char *key, const char *def, char *value, uint32_t len)
 {
     if (strcasecmp(key, WIFI_COUNTRY_CODE_CONFIG) == 0) {
         WIFI_LOGI("GetParamValue WIFI_COUNTRY_CODE_CONFIG");
-        value[0] = '3';
-        value[1] = '1';
+        value[INDEX_ZERO] = '3';
+        value[INDEX_ONE] = '1';
         return 1;  // success
     } else if (strcasecmp(key, WIFI_COUNTRY_CODE_RUN_MODE) == 0) {
         WIFI_LOGI("GetParamValue WIFI_COUNTRY_CODE_RUN_MODE");
-        value[0] = 'f';
-        value[1] = 'a';
-        value[2] = 'c';
-        value[3] = 't';
-        value[4] = 'o';
-        value[5] = 'r';
-        value[6] = 'y';
+        value[INDEX_ZERO] = 'f';
+        value[INDEX_ONE] = 'a';
+        value[INDEX_TWO] = 'c';
+        value[INDEX_THREE] = 't';
+        value[INDEX_FOUR] = 'o';
+        value[INDEX_FIVE] = 'r';
+        value[INDEX_SIX] = 'y';
         return 1;  // success
     } else if (strcasecmp(key, FACTORY_WIFI_COUNTRY_CODE)) {
         WIFI_LOGI("GetParamValue FACTORY_WIFI_COUNTRY_CODE");
-        value[0] = 'U';
-        value[1] = 'S';
+        value[INDEX_ZERO] = 'U';
+        value[INDEX_ONE] = 'S';
         return 1;  // success
     } else if (strcasecmp(key, OPERATOR_NUMERIC_KEY)) {
         WIFI_LOGI("GetParamValue OPERATOR_NUMERIC_KEY");
-        value[0] = '4';
-        value[1] = '6';
-        value[2] = '0';
-        value[3] = '0';
-        value[4] = '0';
-        value[5] = '0';
-        value[6] = '0';
+        value[INDEX_ZERO] = '4';
+        value[INDEX_ONE] = '6';
+        value[INDEX_TWO] = '0';
+        value[INDEX_THREE] = '0';
+        value[INDEX_FOUR] = '0';
+        value[INDEX_FIVE] = '0';
+        value[INDEX_SIX] = '0';
         return 1;  // success
     } else if (strcasecmp(key, WIFI_COUNTRY_CODE_DYNAMIC_UPDATE_KEY)) {
         WIFI_LOGI("GetParamValue WIFI_COUNTRY_CODE_DYNAMIC_UPDATE_KEY");
-        value[0] = 'C';
-        value[1] = 'N';
+        value[INDEX_ZERO] = 'C';
+        value[INDEX_ONE] = 'N';
         return 1;  // success
     } else if (strcasecmp(key, DEFAULT_REGION_KEY)) {
         WIFI_LOGI("GetParamValue DEFAULT_REGION_KEY");
-        value[0] = 'J';
-        value[1] = 'P';
+        value[INDEX_ZERO] = 'J';
+        value[INDEX_ONE] = 'P';
         return 1;  // success
     } else {
         // nothing

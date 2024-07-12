06939febb11d2d3bfcb8934435246b8408fc91cf
xionglei <xionglei3@huawei.com>
2024-07-12 11:26:42 +0800

    wifi 服务告警清理 Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/hdi_interface/wifi_hdi_wpa_p2p_impl.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/hdi_interface/wifi_hdi_wpa_p2p_impl.h
@@ -84,15 +84,15 @@ WifiErrorNo HdiP2pInvite(const char *peerBssid, const char *goBssid, const char 
 
 WifiErrorNo HdiP2pReinvoke(int networkId, const char *bssid);
 
-WifiErrorNo HdiP2pGetDeviceAddress(char *deviceAddress);
+WifiErrorNo HdiP2pGetDeviceAddress(char *deviceAddress, int size);
 
-WifiErrorNo HdiP2pReqServiceDiscovery(struct HdiP2pReqService *reqService, char *replyDisc);
+WifiErrorNo HdiP2pReqServiceDiscovery(struct HdiP2pReqService *reqService, char *replyDisc, int size);
 
 WifiErrorNo HdiP2pCancelServiceDiscovery(const char *id);
 
 WifiErrorNo HdiP2pRespServerDiscovery(struct HdiP2pServDiscReqInfo *info);
 
-WifiErrorNo HdiP2pConnect(P2pConnectInfo *info, char *replyPin);
+WifiErrorNo HdiP2pConnect(P2pConnectInfo *info, char *replyPin, int size);
 
 WifiErrorNo HdiP2pHid2dConnect(struct Hid2dConnectInfo *info);
 

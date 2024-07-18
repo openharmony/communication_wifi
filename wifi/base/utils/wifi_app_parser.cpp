2d8813e9660b26db357f209a0dd5f79180a706b5
sunjingqiang <17319862417@163.com>
2024-07-18 15:35:01 +0800

    xml配置文件初始化修改
    Signed-off-by: sunjingqiang <17319862417@163.com>

--- wifi/base/utils/wifi_app_parser.cpp
+++ wifi/base/utils/wifi_app_parser.cpp
@@ -52,14 +52,6 @@ const std::unordered_map<std::string, AppType> appTypeMap = {
 AppParser::AppParser()
 {
     WIFI_LOGI("%{public}s enter", __FUNCTION__);
-    if (IsReadCloudConfig()) {
-        ReadPackageCloudFilterConfig();
-    }
-    if (InitAppParser(WIFI_MONITOR_APP_FILE_PATH)) {
-        WIFI_LOGD("%{public}s InitAppParser successful", __FUNCTION__);
-    } else {
-        WIFI_LOGE("%{public}s InitAppParser fail", __FUNCTION__);
-    };
 }
 
 AppParser::~AppParser()
@@ -70,11 +62,19 @@ AppParser::~AppParser()
 AppParser &AppParser::GetInstance()
 {
     static std::mutex xmlMutex;
+    static AppParser instance;
     static bool initFlag = false;
     if (!initFlag) {
         std::unique_lock<std::mutex> lock(xmlMutex);
         if (!initFlag) {
-            static AppParser instance;
+            if (IsReadCloudConfig()) {
+                ReadPackageCloudFilterConfig();
+            }
+            if (InitAppParser(WIFI_MONITOR_APP_FILE_PATH)) {
+                WIFI_LOGD("%{public}s InitAppParser successful", __FUNCTION__);
+            } else {
+                WIFI_LOGE("%{public}s InitAppParser fail", __FUNCTION__);
+            };
             initFlag = true;
         }
     }

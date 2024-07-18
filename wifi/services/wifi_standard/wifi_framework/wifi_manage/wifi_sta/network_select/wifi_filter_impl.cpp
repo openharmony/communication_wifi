dd9e6d19b195820f0c05b098cf4821813971654b
yangpengfei <yangpengfei34@huawei.com>
2024-07-18 11:15:56 +0000

    update
    Signed-off-by: yangpengfei <yangpengfei34@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/network_select/wifi_filter_impl.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/network_select/wifi_filter_impl.cpp
@@ -281,7 +281,17 @@ bool NoInternetWifiFilter::Filter(NetworkCandidate &networkCandidate)
             wifiDeviceConfig.noInternetAccess, MacAnonymize(interScanInfo.bssid).c_str());
         return false;
     }
-
+#ifndef OHOS_ARCH_LITE
+    std::map<std::string, std::vector<std::string>> filterMap;
+    WifiSettings::GetInstance().GetPackageFilterMap(filterMap);
+    std::vector<std::string> settings_module_name = filterMap["settings_module_name"];
+    std::string name = settings_module_name.empty() ? "" : settings_module_name.front();
+    if (!name.empty() && WifiAppStateAware::GetInstance().IsForegroundApp(name)) {
+        WIFI_LOGI("NoInternetWifiFilter, settings in foreground, skip candidate, bssid=%{public}s",
+            MacAnonymize(interScanInfo.bssid).c_str());
+        return false;
+    }
+#endif
     if (!NetworkStatusHistoryManager::HasInternetEverByHistory(wifiDeviceConfig.networkStatusHistory)) {
         WIFI_LOGI("NoInternetWifiFilter, never has internet, skip candidate, bssid=%{public}s",
             MacAnonymize(interScanInfo.bssid).c_str());

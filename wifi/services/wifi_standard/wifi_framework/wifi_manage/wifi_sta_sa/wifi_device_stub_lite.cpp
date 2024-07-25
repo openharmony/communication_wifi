a6e6a184bf8d7887663485b819f82e8ad71d5d70
CoderAdo <duliqun1@h-partners.com>
2024-07-25 00:56:51 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_stub_lite.cpp.
    Signed-off-by: CoderAdo <duliqun1@h-partners.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_stub_lite.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_stub_lite.cpp
@@ -1,3 +1,4 @@
+ (ipv6Info.gateway != result->strOptRouter1) || (ipv6Info.linkIpV6Address != result->strOptLinkIpv6Addr)) {
 /*
  * Copyright (C) 2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");

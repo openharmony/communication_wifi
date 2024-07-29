106b62046cb371dbca16544821926b695127a59e
zhangqin88 <zhangqin88@huawei.com>
2024-07-29 17:36:37 +0800

    lanhuangtongbu
    Signed-off-by: zhangqin88 <zhangqin88@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.h
@@ -1008,6 +1008,21 @@ private:
     ErrCode SetExternalSim(const std::string ifName, const std::string &eap, int value) const;
 
     /**
+     * @Description : should sta connect use factory mac address.
+     *
+     * @param networkId - networkId.
+     */
+    bool ShouldUseFactoryMac(const WifiDeviceConfig &deviceConfig);
+
+    /**
+     * @Description : Check Current Connect is used randomized mac or not.
+     *
+     * @param networkId - networkId.
+     * @Return true: used randomized mac address.
+     */
+    bool CurrentIsRandomizedMac();
+
+    /**
      * @Description : Check Current DisConnect event is should retry.
      *
      * @param eventName - eventName.
@@ -1022,21 +1037,6 @@ private:
      * @Return true: need stop timer.
      */
     bool IsDisConnectReasonShouldStopTimer(int reason);
-
-    /**
-     * @Description : should sta connect use factory mac address.
-     *
-     * @param networkId - networkId.
-     */
-    bool ShouldUseFactoryMac(const WifiDeviceConfig &deviceConfig);
-
-    /**
-     * @Description : Check Current Connect is used randomized mac or not.
-     *
-     * @param networkId - networkId.
-     * @Return true: used randomized mac address.
-     */
-    bool CurrentIsRandomizedMac();
 
     /**
      * @Description : Hilink Save Data To Device Config.

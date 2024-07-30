505acd41aab09fa78f2491eb3d0239c000a5c4c1
WangYusen <wangyusen8@huawei.com>
2024-07-30 16:15:53 +0800

    to shared ptr
    Signed-off-by: WangYusen <wangyusen8@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_started_state.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_started_state.h
@@ -248,8 +248,8 @@ private:
 
     void SetRandomMac() const;
     bool SetCountry();
-    void ProcessCmdHotspotChannelChanged(InternalMessage &msg);
-    void ProcessCmdAssociatedStaChanged(InternalMessage &msg);
+    void ProcessCmdHotspotChannelChanged(InternalMessagePtr msg);
+    void ProcessCmdAssociatedStaChanged(InternalMessagePtr msg);
 
 private:
     // Store the configuration when set to hostapd, hostapd will asynchronously notify the setting result

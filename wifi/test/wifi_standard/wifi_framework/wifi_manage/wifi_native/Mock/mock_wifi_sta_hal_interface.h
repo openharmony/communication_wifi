be49416799d1de30d620b242702870428dbeb5dc
likuanxin <likuanxin@huawei.com>
2024-08-10 07:08:16 +0000

    update mock_wifi_sta_hal_interface.h.
    Signed-off-by: likuanxin <likuanxin@huawei.com>

---
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_native/Mock/mock_wifi_sta_hal_interface.h
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_MOCK_WIFI_STA_HAL_INTERFACE_H
+#define OHOS_MOCK_WIFI_STA_HAL_INTERFACE_H
+#include <string>
+#include <vector>
+#include "wifi_event_callback.h"
+#include "wifi_native_struct.h"
+#include "wifi_error_no.h"
+#include "wifi_msg.h"
+#include "inter_scan_info.h"
+
+namespace OHOS {
+namespace Wifi {
+class MockWifiStaHalInterface {
+public:
+    static MockWifiStaHalInterface &GetInstance(void);
+    void SetRetResult(WifiErrorNo retResult);
+    WifiErrorNo GetRetResult();
+private:
+    MockWifiStaHalInterface();
+    WifiErrorNo mRetResult;
+};
+
+class WifiStaHalInterface {
+public:
+    static WifiStaHalInterface &GetInstance(void);
+    WifiErrorNo StartWifi(const std::string &ifaceName = "wlan0");
+    WifiErrorNo StopWifi(void);
+    WifiErrorNo Connect(int networkId);
+    WifiErrorNo Reconnect(void);
+    WifiErrorNo Reassociate(void);
+    WifiErrorNo Disconnect(void);
+    WifiErrorNo GetStaCapabilities(unsigned int &capabilities);
+    WifiErrorNo GetStaDeviceMacAddress(std::string &mac);
+    WifiErrorNo SetWifiCountryCode(const std::string &ifaceName, const std::string &code);
+    WifiErrorNo GetSupportFrequencies(const std::string &ifaceName, int band, std::vector<int> &frequencies);
+    WifiErrorNo SetConnectMacAddr(const std::string &ifaceName, const std::string &mac);
+    WifiErrorNo SetScanMacAddress(const std::string &mac);
+    WifiErrorNo DisconnectLastRoamingBssid(const std::string &mac);
+    WifiErrorNo GetSupportFeature(long &feature);
+    WifiErrorNo SetTxPower(const std::string &ifaceName, int power);
+    WifiErrorNo Scan(const std::string &ifaceName, const WifiHalScanParam &scanParam);
+    WifiErrorNo QueryScanInfos(const std::string &ifaceName, std::vector<InterScanInfo> &scanInfos);
+    WifiErrorNo StartPnoScan(const std::string &ifaceName, const WifiHalPnoScanParam &scanParam);
+    WifiErrorNo StopPnoScan(const std::string &ifaceName);
+    WifiErrorNo RemoveDevice(int networkId);
+    WifiErrorNo ClearDeviceConfig(void) const;
+    WifiErrorNo GetNextNetworkId(int &networkId);
+    WifiErrorNo EnableNetwork(int networkId);
+    WifiErrorNo DisableNetwork(int networkId);
+    WifiErrorNo SetDeviceConfig(int networkId, const WifiHalDeviceConfig &config);
+    WifiErrorNo GetDeviceConfig(WifiHalGetDeviceConfig &config);
+    WifiErrorNo SaveDeviceConfig(void);
+    WifiErrorNo RegisterStaEventCallback(const WifiEventCallback &callback);
+    WifiErrorNo StartWpsPbcMode(const WifiHalWpsConfig &config);
+    WifiErrorNo StartWpsPinMode(const WifiHalWpsConfig &config, int &pinCode);
+    WifiErrorNo StopWps(void);
+    WifiErrorNo GetRoamingCapabilities(WifiHalRoamCapability &capability);
+    WifiErrorNo SetBssid(int networkId, const std::string &bssid);
+    WifiErrorNo SetRoamConfig(const WifiHalRoamConfig &config);
+    WifiErrorNo WpaAutoConnect(int enable);
+    WifiErrorNo WpaBlocklistClear();
+    WifiErrorNo GetNetworkList(std::vector<WifiHalWpaNetworkInfo> &networkList);
+    WifiErrorNo GetConnectSignalInfo(const std::string &ifaceName, const std::string &endBssid,
+        WifiHalWpaSignalInfo &info);
+    WifiErrorNo SetPmMode(const std::string &ifaceName, int frequency, int mode);
+    WifiErrorNo SetDpiMarkRule(const std::string &ifaceName, int uid, int protocol, int enable);
+    WifiErrorNo ShellCmd(const std::string &ifName, const std::string &cmd);
+    WifiErrorNo GetPskPassphrase(const std::string &ifName, std::string &psk);
+    WifiErrorNo GetChipsetCategory(const std::string &ifaceName, int& chipsetCategory);
+    WifiErrorNo GetChipsetWifiFeatrureCapability(const std::string &ifaceName, int& chipsetFeatrureCapability);
+    WifiErrorNo SetNetworkInterfaceUpDown(const std::string &ifaceName, bool upDown);
+    const WifiEventCallback &GetCallbackInst(void) const;
+    const std::function<void(int)> &GetDeathCallbackInst(void) const;
+    WifiErrorNo RegisterNativeProcessCallback(const std::function<void(int)> &callback);
+public:
+    WifiHalWpaSignalInfo mInfo;
+private:
+    WifiEventCallback mStaCallback;
+    std::function<void(int)> mDeathCallback;
+};
+}  // namespace Wifi
+}  // namespace OHOS
+
+#endif

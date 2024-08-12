d3e0e16e432c4ec8af2d2268feaabb66af14f9fc
CodeAdo <baliguan163@163.com>
2024-08-12 16:01:37 +0800

    dhcp dns Signed-off-by: CoderAdo <duliqun1@h-partners.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
@@ -4162,8 +4162,10 @@ void StaStateMachine::DhcpResultNotify::TryToSaveIpV6Result(IpInfo &ipInfo, IpV6
 #ifndef OHOS_ARCH_LITE
         WifiDeviceConfig config;
         WifiSettings::GetInstance().GetDeviceConfig(pStaStateMachine->linkedInfo.networkId, config);
-        WifiNetAgent::GetInstance().OnStaMachineUpdateNetLinkInfo(ipInfo, ipv6Info, config.wifiProxyconfig,
-            pStaStateMachine->GetInstanceId());
+        if (!ipv6Info.primaryDns.empty()) {
+            WifiNetAgent::GetInstance().OnStaMachineUpdateNetLinkInfo(ipInfo, ipv6Info, config.wifiProxyconfig,
+                pStaStateMachine->GetInstanceId());
+        }
 #endif
     } else {
         LOGI("TryToSaveIpV6Result not UpdateNetLinkInfo");

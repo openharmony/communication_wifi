4a923a8b33393e039ff07a84f0bc027a0673fde2
likuanxin <likuanxin@huawei.com>
2024-08-15 12:06:57 +0800

    解决portal网络因信号不好断开不能回连的问题
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.h
@@ -1254,6 +1254,7 @@ private:
     void TransHalDeviceConfig(WifiHalDeviceConfig &halDeviceConfig, const WifiDeviceConfig &config) const;
     void SetRandomMacConfig(WifiStoreRandomMac &randomMacInfo, const WifiDeviceConfig &deviceConfig,
     std::string &currentMac);
+    bool IsGoodSignalQuality();
 };
 }  // namespace Wifi
 }  // namespace OHOS

4926f4201913b51541e170095efcca7478aa0177
xionglei <xionglei3@huawei.com>
2024-08-21 08:11:25 +0000

    wifi_self_cure/self_cure_interface.cpp.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_interface.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_interface.cpp
@@ -30,6 +30,14 @@ SelfCureInterface::~SelfCureInterface()
 {
     WIFI_LOGI("SelfCureInterface::~SelfCureInterface");
     std::lock_guard<std::mutex> lock(mutex);
+    // unRegister callback func
+    IStaService *pService = WifiServiceManager::GetInstance().GetStaServiceInst(0);
+    if (pService == nullptr) {
+        WIFI_LOGE("Get %{public}s service failed!", WIFI_SERVICE_STA);
+    } else {
+        pService->UnRegisterStaServiceCallback(mStaCallback);
+    }
+
     if (pSelfCureService != nullptr) {
         delete pSelfCureService;
         pSelfCureService = nullptr;

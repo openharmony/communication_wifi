c80078d11d6f4e475d422e8c8b75c618aa295a33
likuanxin <likuanxin@huawei.com>
2024-08-30 04:01:04 +0000

    update wifi_device_service_impl.h.
    Signed-off-by: likuanxin <likuanxin@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_service_impl.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_service_impl.h
@@ -133,6 +133,10 @@ public:
 
     ErrCode DeregisterFilterBuilder(const FilterTag &filterTag, const std::string &builderName) override;
 
+     ErrCode RegisterScoreBuilder(const ScoreTag &scoreTag, const std::string &scoreName,
+                                          const ScoreBuilder &scoreBuilder) override;
+ 
+    ErrCode DeregisterScoreBuilder(const ScoreTag &scoreTag, const std::string &scoreName) override;
 
 #ifdef OHOS_ARCH_LITE
     ErrCode RegisterCallBack(const std::shared_ptr<IWifiDeviceCallBack> &callback,

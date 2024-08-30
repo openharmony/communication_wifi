b61ee3cc27a568d145fd401ecf6d0fa84bc226a4
xionglei <xionglei3@huawei.com>
2024-08-30 08:12:28 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_state_machine.cpp.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_ap/ap_state_machine.cpp
@@ -26,7 +26,6 @@ DEFINE_WIFILOG_HOTSPOT_LABEL("WifiApStateMachine");
 
 namespace OHOS {
 namespace Wifi {
-    
 ApStateMachine::ApStateMachine(ApStationsManager &apStationsManager, ApRootState &apRootState, ApIdleState &apIdleState,
     ApStartedState &apStartedState, ApMonitor &apMonitor, int id)
     : StateMachine("ApStateMachine"),
@@ -61,7 +60,7 @@ void ApStateMachine::Init()
     StatePlus(&m_ApRootState, nullptr);
     StatePlus(&m_ApIdleState, &m_ApRootState);
     StatePlus(&m_ApStartedState, &m_ApRootState);
-    SetFirstState(&m_ApIdleState);
+    SetFirstState(&m_ApStartedState);
     m_iface = WifiConfigCenter::GetInstance().GetApIfaceName();
     StartStateMachine();
 }

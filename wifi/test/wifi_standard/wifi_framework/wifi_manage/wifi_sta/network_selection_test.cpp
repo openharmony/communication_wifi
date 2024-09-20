3a11c9b039be1b62c1d5e0d60965a0340e918b16
zhangqin88 <zhangqin88@huawei.com>
2024-09-20 19:24:21 +0800

    TDD crash
    Signed-off-by: zhangqin88 <zhangqin88@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/network_selection_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/network_selection_test.cpp
@@ -254,7 +254,8 @@ HWTEST_F(NetworkSelectionTest, TestMatchUserSelectBssidNetwork, TestSize.Level1)
     EXPECT_CALL(WifiConfigCenter::GetInstance(), GetUserLastSelectedNetworkId(_)).WillRepeatedly(Return(0));
     EXPECT_CALL(WifiConfigCenter::GetInstance(), GetUserLastSelectedNetworkTimeVal(_)).WillRepeatedly(Return(0));
     EXPECT_CALL(WifiSettings::GetInstance(), GetSignalLevel(_, _, _)).WillRepeatedly(Return(4));
-    EXPECT_CALL(WifiSettings::GetInstance(), GetDeviceConfig(An<const std::string &>(), An<const std::string &>(), _, _)).
+    EXPECT_CALL(WifiSettings::GetInstance(), GetDeviceConfig(
+        An<const std::string &>(), An<const std::string &>(), _, _)).
     WillRepeatedly(Return(0));
     EXPECT_CALL(WifiSettings::GetInstance(),
         GetDeviceConfig(An<const std::string &>(), An<const std::string &>(), _, _)).
@@ -716,7 +717,8 @@ HWTEST_F(NetworkSelectionTest, TestRecentUserSelectNetwork, TestSize.Level1)
     EXPECT_CALL(WifiSettings::GetInstance(), GetSignalLevel(_, _, _)).WillRepeatedly(Return(4));
     EXPECT_CALL(WifiSettings::GetInstance(),
         GetDeviceConfig(An<const std::string &>(), An<const std::string &>(), _, _)).
-        WillRepeatedly(Invoke([](const std::string &ssid, const std::string &, WifiDeviceConfig &wifiDeviceConfig, int) {
+        WillRepeatedly(
+            Invoke([](const std::string &ssid, const std::string &, WifiDeviceConfig &wifiDeviceConfig, int) {
         if (ssid == "test1") {
             wifiDeviceConfig.networkId = 0;
         } else {

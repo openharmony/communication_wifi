be477c4ed3e0419080010900a61dac5c9dd714b7
shenqihang <shenqihang@huawei.com>
2024-09-26 10:54:01 +0800

    wifi2wifi架构调整
    Signed-off-by: shenqihang <shenqihang@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_pro/wifi_pro_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_pro/wifi_pro_service.cpp
@@ -12,33 +12,33 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <memory>
 #include "wifi_logger.h"
 #include "wifi_msg.h"
 #include "wifi_pro_service.h"
 #include "wifi_pro_utils.h"
 #include "wifi_common_util.h"
- 
+
 #ifndef FALLTHROUGH_INTENDED
 #define FALLTHROUGH_INTENDED [[clang::fallthrough]]  // NOLINT
 #endif
- 
+
 namespace OHOS {
 namespace Wifi {
 DEFINE_WIFILOG_LABEL("WifiProService");
- 
+
 WifiProService::WifiProService(int32_t instId)
     : instId_(instId)
 {
     WIFI_LOGI("Enter WifiProService");
 }
- 
+
 WifiProService::~WifiProService()
 {
     WIFI_LOGI("Enter ~WifiProService");
 }
- 
+
 ErrCode WifiProService::InitWifiProService()
 {
     WIFI_LOGI("Enter InitSelfCureService.");
@@ -53,7 +53,7 @@ ErrCode WifiProService::InitWifiProService()
     }
     return WIFI_OPT_SUCCESS;
 }
- 
+
 void WifiProService::HandleStaConnChanged(OperateResState state, const WifiLinkedInfo &linkedInfo)
 {
     WIFI_LOGI("WifiProService wifi connection state change, state:%{public}d,connState:%{public}d,"
@@ -62,7 +62,7 @@ void WifiProService::HandleStaConnChanged(OperateResState state, const WifiLinke
         WIFI_LOGE("%{public}s pWifiProStateMachine_ is null.", __FUNCTION__);
         return;
     }
- 
+
     switch (state) {
         case OperateResState::CONNECT_AP_CONNECTED:
             FALLTHROUGH_INTENDED;
@@ -73,13 +73,13 @@ void WifiProService::HandleStaConnChanged(OperateResState state, const WifiLinke
             FALLTHROUGH_INTENDED;
         case OperateResState::CONNECT_NETWORK_DISABLED:
             FALLTHROUGH_INTENDED;
+        case OperateResState::CONNECT_NETWORK_ENABLED:
+            NotifyCheckWifiInternetResult(state);
+            break;
         case OperateResState::CONNECT_ENABLE_NETWORK_FAILED:
             FALLTHROUGH_INTENDED;
         case OperateResState::CONNECT_CONNECTING_TIMEOUT:
             FALLTHROUGH_INTENDED;
-        case OperateResState::CONNECT_NETWORK_ENABLED:
-            NotifyCheckWifiInternetResult(state);
-            break;
         case OperateResState::CONNECT_CONNECTION_REJECT:
             FALLTHROUGH_INTENDED;
         case OperateResState::CONNECT_PASSWORD_WRONG:
@@ -91,13 +91,13 @@ void WifiProService::HandleStaConnChanged(OperateResState state, const WifiLinke
             break;
     }
 }
- 
+
 void WifiProService::NotifyWifi2WifiFailed()
 {
     WIFI_LOGI("NotifyWifi2WifiFailed: wifi2wifi failed");
     pWifiProStateMachine_->SendMessage(EVENT_WIFI2WIFI_FAILED);
 }
- 
+
 void WifiProService::NotifyWifiConnectStateChanged(OperateResState state, const WifiLinkedInfo &linkedInfo)
 {
     WIFI_LOGI("NotifyWifiConnectStateChanged:networkId:%{public}d, ssid:%{public}s,"
@@ -106,13 +106,13 @@ void WifiProService::NotifyWifiConnectStateChanged(OperateResState state, const 
     pWifiProStateMachine_->SendMessage(EVENT_WIFI_CONNECT_STATE_CHANGED, static_cast<int32_t>(state),
         linkedInfo.networkId, linkedInfo.bssid);
 }
- 
+
 void WifiProService::NotifyCheckWifiInternetResult(OperateResState state)
 {
     WIFI_LOGI("NotifyCheckWifiInternetResult: wifi internet result:%{public}d", static_cast<int32_t>(state));
     pWifiProStateMachine_->SendMessage(EVENT_CHECK_WIFI_INTERNET_RESULT, static_cast<int32_t>(state));
 }
- 
+
 void WifiProService::HandleRssiLevelChanged(int32_t rssi)
 {
     WIFI_LOGI("WifiProService::HandleRssiLevelChanged, %{public}d.", rssi);
@@ -120,10 +120,10 @@ void WifiProService::HandleRssiLevelChanged(int32_t rssi)
         WIFI_LOGE("%{public}s pWifiProStateMachine_ is null.", __FUNCTION__);
         return;
     }
- 
+
     pWifiProStateMachine_->SendMessage(EVENT_WIFI_RSSI_CHANGED, rssi);
 }
- 
+
 void WifiProService::HandleScanResult(const std::vector<InterScanInfo> &scanInfos)
 {
     WIFI_LOGI("Enter WifiProService::HandleScanResult.");
@@ -131,7 +131,7 @@ void WifiProService::HandleScanResult(const std::vector<InterScanInfo> &scanInfo
         WIFI_LOGE("%{public}s pWifiProStateMachine_ is null.", __FUNCTION__);
         return;
     }
- 
+
     pWifiProStateMachine_->SendMessage(EVENT_HANDLE_SCAN_RESULT, scanInfos);
 }
 }  // namespace Wifi

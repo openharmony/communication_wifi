d8f4eecf236a2080b8758d90d4f7d055c5e04849
xionglei <xionglei3@huawei.com>
2024-10-11 15:02:29 +0800

    5.0.1同步
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p_sa/wifi_p2p_service_impl.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p_sa/wifi_p2p_service_impl.cpp
@@ -506,7 +506,7 @@ ErrCode WifiP2pServiceImpl::RemoveGroupClient(const GcInfo &info)
 ErrCode WifiP2pServiceImpl::DeleteGroup(const WifiP2pGroupInfo &group)
 {
     WIFI_LOGI("DeleteGroup, group name [%{public}s]", group.GetGroupName().c_str());
-    if (!WifiAuthCenter::IsSystemAppByToken()) {
+    if (!WifiAuthCenter::IsSystemAccess()) {
         WIFI_LOGE("DeleteGroup:NOT System APP, PERMISSION_DENIED!");
         return WIFI_OPT_NON_SYSTEMAPP;
     }
@@ -876,7 +876,7 @@ ErrCode WifiP2pServiceImpl::QueryP2pLocalDevice(WifiP2pDevice &device)
 ErrCode WifiP2pServiceImpl::QueryP2pGroups(std::vector<WifiP2pGroupInfo> &groups)
 {
     WIFI_LOGI("QueryP2pGroups");
-    if (!WifiAuthCenter::IsSystemAppByToken()) {
+    if (!WifiAuthCenter::IsSystemAccess()) {
         WIFI_LOGE("QueryP2pGroups:NOT System APP, PERMISSION_DENIED!");
         return WIFI_OPT_NON_SYSTEMAPP;
     }
@@ -990,7 +990,7 @@ ErrCode WifiP2pServiceImpl::GetSupportedFeatures(long &features)
 bool WifiP2pServiceImpl::IsCallingAllowed()
 {
     auto state = WifiConfigCenter::GetInstance().GetWifiDetailState();
-    if (state == WifiDetailState::STATE_SEMI_ACTIVE && !WifiAuthCenter::IsSystemAppByToken()) {
+    if (state == WifiDetailState::STATE_SEMI_ACTIVE && !WifiAuthCenter::IsSystemAccess()) {
         WIFI_LOGW("curr wifi state is semiactive, only allow system app use p2p service");
         return false;
     }
@@ -1013,7 +1013,7 @@ bool WifiP2pServiceImpl::IsP2pServiceRunning()
 ErrCode WifiP2pServiceImpl::SetP2pDeviceName(const std::string &deviceName)
 {
     WIFI_LOGI("SetP2pDeviceName:%{public}s", deviceName.c_str());
-    if (!WifiAuthCenter::IsSystemAppByToken()) {
+    if (!WifiAuthCenter::IsSystemAccess()) {
         WIFI_LOGE("SetP2pDeviceName:NOT System APP, PERMISSION_DENIED!");
         return WIFI_OPT_NON_SYSTEMAPP;
     }

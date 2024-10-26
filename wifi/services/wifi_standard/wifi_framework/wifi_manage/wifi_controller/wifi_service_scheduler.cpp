712ad4de5f0d01bb354e22ce30c9e308b73801fe
xiaosi <xiaosi2@huawei.com>
2024-10-26 11:41:31 +0800

    wifi stop bugfix
    Signed-off-by: xiaosi <xiaosi2@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_controller/wifi_service_scheduler.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_controller/wifi_service_scheduler.cpp
@@ -187,6 +187,7 @@ ErrCode WifiServiceScheduler::AutoStopStaService(int instId)
         WifiOprMidState staState = WifiConfigCenter::GetInstance().GetWifiMidState(instId);
         WriteWifiOpenAndCloseFailedHiSysEvent(static_cast<int>(OperateResState::CLOSE_WIFI_FAILED), "TIME_OUT",
             static_cast<int>(staState));
+        WifiManager::GetInstance().GetWifiTogglerManager()->ForceStopWifi();
         return WIFI_OPT_FAILED;
     }
     WifiManager::GetInstance().PushServiceCloseMsg(WifiCloseServiceCode::STA_MSG_STOPED, instId);

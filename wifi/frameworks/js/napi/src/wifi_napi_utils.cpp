ae06faf05f8d26aabad8b72c1a9c3010a14a54e8
xionglei <xionglei3@huawei.com>
2024-10-28 13:33:44 +0000

    update wifi/frameworks/js/napi/src/wifi_napi_utils.cpp.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/frameworks/js/napi/src/wifi_napi_utils.cpp
+++ wifi/frameworks/js/napi/src/wifi_napi_utils.cpp
@@ -113,7 +113,7 @@ napi_value JsObjectToInt(const napi_env& env, const napi_value& object, const ch
         napi_get_named_property(env, object, fieldStr, &field);
         NAPI_CALL(env, napi_typeof(env, field, &valueType));
         NAPI_ASSERT(env, valueType == napi_number, "Wrong argument type. Number expected.");
-        napi_get_value_int32(env, field, &fieldRef);
+        auto ret = napi_get_value_int32(env, field, &fieldRef);
         WIFI_LOGD("[%{public}s]:%{public}d, fieldStr:%{public}s=>%{public}d.", __FUNCTION__, ret, fieldStr, fieldRef);
     } else {
         WIFI_LOGW("Js to int no property: %{public}s", fieldStr);
@@ -132,7 +132,7 @@ napi_value JsObjectToUint(const napi_env& env, const napi_value& object, const c
         napi_get_named_property(env, object, fieldStr, &field);
         NAPI_CALL(env, napi_typeof(env, field, &valueType));
         NAPI_ASSERT(env, valueType == napi_number, "Wrong argument type. Number expected.");
-        napi_get_value_uint32(env, field, &fieldRef);
+        auto ret = napi_get_value_uint32(env, field, &fieldRef);
         WIFI_LOGD("[%{public}s]:%{public}u, fieldStr:%{public}s=>%{public}u.", __FUNCTION__, ret, fieldStr, fieldRef);
     } else {
         WIFI_LOGW("Js to int no property: %{public}s", fieldStr);

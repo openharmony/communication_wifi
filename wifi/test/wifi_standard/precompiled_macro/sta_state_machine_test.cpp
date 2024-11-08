08a53dfcad46aa28151c28ae9c02f4deb83cfca8
xionglei <xionglei3@huawei.com>
2024-11-08 07:55:39 +0000

    TDD
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/test/wifi_standard/precompiled_macro/sta_state_machine_test.cpp
+++ wifi/test/wifi_standard/precompiled_macro/sta_state_machine_test.cpp
@@ -69,6 +69,7 @@ public:
     {
         WifiAppStateAware& wifiAppStateAware = WifiAppStateAware::GetInstance();
         wifiAppStateAware.appChangeEventHandler.reset();
+        wifiAppStateAware.mAppStateObserver = nullptr;
     }
     virtual void SetUp()
     {

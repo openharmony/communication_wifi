e7a688830a93b2f6d98259d1d006a446329c8e31
zhangqin88 <zhangqin88@huawei.com>
2024-11-20 08:57:14 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_toolkit/include/wifi_internal_msg.h.
    Signed-off-by: zhangqin88 <zhangqin88@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/include/wifi_internal_msg.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/include/wifi_internal_msg.h
@@ -175,6 +175,7 @@ struct WifiEventCallbackMsg {
     WifiP2pDevice p2pDevice;
     P2pActionCallback p2pAction;
     CfgInfo* cfgInfo;
+    GcInfo gcInfo;
     std::string privateWfdInfo;
     WifiEventCallbackMsg()
     {

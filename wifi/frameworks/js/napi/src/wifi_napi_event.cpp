29394ce949919fb09a622cfa63941d902140743e
xionglei <xionglei3@huawei.com>
2024-11-26 18:03:59 +0800

    内存卷积
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/frameworks/js/napi/src/wifi_napi_event.cpp
+++ wifi/frameworks/js/napi/src/wifi_napi_event.cpp
@@ -721,11 +721,16 @@ void EventRegister::Register(const napi_env& env, const std::string& type, napi_
     napi_ref handlerRef = nullptr;
     napi_create_reference(env, handler, 1, &handlerRef);
     RegObj regObj(env, handlerRef);
-    auto iter = g_eventRegisterInfo.find(type);
-    if (iter == g_eventRegisterInfo.end()) {
-        g_eventRegisterInfo[type] = std::vector<RegObj>{regObj};
+    int RegisterInfoNumMax = 1000;
+    if (g_eventRegisterInfo.size() < RegisterInfoNumMax) {
+        auto iter = g_eventRegisterInfo.find(type);
+        if (iter == g_eventRegisterInfo.end()) {
+            g_eventRegisterInfo[type] = std::vector<RegObj>{regObj};
+        } else {
+            iter->second.emplace_back(regObj);
+        }
     } else {
-        iter->second.emplace_back(regObj);
+        WIFI_LOGE("Register Num Max!");
     }
 }
 

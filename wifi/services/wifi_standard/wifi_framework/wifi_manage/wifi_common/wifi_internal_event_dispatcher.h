c2b3f1372c49f8a4f7e0c262befe726d467aa0f0
xionglei <xionglei3@huawei.com>
2024-11-27 08:50:08 +0000

    code fix
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/wifi_internal_event_dispatcher.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/wifi_internal_event_dispatcher.h
@@ -122,6 +122,7 @@ public:
     void SetAppFrozen(std::set<int> pidList, bool isFrozen);
     void ResetAllFrozenApp();
     bool IsAppFrozen(int pid);
+    void FreecfgInfo(CfgInfo* cfgInfo);
 private:
     static void DealStaCallbackMsg(WifiInternalEventDispatcher &pInstance, const WifiEventCallbackMsg &msg);
     static void DealScanCallbackMsg(WifiInternalEventDispatcher &pInstance, const WifiEventCallbackMsg &msg);

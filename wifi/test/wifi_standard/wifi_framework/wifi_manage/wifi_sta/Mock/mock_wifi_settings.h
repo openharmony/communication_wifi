9cd6123f530f47bbb3a8f95caeb3352b17286b4e
xionglei <xionglei3@huawei.com>
2024-12-12 06:23:24 +0000

    TDD
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/Mock/mock_wifi_settings.h
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/Mock/mock_wifi_settings.h
@@ -96,7 +96,13 @@ public:
     virtual int SetDeviceEverConnected(int networkId) = 0;
     virtual int SetAcceptUnvalidated(int networkId, bool state) = 0;
     virtual bool GetAcceptUnvalidated(int networkId) = 0;
-    
+    virtual void SetUserConnectChoice(int networkId) = 0;
+    virtual void ClearAllNetworkConnectChoice() = 0;
+    virtual bool ClearNetworkConnectChoice(int networkId) = 0;
+    virtual void RemoveConnectChoiceFromAllNetwork(int networkId) = 0;
+    virtual bool SetNetworkConnectChoice(int networkId, int selectNetworkId, long timestamp) = 0;
+    virtual bool ClearNetworkCandidateScanResult(int networkId) = 0;
+    virtual bool SetNetworkCandidateScanResult(int networkId) = 0;
 };
 
 class WifiSettings : public MockWifiSettings {
@@ -174,6 +180,13 @@ public:
     MOCK_METHOD1(SetDeviceEverConnected, int(int networkId));
     MOCK_METHOD2(SetAcceptUnvalidated, int(int networkId, bool state));
     MOCK_METHOD1(GetAcceptUnvalidated, bool(int networkId));
+    MOCK_METHOD1(SetUserConnectChoice, void(int networkId));
+    MOCK_METHOD0(ClearAllNetworkConnectChoice, void());
+    MOCK_METHOD1(ClearNetworkConnectChoice, bool(int networkId));
+    MOCK_METHOD1(RemoveConnectChoiceFromAllNetwork, void(int networkId));
+    MOCK_METHOD3(SetNetworkConnectChoice, bool(int networkId, int selectNetworkId, long timestamp));
+    MOCK_METHOD1(ClearNetworkCandidateScanResult, bool(int networkId));
+    MOCK_METHOD1(SetNetworkCandidateScanResult, bool(int networkId));
 };
 }  // namespace OHOS
 }  // namespace Wifi

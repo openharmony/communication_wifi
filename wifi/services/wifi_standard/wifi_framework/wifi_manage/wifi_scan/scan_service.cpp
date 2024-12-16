6e9f51a719914839e99f338161213a539c9fa018
shihaojie <shihaojie11@huawei.com>
2024-12-16 09:32:23 +0000

    1
    Signed-off-by: shihaojie <shihaojie11@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
@@ -1408,8 +1408,7 @@ ErrCode ScanService::AllowSystemTimerScan()
         return WIFI_OPT_FAILED;
     }
 
-    if (staStatus != static_cast<int>(OperateResState::DISCONNECT_DISCONNECTED) &&
-        staStatus != static_cast<int>(OperateResState::CONNECT_AP_CONNECTED)) {
+    if (staStatus == static_cast<int>(OperateResState::CONNECT_CONNECTING)) {
         WIFI_LOGW("system timer scan not allowed for staStatus: %{public}d.", staStatus);
         return WIFI_OPT_SCAN_NEXT_PERIOD;
     }

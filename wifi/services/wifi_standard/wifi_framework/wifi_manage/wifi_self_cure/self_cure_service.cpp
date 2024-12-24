ebd6be2c8cf0a23ff4005dc27c3ca2c30bfef47d
chen yi wen <15068825070@163.com>
2024-12-24 06:16:15 +0000

    /services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_service.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_service.cpp
@@ -173,7 +173,7 @@ void SelfCureService::RegisterP2pEnhanceCallback()
     using namespace std::placeholders;
     p2pEnhanceStateChange_ = [this](const std::string &ifName, int32_t state) {
         this->P2pEnhanceStateChange(ifName, state);
-    },
+    };
     IEnhanceService *pEnhanceService = WifiServiceManager::GetInstance().GetEnhanceServiceInst();
     if (pEnhanceService == nullptr) {
         WIFI_LOGE("RegisterP2pEnhanceCallback get pEnhanceService failed!");

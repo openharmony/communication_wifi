5fecb9d41e4225cbb9af3c44ac8b0cde33ecf716
yinyongbin <yinyongbin@huawei.com>
2025-01-08 06:54:49 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp.
    Signed-off-by: yinyongbin <yinyongbin@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_service.cpp
@@ -295,7 +295,7 @@ ErrCode StaService::ConnectToCandidateConfig(const int uid, const int networkId)
     if (config.lastConnectTime <= 0) {
         WifiConfigCenter::GetInstance().SetSelectedCandidateNetworkId(networkId);
         if (WifiConfigCenter::GetInstance().IsAllowPopUp()) {
-            WifiNotificationUtil::GetInstance().ShowDialog(WifiDialogType::CANDIDATE_CONNECT, "");
+            WifiNotificationUtil::GetInstance().ShowDialog(WifiDialogType::CANDIDATE_CONNECT);
         }
         return WIFI_OPT_SUCCESS;
     }

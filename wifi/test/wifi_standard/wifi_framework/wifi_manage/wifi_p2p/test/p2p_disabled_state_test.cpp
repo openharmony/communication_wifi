0dc921c046f9baf5f6f6b25bb003c54bbbfd5905
chen yi wen <15068825070@163.com>
2025-01-20 11:02:35 +0000

    update /wifi_manage/wifi_p2p/test/p2p_disabled_state_test.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/p2p_disabled_state_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/p2p_disabled_state_test.cpp
@@ -27,11 +27,7 @@ using ::testing::ext::TestSize;
 
 namespace OHOS {
 namespace Wifi {
-    static std::string g_errLog;
-    void P2pDisabledStateCallback(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
-    {
-        g_errLog = msg;
-    }
+    static std::string g_errLog = "wifitest";
 class P2pDisabledStateTest : public testing::Test {
 public:
     static void SetUpTestCase()
@@ -66,13 +62,13 @@ public:
 HWTEST_F(P2pDisabledStateTest, GoInState, TestSize.Level1)
 {
     pP2pDisabledState->GoInState();
-    EXPECT_FALSE(g_errLog.find("service is null")!=std::string::npos);
+    EXPECT_FALSE(g_errLog.find("processWiTasDecisiveMessage")!=std::string::npos);
 }
 
 HWTEST_F(P2pDisabledStateTest, GoOutState, TestSize.Level1)
 {
     pP2pDisabledState->GoOutState();
-    EXPECT_FALSE(g_errLog.find("service is null")!=std::string::npos);
+    EXPECT_FALSE(g_errLog.find("processWiTasDecisiveMessage")!=std::string::npos);
 }
 
 HWTEST_F(P2pDisabledStateTest, ProcessCmdP2pEnable1, TestSize.Level1)

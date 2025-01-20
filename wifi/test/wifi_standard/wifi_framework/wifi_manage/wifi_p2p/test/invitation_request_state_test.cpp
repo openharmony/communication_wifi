162e542959a71dbf23fcc25e83503ea69033110f
chen yi wen <15068825070@163.com>
2025-01-20 06:58:27 +0000

    规范格式
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/invitation_request_state_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/invitation_request_state_test.cpp
@@ -25,7 +25,7 @@ using ::testing::ext::TestSize;
 
 namespace OHOS {
 namespace Wifi {
-        static std::string g_errLog;
+    static std::string g_errLog;
     void InvitationRequestStateCallback(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
     {
         g_errLog = msg;

e69b5a4de94f3572150d7d44d4ca267e4251e0a8
chen yi wen <15068825070@163.com>
2025-01-21 09:30:51 +0000

    断言
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/xml_parser_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/xml_parser_test.cpp
@@ -40,7 +40,7 @@ namespace OHOS {
 namespace Wifi {
 static std::string g_errLog;
 void XmlParserLogCallback(const LogType type, const LogLevel level,
-                          const unsigned int domain, const char *tag, 
+                          const unsigned int domain, const char *tag,
                           const char *msg)
 {
     g_errLog = msg;

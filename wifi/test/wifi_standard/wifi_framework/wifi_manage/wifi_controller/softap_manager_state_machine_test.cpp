e69fa7f9b6e5e29e95ae59504fb57075be914b27
chen yi wen <15068825070@163.com>
2025-01-21 06:54:30 +0000

    update softap_manager_state_machine_test.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_controller/softap_manager_state_machine_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_controller/softap_manager_state_machine_test.cpp
@@ -34,7 +34,8 @@ using ::testing::ext::TestSize;
 namespace OHOS {
 namespace Wifi {
         static std::string g_errLog;
-    void SoftapManagerMachineCallback(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
+    void SoftapManagerMachineCallback(const LogType type, const LogLevel level, 
+    const unsigned int domain, const char *tag, const char *msg)
     {
         g_errLog = msg;
     }

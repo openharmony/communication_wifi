68ac0e214d7606c56d14be884de38ebb3e587bb2
chen yi wen <15068825070@163.com>
2025-01-21 16:45:41 +0800

    wifi断言补充3.00
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/self_cure_state_machine_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/self_cure_state_machine_test.cpp
@@ -53,7 +53,9 @@ static const std::vector<std::string> TESTED_ADDR = {"192.168.0.101", "192.168.0
 constexpr int TEN = 10;
 
 static std::string g_errLog;
-void SelfCureStateMachineCallBack(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
+void SelfCureStateMachineCallBack(const LogType type, const LogLevel level,
+                                  const unsigned int domain,
+                                  const char *tag, const char *msg)
 {
     g_errLog = msg;
 }

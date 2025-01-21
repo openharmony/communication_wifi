cf1cc1f5de84c1950a1abc74d5ecf3bba9c89cc3
chen yi wen <15068825070@163.com>
2025-01-21 09:59:22 +0000

    111
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta_new/network_status_history_manager_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta_new/network_status_history_manager_test.cpp
@@ -35,7 +35,7 @@ class NetworkStatusHistoryManagerTest : public testing::Test {
 public:
     static void SetUpTestCase() {}
     static void TearDownTestCase() {}
-    virtual void SetUp() 
+    virtual void SetUp()
     {
         LOG_SetCallback(NetworkCallback);
     }

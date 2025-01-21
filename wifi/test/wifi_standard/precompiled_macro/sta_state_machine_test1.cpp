adb4f9c252a8b34dbdd821afc9fd0e37d422ab45
chen yi wen <15068825070@163.com>
2025-01-21 07:12:01 +0000

    sta_state_machine_test1.cpp
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/precompiled_macro/sta_state_machine_test1.cpp
+++ wifi/test/wifi_standard/precompiled_macro/sta_state_machine_test1.cpp
@@ -42,7 +42,8 @@ using ::testing::ext::TestSize;
 namespace OHOS {
 namespace Wifi {
 static std::string g_errLog;
-void StaMachLogCallback(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
+void StaMachLogCallback(const LogType type, const LogLevel level,
+ const unsigned int domain, const char *tag, const char *msg)
 {
     g_errLog = msg;
 }

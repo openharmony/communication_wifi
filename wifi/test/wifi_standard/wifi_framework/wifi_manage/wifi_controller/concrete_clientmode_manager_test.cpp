40f12b8e574366474124f8a16077a8414742e843
chen yi wen <15068825070@163.com>
2025-01-21 09:08:29 +0000

    断言
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_controller/concrete_clientmode_manager_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_controller/concrete_clientmode_manager_test.cpp
@@ -32,7 +32,7 @@ namespace OHOS {
 namespace Wifi {
 static std::string g_errLog;
 void ConcreteClientModeManagerCallback(const LogType type, const LogLevel level, 
-    const unsigned int domain, const char *tag, const char *msg)
+                                    const unsigned int domain, const char *tag, const char *msg)
 {
     g_errLog = msg;
 }

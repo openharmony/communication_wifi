86d5db37e1b9687c950d64d9692c9f8f463d013d
chen yi wen <15068825070@163.com>
2025-01-21 07:56:38 +0000

    格式修改
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/hdi_interface/wifi_hdi_wpa_p2p_impl_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/hdi_interface/wifi_hdi_wpa_p2p_impl_test.cpp
@@ -75,14 +75,14 @@ HWTEST_F(WifiHdiWpaP2pImplTest, HdiP2pSetWfdEnableTest, TestSize.Level1)
 {
     int enable = 1;
     HdiP2pSetWfdEnable(enable);
-    EXPECT_EQ(HdiP2pSetWfdEnable(enable),WIFI_HAL_OPT_OK);
+    EXPECT_EQ(HdiP2pSetWfdEnable(enable), WIFI_HAL_OPT_OK);
 }
 
 HWTEST_F(WifiHdiWpaP2pImplTest, HdiP2pSetPersistentReconnectTest, TestSize.Level1)
 {
     int status = 1;
     HdiP2pSetPersistentReconnect(status);
-    EXPECT_EQ(HdiP2pSetPersistentReconnect(status),WIFI_HAL_OPT_OK);
+    EXPECT_EQ(HdiP2pSetPersistentReconnect(status), WIFI_HAL_OPT_OK);
 }
 
 HWTEST_F(WifiHdiWpaP2pImplTest, HdiP2pSetWpsSecondaryDeviceTypeTest, TestSize.Level1)
@@ -289,7 +289,7 @@ HWTEST_F(WifiHdiWpaP2pImplTest, HdiP2pSetServDiscExternalTest, TestSize.Level1)
 {
     int mode = 1;
     HdiP2pSetServDiscExternal(mode);
-    EXPECT_EQ(HdiP2pSetServDiscExternal(mode),WIFI_HAL_OPT_OK);
+    EXPECT_EQ(HdiP2pSetServDiscExternal(mode), WIFI_HAL_OPT_OK);
 }
 
 HWTEST_F(WifiHdiWpaP2pImplTest, HdiP2pRemoveGroupTest, TestSize.Level1)

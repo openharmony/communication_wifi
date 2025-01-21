8126345510f071f11469b66ac7ea29561705ce12
chen yi wen <15068825070@163.com>
2025-01-21 06:38:28 +0000

    update wifi/test/wifi_standard/native/wifi_c_hotspot_test.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/native/wifi_c_hotspot_test.cpp
+++ wifi/test/wifi_standard/native/wifi_c_hotspot_test.cpp
@@ -28,7 +28,8 @@ using ::testing::ext::TestSize;
 namespace OHOS {
 namespace Wifi {
 static std::string g_errLog;
-void WificHostLogCallback(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
+void WificHostLogCallback(const LogType type, const LogLevel level, const unsigned int domain,
+const char *tag,const char *msg)
 {
         g_errLog = msg;
 }

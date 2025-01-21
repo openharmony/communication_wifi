389914d26edcedb7ccb4bca7fad6194627ddeba5
chen yi wen <15068825070@163.com>
2025-01-21 09:30:42 +0000

    断言
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_controller/rpt_manager_state_machine_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_controller/rpt_manager_state_machine_test.cpp
@@ -31,7 +31,7 @@ using ::testing::ext::TestSize;
 
 namespace OHOS::Wifi {
     static std::string g_errLog;
-    void RptManagerMachineCallback(const LogType type, const LogLevel level, 
+    void RptManagerMachineCallback(const LogType type, const LogLevel level,
                                    const unsigned int domain, const char *tag,const char *msg)
     {
         g_errLog = msg;

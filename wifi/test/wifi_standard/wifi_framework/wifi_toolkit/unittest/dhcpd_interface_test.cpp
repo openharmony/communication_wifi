b88e842ac9ea975f9225c95d89ab0daad73d17c9
chen yi wen <15068825070@163.com>
2025-01-21 06:41:26 +0000

    update wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/dhcpd_interface_test.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/dhcpd_interface_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/dhcpd_interface_test.cpp
@@ -28,7 +28,8 @@ using ::testing::ext::TestSize;
 namespace OHOS {
 namespace Wifi {
 static std::string g_errLog;
-void DhcpdInterfaceLogCallback(const LogType type,const LogLevel level,const unsigned int domain ,const char *tag,const char *msg)
+void DhcpdInterfaceLogCallback(const LogType type, const LogLevel level, const unsigned int domain,
+const char *tag, const char *msg)
 {
     g_errLog = msg;
 }

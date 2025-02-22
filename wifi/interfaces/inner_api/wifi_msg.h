ce44617b0c78f67e190106601c4d936a976256a9
yinyongbin <yinyongbin@huawei.com>
2025-02-22 11:17:06 +0000

    update wifi/interfaces/inner_api/wifi_msg.h.
    Signed-off-by: yinyongbin <yinyongbin@huawei.com>

--- wifi/interfaces/inner_api/wifi_msg.h
+++ wifi/interfaces/inner_api/wifi_msg.h
@@ -1041,7 +1041,7 @@ struct WifiSignalPollInfo {
 
     WifiSignalPollInfo() : signal(0), txrate(0), rxrate(0), noise(0), frequency(0),
         txPackets(0), rxPackets(0), snr(0), chload(0), ulDelay(0), txBytes(0), rxBytes(0),
-        txFailed(0), chloadSelf(0), c0Rssi(0), c1Rssi(0), ext(), extLen(0), timestamp(0)
+        txFailed(0), chloadSelf(0), c0Rssi(0), c1Rssi(0), ext(), extLen(0), timeStamp(0)
     {}
 
     ~WifiSignalPollInfo()

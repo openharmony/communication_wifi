ea6d920a43efb8bee352d4ed9f459b6b3bfdba74
yinyongbin <yinyongbin@huawei.com>
2025-02-24 07:32:34 +0000

    wifi_device_service_impl.h.
    Signed-off-by: yinyongbin <yinyongbin@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_service_impl.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_service_impl.h
@@ -110,6 +110,7 @@ public:
     ErrCode GetLinkedInfo(WifiLinkedInfo &info) override;
 
     ErrCode GetSignalPollInfoArray(std::vector<WifiSignalPollInfo> &wifiSignalPollInfos, int length) override;
+
     ErrCode GetMultiLinkedInfo(std::vector<WifiLinkedInfo> &mloLinkInfo) override;
 
     ErrCode GetDisconnectedReason(DisconnectedReason &reason) override;

df1c036e0d54749de194b871c05a1116918aa667
yangpengfei <yangpengfei34@huawei.com>
2025-02-24 11:21:04 +0000

    update
    Signed-off-by: yangpengfei <yangpengfei34@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/network_status_history/wifi_history_record_manager.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/network_status_history/wifi_history_record_manager.cpp
@@ -446,7 +446,7 @@ int WifiHistoryRecordManager::QueryApInfoRecordByBssid(const std::string &bssid,
     std::vector<std::string> queryAllColumn;
     auto resultSet = wifiDataBaseUtils_->Query(predicates, queryAllColumn);
     if (resultSet == nullptr) {
-        WIFI_LOGI("%{public}s, query fail", __func__);
+        WIFI_LOGE("%{public}s, query fail", __func__);
         return QUERY_FAILED;
     }
     int32_t resultSetNum = resultSet->GoToFirstRow();
@@ -483,7 +483,7 @@ int WifiHistoryRecordManager::QueryAllApInfoRecord(std::vector<ConnectedApInfo> 
     std::vector<std::string> queryAllColumn;
     auto resultSet = wifiDataBaseUtils_->Query(predicates, queryAllColumn);
     if (resultSet == nullptr) {
-        WIFI_LOGI("%{public}s, all query fail", __func__);
+        WIFI_LOGE("%{public}s, all query fail", __func__);
         return QUERY_FAILED;
     }
     int32_t resultSetNum = resultSet->GoToFirstRow();

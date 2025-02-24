68f20bb6a52693360786d68262528219e7806df1
silenceyi <yiyajun@huawei.com>
2025-02-24 18:03:15 +0800

    selfcure 打点
    Signed-off-by: silenceyi <yiyajun@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_state_machine.cpp
@@ -1339,7 +1339,6 @@ void SelfCureStateMachine::InternetSelfCureState::SelfcureForMultiGateway(Intern
         return;
     }
     isUsedMultiGwSelfcure_ = true;
-    pSelfCureStateMachine_->UpdateSelfcureState(WIFI_CURE_RESET_LEVEL_MULTI_GATEWAY, true);
     std::string ipAddr = MultiGateway::GetInstance().GetGatewayIp();
     std::string macString = "";
     MultiGateway::GetInstance().GetNextGatewayMac(macString);
@@ -1350,13 +1349,21 @@ void SelfCureStateMachine::InternetSelfCureState::SelfcureForMultiGateway(Intern
         }
         return;
     }
-
     std::string ifaceName = WifiConfigCenter::GetInstance().GetStaIfaceName();
-    MultiGateway::GetInstance().SetStaticArp(ifaceName, ipAddr, macString);
+    if (MultiGateway::GetInstance().SetStaticArp(ifaceName, ipAddr, macString) != 0) {
+        WIFI_LOGE("SetStaticArp failed");
+        if (lastMultiGwSelfFailedType_ != -1) {
+            SelectSelfCureByFailedReason(lastMultiGwSelfFailedType_);
+        }
+        return;
+    }
+    WriteWifiSelfcureHisysevent(static_cast<int>(WifiSelfcureType::MULTI_GATEWAY_SELFCURE));
+    pSelfCureStateMachine_->UpdateSelfcureState(WIFI_CURE_RESET_LEVEL_MULTI_GATEWAY, true);
     if (!pSelfCureStateMachine_->IsHttpReachable()) {
         MultiGateway::GetInstance().DelStaticArp(ifaceName, ipAddr);
         pSelfCureStateMachine_->SendMessage(WIFI_CURE_CMD_MULTI_GATEWAY);
     } else {
+        WriteWifiSelfcureHisysevent(static_cast<int>(WifiSelfcureType::MULTI_GATEWAY_SELFCURE_SUCC));
         pSelfCureStateMachine_->SwitchState(pSelfCureStateMachine_->pConnectedMonitorState_);
     }
 }
@@ -1394,6 +1401,7 @@ void SelfCureStateMachine::InternetSelfCureState::SelfCureForRandMacReassoc(int 
         pSelfCureStateMachine_->HandleSelfCureException(SCE_WIFI_STATUS_FAIL);
         return;
     }
+    WriteWifiSelfcureHisysevent(static_cast<int>(WifiSelfcureType::RAND_MAC_REASSOC_SELFCURE));
     pSelfCureStateMachine_->SetSelfCureWifiTimeOut(SelfCureState::SCE_WIFI_REASSOC_STATE);
 }
 
@@ -1616,6 +1624,8 @@ void SelfCureStateMachine::InternetSelfCureState::HandleHttpReachableAfterSelfCu
         WriteWifiSelfcureHisysevent(static_cast<int>(WifiSelfcureType::REASSOC_SELFCURE_SUCC));
     } else if (currentCureLevel == WIFI_CURE_RESET_LEVEL_HIGH_RESET) {
         WriteWifiSelfcureHisysevent(static_cast<int>(WifiSelfcureType::RESET_SELFCURE_SUCC));
+    } else if (currentCureLevel == WIFI_CURE_RESET_LEVEL_RAND_MAC_REASSOC) {
+        WriteWifiSelfcureHisysevent(static_cast<int>(WifiSelfcureType::RAND_MAC_REASSOC_SELFCURE_SUCC));
     }
 }
 

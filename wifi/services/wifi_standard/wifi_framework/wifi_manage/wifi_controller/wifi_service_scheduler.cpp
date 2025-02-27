f630c6e1ce9026f256cb8d7ee188af015b4d602a
shihaojie <shihaojie11@huawei.com>
2025-02-27 06:48:59 +0000

    工厂模式修复
    Signed-off-by: shihaojie <shihaojie11@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_controller/wifi_service_scheduler.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_controller/wifi_service_scheduler.cpp
@@ -512,7 +512,7 @@ ErrCode WifiServiceScheduler::StartDependentService(int instId)
     }
 
 #ifdef FEATURE_WIFI_PRO_SUPPORT
-    if (StartWifiProService(instId) != WIFI_OPT_SUCCESS) {
+    if (StartWifiProService(instId) == WIFI_OPT_FAILED) {
         WIFI_LOGE("StartWifiProService failed!");
         return WIFI_OPT_FAILED;
     }
@@ -584,7 +584,7 @@ ErrCode WifiServiceScheduler::StartWifiProService(int instId)
 {
     if (WifiConfigCenter::GetInstance().GetSystemMode() == SystemMode::M_FACTORY_MODE) {
         WIFI_LOGI("factory mode, not start wifipro service");
-        return WIFI_OPT_FAILED;
+        return WIFI_OPT_NOT_SUPPORTED;
     }
     if (WifiServiceManager::GetInstance().CheckAndEnforceService(WIFI_SERVICE_WIFIPRO) < 0) {
         WIFI_LOGE("Load %{public}s service failed!", WIFI_SERVICE_WIFIPRO);

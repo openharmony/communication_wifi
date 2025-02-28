013c75ebac4ccbb96c44ffec5e6432fe897f526d
huyu <huyu31@huawei.com>
2025-02-28 10:49:41 +0800

    permission
    Signed-off-by: huyu <huyu31@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/wifi_permission_helper.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/wifi_permission_helper.cpp
@@ -121,7 +121,8 @@ int WifiPermissionHelper::VerifyWifiConnectionPermission(const int &pid, const i
 
 int WifiPermissionHelper::VerifyGetWifiDirectDevicePermission(const int &pid, const int &uid)
 {
-    if (VerifyPermission("ohos.permission.LOCATION", pid, uid, 0) == PERMISSION_DENIED) {
+    if (VerifyPermission("ohos.permission.GET_WIFI_LOCAL_MAC", pid, uid, 0) == PERMISSION_DENIED ||
+        VerifyPermission("ohos.permission.GET_WIFI_PEERS_MAC", pid, uid, 0) == PERMISSION_DENIED) {
         return PERMISSION_DENIED;
     }
     return PERMISSION_GRANTED;

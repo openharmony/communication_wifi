94e6ff540adb787a150d1d502ee9db29247d7d79
孙浚予 <sunjunyu1@huawei.com>
2025-03-01 11:53:50 +0000

    modify
    Signed-off-by: 孙浚予 <sunjunyu1@huawei.com>

--- wifi/frameworks/ani/wifi_ani_manager.cpp
+++ wifi/frameworks/ani/wifi_ani_manager.cpp
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <ani.h>
 #include <array>
 #include <iostream>
@@ -17,14 +32,14 @@ ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
     ani_env *env;
     if (ANI_OK != vm->GetEnv(ANI_VERSION_1, &env)) {
         std::cerr << "Unsupported ANI_VERSION_1" << std::endl;
-        return (ani_status)9;
+        return ANI_OUT_OF_REF;
     }
 
     static const char *NameSpaceName = "Lwifi_manager/wifiManager;";
     ani_namespace wifimanager {};
     if (ANI_OK != env->FindNamespace(NameSpaceName, &wifimanager)) {
         std::cerr << "Not found '" << NameSpaceName << "'" << std::endl;
-        return (ani_status)2;
+        return ANI_INVALID_ARGS;
     }
 
     std::array functions = {
@@ -33,7 +48,7 @@ ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
 
     if (ANI_OK != env->Namespace_BindNativeFunctions(wifimanager, functions.data(), functions.size())) {
         std::cerr << "Namespace_BindNativeFunctions not OK" << std::endl;
-        return (ani_status)2;
+        return ANI_INVALID_ARGS;
     }
     std::cout << "Start bind native methods to '" << NameSpaceName << "'" << std::endl;
 

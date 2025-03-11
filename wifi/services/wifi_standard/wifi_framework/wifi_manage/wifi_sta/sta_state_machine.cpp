29ec880a4766bab7fd8b415bd108b79745ba12bc
TeamCorePro <siort401@163.com>
2025-03-11 12:57:26 +0800

    bug fixed
    Signed-off-by: TeamCorePro <siort401@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
@@ -1416,7 +1416,8 @@ void StaStateMachine::GetIpState::GoInState()
     if (pStaStateMachine->NetSupplierInfo != nullptr) {
         pStaStateMachine->NetSupplierInfo->isAvailable_ = true;
         pStaStateMachine->NetSupplierInfo->isRoaming_ = pStaStateMachine->isRoam;
-        pStaStateMachine->NetSupplierInfo->ident_ = WifiConfigCenter::GetInstance().GetStaIfaceName(pStaStateMachine->m_instId);
+        pStaStateMachine->NetSupplierInfo->ident_ =
+            WifiConfigCenter::GetInstance().GetStaIfaceName(pStaStateMachine->m_instId);
         WIFI_LOGI("On connect update net supplier info\n");
         WifiNetAgent::GetInstance().OnStaMachineUpdateNetSupplierInfo(pStaStateMachine->NetSupplierInfo,
             pStaStateMachine->m_instId);

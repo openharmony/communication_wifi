e28b26ea9ace63909c06b59c67d5bd65c9fe4250
孙浚予 <sunjunyu1@huawei.com>
2025-03-13 13:53:37 +0000

    final modify?
    Signed-off-by: 孙浚予 <sunjunyu1@huawei.com>

--- wifi/frameworks/ets/ani/wifi_manager/src/wifi_manager_ani.cpp
+++ wifi/frameworks/ets/ani/wifi_manager/src/wifi_manager_ani.cpp
@@ -12,18 +12,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <ani.h>
 #include <array>
 #include <iostream>
 #include "wifi_manager_ani.h"
 #include "wifi_logger.h"
 #include "wifi_device.h"
- 
+
 using namespace OHOS::Wifi;
- 
+
 std::shared_ptr<WifiDevice> g_wifiDevicePtr = WifiDevice::GetInstance(WIFI_DEVICE_ABILITY_ID);
- 
+
 ani_boolean IsWifiActive([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_object object)
 {
     bool activeStatus = false;
@@ -33,7 +33,7 @@ ani_boolean IsWifiActive([[maybe_unused]] ani_env *env, [[maybe_unused]] ani_obj
     }
     return static_cast<ani_boolean>(activeStatus);
 }
- 
+
 ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
 {
     ani_env *env;
@@ -41,24 +41,24 @@ ANI_EXPORT ani_status ANI_Constructor(ani_vm *vm, uint32_t *result)
         std::cerr << "Unsupported ANI_VERSION_1" << std::endl;
         return ANI_OUT_OF_REF;
     }
- 
+
     static const char *NameSpaceName = "L@ohos/wifiManager/wifiManager;";
     ani_namespace wifimanager {};
     if (ANI_OK != env->FindNamespace(NameSpaceName, &wifimanager)) {
         std::cerr << "Not found '" << NameSpaceName << "'" << std::endl;
         return ANI_INVALID_ARGS;
     }
- 
+
     std::array functions = {
         ani_native_function {"isWifiActive", ":Z", reinterpret_cast<ani_boolean *>(IsWifiActive)},
     };
- 
+
     if (ANI_OK != env->Namespace_BindNativeFunctions(wifimanager, functions.data(), functions.size())) {
         std::cerr << "Namespace_BindNativeFunctions not OK" << std::endl;
         return ANI_INVALID_ARGS;
     }
     std::cout << "Start bind native methods to '" << NameSpaceName << "'" << std::endl;
- 
+
     *result = ANI_VERSION_1;
     return ANI_OK;
 }

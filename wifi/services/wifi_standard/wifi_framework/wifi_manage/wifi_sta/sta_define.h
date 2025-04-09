73dc66b82b546352c3e4eb4e0467098003c11b23
pandou1 <pandou1@huawei.com>
2025-04-09 03:59:02 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_define.h.
    Signed-off-by: pandou1 <pandou1@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_define.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_define.h
@@ -89,7 +89,6 @@ namespace Wifi {
 #define VALUE_LIMIT_MAX_RSSI (-55)
 #define AUDIO_ON (1)
 #define AUDIO_OFF (0)
-#define SIGNAL_LEVEL_2 (2)
 
 enum SelectedType {
     NETWORK_SELECTED_BY_UNKNOWN = -1,

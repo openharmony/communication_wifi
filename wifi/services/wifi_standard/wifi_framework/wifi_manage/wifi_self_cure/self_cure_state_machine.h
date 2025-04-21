dcc24cf14990e69d7208078d4166dc003ce07233
silenceyi <yiyajun@huawei.com>
2025-04-21 15:59:34 +0800

    关闭WiFi自愈未正常退出
    Signed-off-by: silenceyi <yiyajun@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_state_machine.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_state_machine.h
@@ -393,6 +393,7 @@ private:
     void SetSelfCureWifiTimeOut(SelfCureState wifiSelfCureState);
     void ResetSelfCureParam();
     void NotifySelfCureCompleted(int status);
+    void ForceStopSelfCure();
 
 private:
     SelfCureSmHandleFuncMap selfCureSmHandleFuncMap_;

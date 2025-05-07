e7e88e86896f76cead29587b1d9b048eb6b9eae3
chen yi wen <15068825070@163.com>
2025-05-07 12:57:07 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_chr_utils.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_chr_utils.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_chr_utils.cpp
@@ -80,7 +80,7 @@ void WifiChrUtils::BeaconLostReport(const std::string &bssid, const int32_t sign
         int32_t errorCode = (signalLevel <= SIGNAL_LEVEL_TWO) ?
             BeaconLostType::SIGNAL_LEVEL_LOW : BeaconLostType::SIGNAL_LEVEL_HIGH;
         int64_t currentTime = GetCurrentTimeSeconds();
-        if (currentTime - startTime_ > SIGNAL_RECORD_5S) {
+        if (currentTime - startTime_ >= SIGNAL_RECORD_5S) {
             startTime_ = currentTime;
             WriteWifiBeaconLostHiSysEvent(errorCode);
         }

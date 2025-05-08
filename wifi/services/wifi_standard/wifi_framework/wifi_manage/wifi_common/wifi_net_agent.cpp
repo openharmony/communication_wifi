4996adcce859d9dce2e8d43146e6b334370b9e64
孙浚予 <sunjunyu1@huawei.com>
2025-05-08 09:57:42 +0000

    name fix
    Signed-off-by: 孙浚予 <sunjunyu1@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/wifi_net_agent.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/wifi_net_agent.cpp
@@ -177,7 +177,7 @@ void WifiNetAgent::UpdateNetLinkInfo(IpInfo &wifiIpInfo, IpV6Info &wifiIpV6Info,
     TimeStats timeStats(__func__);
     WIFI_LOGI("Enter UpdateNetLinkInfo.");
     std::unique_lock<std::mutex> lock(netAgentMutex_);
-    if (!isWifiAvaliable_[instId]) {
+    if (!isWifiAvailable_[instId]) {
         WIFI_LOGE("wifi is not avaliable, no need UpdateNetLinkInfo");
         return;
     }

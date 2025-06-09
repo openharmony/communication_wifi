a431ac1a23b423bcb2a932fd66df5512c07790ed
chen yi wen <15068825070@163.com>
2025-06-09 12:09:15 +0000

    update wifi/test/wifi_utils/unittest/wifi_common_util_test.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_utils/unittest/wifi_common_util_test.cpp
+++ wifi/test/wifi_utils/unittest/wifi_common_util_test.cpp
@@ -227,14 +227,14 @@ HWTEST_F(WifiCommonUtilTest, GetSplitInfoTest, TestSize.Level1)
     EXPECT_EQ(result.size(), 5);
 }
 
-HWTEST_F(WifiCommonUtilTest, isBeaconLostTest, TestSize.Level1)
+HWTEST_F(WifiCommonUtilTest, IsBeaconLostTest, TestSize.Level1)
 {
-    WIFI_LOGI("isBeaconLostTest enter");
+    WIFI_LOGI("IsBeaconLostTest enter");
     std::vector<std::string> bssidArray = {};
     std::vector<WifiSignalPollInfo> wifiBeaconCheckInfoArray ={};
-    bool result = isBeaconLost(bssidArray, wifiBeaconCheckInfoArray);
+    bool result = IsBeaconLost(bssidArray, wifiBeaconCheckInfoArray);
     EXPECT_FALSE(result);
-    WIFI_LOGI("isBeaconLostTest enter");
+    WIFI_LOGI("IsBeaconLostTest enter");
     bssidArray = {
         "00::55::DD::ff::MM", "00::55::DD::ff::MM", "00::55::DD::ff::MM",
         "00::55::DD::ff::MM", "00::55::DD::ff::MM", "00::55::DD::ff::MM"
@@ -258,7 +258,7 @@ HWTEST_F(WifiCommonUtilTest, isBeaconLostTest, TestSize.Level1)
     signalPoll4.ext = {128, 128, 128, 128, 128, 128, 128, 128, 128, 128};
     signalPoll5.ext = {128, 128, 128, 128, 128, 128, 128, 128, 128, 128};
     wifiBeaconCheckInfoArray ={signalPoll0, signalPoll1, signalPoll2, signalPoll3, signalPoll4, signalPoll5};
-    result = isBeaconLost(bssidArray, wifiBeaconCheckInfoArray);
+    result = IsBeaconLost(bssidArray, wifiBeaconCheckInfoArray);
     EXPECT_TRUE(result);
 }
 

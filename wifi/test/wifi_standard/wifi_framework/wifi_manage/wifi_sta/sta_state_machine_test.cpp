27fcac2ae0eab48ed47be34f55ebaa133cfdc513
xionglei <xionglei3@huawei.com>
2025-06-11 03:26:26 +0000

    update wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine_test.cpp.
    Signed-off-by: xionglei <xionglei3@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine_test.cpp
@@ -62,6 +62,7 @@ static constexpr int MAX_STR_LENT = 127;
 constexpr int CHIPSET_FEATURE_CAPABILITY_WIFI6_TEST = 127;
 constexpr int CHIPSET_FEATURE_CAPABILITY_WIFI7_TEST = 255;
 constexpr int TEN = 10;
+constexpr int MAX_NO_INTERNET_CNTS = 3;
 static const std::string TEMP_TEST_DATA = "1234567890abcdef1234567890abcdef";
 static std::string g_errLog;
     void StaStateMachineCallback(const LogType type, const LogLevel level,

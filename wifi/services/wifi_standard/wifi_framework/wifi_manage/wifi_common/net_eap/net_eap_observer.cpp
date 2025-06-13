17a33d56be8cdbbbaf71302931bf6ec41b314b1f
shihaojie <shihaojie11@huawei.com>
2025-06-13 06:46:36 +0000

    extended certification
    Signed-off-by: shihaojie <shihaojie11@huawei.com>

---
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/net_eap/net_eap_observer.cpp
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "net_eap_observer.h"
+#include <unistd.h>
+#include <pthread.h>
+#include <thread>
+#include "wifi_common_util.h"
+#include "wifi_logger.h"
+#include "wifi_errcode.h"
+ 
+DEFINE_WIFILOG_LABEL("WifiNetEapObserver");
+ 
+namespace OHOS {
+namespace Wifi {
+ 
+NetEapObserver::NetEapObserver()
+{
+    WIFI_LOGD("construct NetEapObserver");
+    netEapCallback_ = sptr<NetEapCallback>(new NetEapCallback());
+}
+ 
+NetEapObserver::~NetEapObserver()
+{
+    WIFI_LOGD("~NetEapObserver");
+}
+ 
+NetEapObserver &NetEapObserver::GetInstance()
+{
+    static NetEapObserver obj;
+    return obj;
+}
+ 
+bool NetEapObserver::SetRegisterCustomEapCallback(const std::function<void(const std::string &)> &callback)
+{
+    if (netEapCallback_ == nullptr) {
+        WIFI_LOGE("%{public}s, netEapCallback_ is nullptr", __func__);
+        return false;
+    }
+    return netEapCallback_->SetRegisterCustomEapCallback(callback);
+}
+ 
+bool NetEapObserver::SetReplyCustomEapDataCallback(const std::function<void(int, const std::string&)> &callback)
+{
+    if (netEapCallback_ == nullptr) {
+        WIFI_LOGE("%{public}s, netEapCallback_ is nullptr", __func__);
+        return false;
+    }
+    return netEapCallback_->SetReplyCustomEapDataCallback(callback);
+}
+ 
+void NetEapObserver::StartNetEapObserver()
+{
+    int32_t ret = NetManagerStandard::NetConnClient::GetInstance().RegisterCustomEapCallback(
+        NetManagerStandard::NetType::WLAN0, netEapCallback_);
+    if (ret == NetManagerStandard::NETMANAGER_SUCCESS) {
+        WIFI_LOGI("StartNetEapObserver register success");
+        return;
+    }
+    WIFI_LOGI("StartNetEapObserver failed, ret=%{public}d", ret);
+}
+ 
+void NetEapObserver::StopNetEapObserver()
+{
+    int32_t ret = NetManagerStandard::NetConnClient::GetInstance().UnRegisterCustomEapCallback(
+        NetManagerStandard::NetType::WLAN0, netEapCallback_);
+    if (ret == NetManagerStandard::NETMANAGER_SUCCESS) {
+        WIFI_LOGI("StopNetEapObserver unregister success");
+        return;
+    }
+    WIFI_LOGI("StopNetEapObserver failed, ret=%{public}d", ret);
+}
+ 
+void NetEapObserver::ReRegisterCustomEapCallback()
+{
+    WIFI_LOGI("%{public}s, enter", __func__);
+    if (netEapCallback_ == nullptr) {
+        WIFI_LOGE("%{public}s, netEapCallback_ is nullptr", __func__);
+        return;
+    }
+    auto callback = netEapCallback_->GetRegisterCustomEapCallback();
+    if (callback == nullptr) {
+        WIFI_LOGE("%{public}s, callback is nullptr", __func__);
+        return;
+    }
+    callback(netEapCallback_->regCmd_);
+}
+ 
+bool NetEapObserver::NotifyWpaEapInterceptInfo(const WpaEapData &wpaEapData)
+{
+    NetManagerStandard::EapData eapData;
+    eapData.eapCode = wpaEapData.code;
+    eapData.eapType = wpaEapData.type;
+    eapData.msgId = wpaEapData.msgId;
+    eapData.bufferLen = wpaEapData.bufferLen;
+    eapData.eapBuffer = std::move(wpaEapData.eapBuffer);
+    int32_t ret = NetManagerStandard::NetConnClient::GetInstance().NotifyWpaEapInterceptInfo(
+        NetManagerStandard::NetType::WLAN0, eapData);
+    if (ret != NetManagerStandard::NETMANAGER_SUCCESS) {
+        WIFI_LOGE("NotifyWpaEapInterceptInfo fail, ret:%{public}d", ret);
+        return false;
+    }
+    return true;
+}
+ 
+NetEapCallback::NetEapCallback()
+{
+}
+ 
+NetEapCallback::~NetEapCallback()
+{
+}
+ 
+bool NetEapCallback::SetRegisterCustomEapCallback(const std::function<void(const std::string &regCmd)> &callback)
+{
+    if (callback == nullptr) {
+        WIFI_LOGE("%{public}s, callback is nullptr", __func__);
+        return false;
+    }
+    regCallback_ = callback;
+    return true;
+ 
+}
+ 
+std::function<void(const std::string &)> NetEapCallback::GetRegisterCustomEapCallback()
+{
+    return regCallback_;
+}
+ 
+bool NetEapCallback::SetReplyCustomEapDataCallback(const std::function<void(int, const std::string&)> &callback)
+{
+    if (callback == nullptr) {
+        WIFI_LOGE("%{public}s, callback is nullptr", __func__);
+        return false;
+    }
+    replyCallback_ = callback;
+    return true;
+}
+ 
+int32_t NetEapCallback::OnRegisterCustomEapCallback(const std::string &regCmd)
+{
+    if (regCmd_ == regCmd) {
+        WIFI_LOGW("%{public}s regCmd is registered, %{public}s", __func__, regCmd.c_str());
+        return WIFI_OPT_SUCCESS;
+    }
+    regCmd_ = regCmd;
+    if (regCallback_ == nullptr) {
+        WIFI_LOGE("%{public}s regCallback_ is nullptr", __func__);
+        return WIFI_OPT_FAILED;
+    }
+    regCallback_(regCmd_);
+    return WIFI_OPT_SUCCESS;
+}
+ 
+int32_t NetEapCallback::OnReplyCustomEapDataEvent(int result, const NetManagerStandard::EapData &eapData)
+{
+    if (replyCallback_ == nullptr) {
+        WIFI_LOGE("%{public}s, replyCallback_ is nullptr", __func__);
+        return WIFI_OPT_FAILED;
+    }
+    std::string encodeEapData = EncodeBase64(eapData.eapBuffer);
+    if (encodeEapData.empty()) {
+        WIFI_LOGE("%{public}s, encodeEapData is empty", __func__);
+        return WIFI_OPT_FAILED;
+    }
+    std::string strEapData = std::to_string(eapData.msgId) + std::string(":") + std::to_string(eapData.bufferLen)
+        + std::string(":") + encodeEapData;
+    replyCallback_(result, strEapData);
+    return WIFI_OPT_SUCCESS;
+}
+ 
+}
+}
---
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_common/net_eap/net_eap_observer.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#ifndef WIFI_NET_EAP_OBSERVER_H
+#define WIFI_NET_EAP_OBSERVER_H
+ 
+#include <memory>
+#include <string>
+#include <vector>
+#include "wifi_log.h"
+#include "wifi_msg.h"
+#include "iremote_broker.h"
+#include "net_conn_client.h"
+#include "net_eap_callback_stub.h"
+ 
+namespace OHOS {
+namespace Wifi {
+ 
+class NetEapCallback : public NetManagerStandard::NetRegisterEapCallbackStub {
+public:
+ 
+    /**
+     * @Description Construct of NetEapObserver
+     */
+    NetEapCallback();
+ 
+    /**
+     * @Description Destructor function
+     */
+    ~NetEapCallback();
+ 
+    /**
+     * @Description registers the EapCustomHandler callback function of the Wi-Fi state machine.
+     *
+     * @param callback - callback func
+     */
+    bool SetRegisterCustomEapCallback(const std::function<void(const std::string &)> &callback);
+ 
+    /**
+     * @Description registers the ReplyCustomEapData function of the Wi-Fi state machine.
+     *
+     * @param callback - callback func
+     */
+    bool SetReplyCustomEapDataCallback(const std::function<void(int, const std::string&)> &callback);
+ 
+public:
+    /**
+     * @Description Register Custom Eap Callback
+     *
+     * @param regCmd - register command. eg: 2:277:278
+     */
+    int32_t OnRegisterCustomEapCallback(const std::string &regCmd) override;
+ 
+    /**
+     * @Description callback function used to Reply Custom EapData Event
+     * @param result - Indicates the result of custom authentication
+     * @param strEapData - Indicates eap data
+     */
+    int32_t OnReplyCustomEapDataEvent(int result, const NetManagerStandard::EapData &eapData) override;
+ 
+    std::function<void(const std::string &)> GetRegisterCustomEapCallback();
+public:
+    std::function<void(const std::string &)> regCallback_ = nullptr;
+    std::function<void(int, const std::string&)> replyCallback_ = nullptr;
+    NetManagerStandard::EapData netEapData_ = {};
+    std::string regCmd_;
+};
+ 
+class NetEapObserver
+{
+public:    
+    static NetEapObserver &GetInstance();
+    NetEapObserver() = delete;
+    ~NetEapObserver();
+ 
+    void StartNetEapObserver();
+    void StopNetEapObserver();
+    bool SetRegisterCustomEapCallback(const std::function<void(const std::string &)> &callback);
+    bool SetReplyCustomEapDataCallback(const std::function<void(int, const std::string&)> &callback);
+    void ReRegisterCustomEapCallback();
+    bool NotifyWpaEapInterceptInfo(const WpaEapData &wpaEapData);
+    sptr<NetEapCallback> GetNetEapCallbackPtr() {
+        return netEapCallback_;
+    }
+ 
+public:
+    sptr<NetEapCallback> netEapCallback_;
+};
+ 
+}
+}
+#endif

9ca972f10eeb44e7e2495bb2ecd2cfa4fa950d4c
chenqiwei <chenqiwei6@huawei.com>
2025-06-17 11:17:55 +0000

    modify
    Signed-off-by: chenqiwei <chenqiwei6@huawei.com>

---
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta_ext/wifi_security_detect_test.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_WIFI_SECURITY_DETECT_TEST_H
+#define OHOS_WIFI_SECURITY_DETECT_TEST_H
+
+namespace OHOS {
+namespace Wifi {
+
+class WifiSecurityDetect : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp() override {}
+    void TearDown() override {}
+};
+
+} // namespace Wifi
+} // namespace OHOS
+#endif

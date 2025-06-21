84bcef79a752a823cf004a9031a7908570fd6e21
chenqiwei <chenqiwei6@huawei.com>
2025-06-21 09:00:31 +0000

    modify
    Signed-off-by: chenqiwei <chenqiwei6@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_security_detect/wifi_security_detect.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_security_detect/wifi_security_detect.cpp
@@ -41,36 +41,7 @@ const std::string SETTINGS_DATA_VALUE = "VALUE";
 const int MIN_5G_FREQUENCY = 5160;
 const int MAX_5G_FREQUENCY = 5865;
 const uint32_t securityGuardModelID = 3001000011;
-enum WireType {
-    WIRE_802_11A = 1,
-    WIRE_802_11B = 2,
-    WIRE_802_11G = 3,
-    WIRE_802_11N = 4,
-    WIRE_802_11AC = 5,
-    WIRE_802_11AX = 6,
-};
-enum Security {
-    SECURITY_TYPE_OPEN = 0,
-    SECURITY_TYPE_WEP = 1,
-    SECURITY_TYPE_PSK = 2,
-    SECURITY_TYPE_EAP = 3,
-    SECURITY_TYPE_SAE = 4,
-    SECURITY_TYPE_EAP_WPA3_ENTERPRISE_192_BIT = 5,
-    SECURITY_TYPE_OWE = 6,
-    SECURITY_TYPE_WAPI_PSK = 7,
-    SECURITY_TYPE_WAPI_CERT = 8,
-    SECURITY_TYPE_EAP_WPA3_ENTERPRISE = 9,
-    SECURITY_TYPE_OSEN = 10,
-    SECURITY_TYPE_PASSPOINT_R1_R2 = 11,
-    SECURITY_TYPE_PASSPOINT_R3 = 12,
-    SECURITY_TYPE_DPP = 13,
-    SECURITY_TYPE_UNKNOWN = -1,
-};
 const int NUM24 = 24;
-enum WifiNotification {
-    OPEN = 1,  /* wifi_notification is open */
-    CLOSE = 2, /* wifi_notification is close */
-};
 
 WifiSecurityDetect::WifiSecurityDetect()
 {

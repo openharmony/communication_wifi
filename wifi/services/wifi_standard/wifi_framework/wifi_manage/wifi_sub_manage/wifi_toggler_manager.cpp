23a48bfa53c9a431e8ed7b36a34b1bf84c821817
chen yi wen <15068825070@163.com>
2025-06-25 02:08:55 +0000

    log精简
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_toggler_manager.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_toggler_manager.cpp
@@ -275,10 +275,7 @@ void WifiTogglerManager::DealRptStartFailure(int id)
 void WifiTogglerManager::DealClientRemoved(int id)
 {
     if (pWifiControllerMachine) {
-        WIFI_LOGI("SendMessage msg is  CMD_CONCRETECLIENT_REMOVED");
         pWifiControllerMachine->SendMessage(CMD_CONCRETECLIENT_REMOVED, id);
-    } else {
-        WIFI_LOGE("pWifiControllerMachine is nullptr");
     }
 }
 

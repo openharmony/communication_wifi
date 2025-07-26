6ec4809dddb897b545116d578c88d6d83210b501
shihaojie <shihaojie11@huawei.com>
2025-07-26 08:25:35 +0000

    1
    Signed-off-by: shihaojie <shihaojie11@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_pro/wifi_pro_state_machine.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_pro/wifi_pro_state_machine.h
@@ -148,6 +148,7 @@ public:
         void RequestHttpDetect(bool forceHttpDetect);
         void ParseQoeInfoAndRequestDetect();
         void HandleWifiQoeSlow();
+        void HandleWifiEmlsrStateChanged(const InternalMessagePtr msg);
     };
 
     class WifiNoNetState : public State {

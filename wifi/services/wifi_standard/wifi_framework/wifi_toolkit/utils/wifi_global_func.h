e9b30904afd977d872e8ffc24a076550b3771c0c
shihaojie <shihaojie11@huawei.com>
2025-07-28 03:17:45 +0000

    1
    Signed-off-by: shihaojie <shihaojie11@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_global_func.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_global_func.h
@@ -71,7 +71,7 @@ bool IsAllowScanAnyTime(const ScanControlInfo &info);
  * @param isReport - true : need report; flase : not report
  * @return ConnState - convert output connection state
  */
-ConnState ConvertConnStateInternalExtral(OperateResState resState, bool &isReport)
+ConnState ConvertConnStateInternalExtral(OperateResState resState, bool &isReport);
 
 /**
  * @Description Internal transition from OperateResState struct to ConnState

a68f4ad33895353ba12da38bf08ad87808a918a1
chen yi wen <15068825070@163.com>
2025-07-29 09:17:52 +0000

    1
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_global_func.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/utils/wifi_global_func.cpp
@@ -651,8 +651,12 @@ bool ParseJsonKey(const cJSON *jsonValue, const std::string &key, std::string &v
     int nSize = cJSON_GetArraySize(jsonValue);
     for (int i = 0; i < nSize; ++i) {
         cJSON *item = cJSON_GetArrayItem(jsonValue, i);
-        if (item == nullptr || !cJSON_IsObject(item)) {
+        if (item == nullptr) {
             return false;
+        }
+        if (!cJSON_IsObject(item)) {
+            cJSON_Delete(item);
+            continue;
         }
         cJSON *keyItem = cJSON_GetObjectItem(item, key.c_str());
         if (keyItem == nullptr) {

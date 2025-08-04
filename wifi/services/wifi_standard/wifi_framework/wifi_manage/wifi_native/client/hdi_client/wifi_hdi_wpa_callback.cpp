e237d16a99ffc66fdafa752fe8967f9b63faf9d5
chen yi wen <15068825070@163.com>
2025-08-04 13:43:45 +0000

    /wifi_native/client/hdi_client/wifi_hdi_wpa_callback.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/wifi_hdi_wpa_callback.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_native/client/hdi_client/wifi_hdi_wpa_callback.cpp
@@ -385,8 +385,7 @@ int32_t OnEventAuthReject(struct IWpaCallback *self,
     if (cbk.onWpaSsidWrongKey && statusCode == Wifi80211StatusCode::WLAN_STATUS_CHALLENGE_FAIL &&
         authType == Wifi80211AuthType::WLAN_AUTH_SAE) {
         LOGI("OnEventAuthReject, wrong password");
-        std::string bssid1 = std::string(bssid);
-        cbk.onWpaSsidWrongKey(bssid1);
+        cbk.onWpaSsidWrongKey(std::string(bssid));
         OHOS::Wifi::WriteAuthFailHiSysEvent("WRONG_PSWD");
         return 0;
     }

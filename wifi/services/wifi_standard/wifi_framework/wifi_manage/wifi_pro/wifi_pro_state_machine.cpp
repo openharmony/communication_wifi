c0b60892060660246387a0afee00a913155b0192
shihaojie <shihaojie11@huawei.com>
2025-08-21 13:51:33 +0000

    1
    Signed-off-by: shihaojie <shihaojie11@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_pro/wifi_pro_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_pro/wifi_pro_state_machine.cpp
@@ -485,6 +485,14 @@ bool WifiProStateMachine::TryWifi2Wifi(const NetworkSelectionResult &networkSele
 ErrCode WifiProStateMachine::FullScan()
 {
     WIFI_LOGD("start Fullscan");
+    int32_t signalLevel = WifiProUtils::GetSignalLevel(instId_);
+#ifndef OHOS_ARCH_LITE
+    if (currentState_ == WifiProState::WIFI_HASNET && WifiConfigCenter::GetInstance().IsScreenLandscape() &&
+        signalLevel >= SIG_LEVEL_2 && InLandscapeSwitchLimitList()) {
+        WIFI_LOGI("FullScan ScreenLandscape and InLandscapeSwitchLimitList.");
+        return WIFI_OPT_SUCCESS;
+    }
+#endif
     IScanService *pScanService = WifiServiceManager::GetInstance().GetScanServiceInst(instId_);
     if (pScanService == nullptr) {
         WIFI_LOGI("TryStartScan, pService is nullptr.");

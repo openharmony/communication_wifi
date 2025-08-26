e85706f32c489f78dfabcbd3a1d5db58935d833b
chen yi wen <15068825070@163.com>
2025-08-26 12:30:55 +0000

    update wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_sta_manager_test.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_sta_manager_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_sta_manager_test.cpp
@@ -147,8 +147,7 @@ HWTEST_F(WifiStaManagerTest, DealSignalPollReportTest01, TestSize.Level1)
     std::string bssid = "11:22:33:44:55:66";
     int32_t signalLevel = 2;
     int32_t instId = 0;
-    bool isAudioOn = true;
-    wifiStaManager_->DealSignalPollReport(bssid, signalLevel, isAudioOn, instId);
+    wifiStaManager_->DealSignalPollReport(bssid, signalLevel, instId);
     EXPECT_NE(wifiStaManager_->unloadStaSaTimerId, TEN);
 }
 

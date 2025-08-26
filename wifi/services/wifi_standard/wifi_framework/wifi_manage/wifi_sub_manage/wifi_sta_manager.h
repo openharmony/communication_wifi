bc615cfd25c1a7bed9b94720d9d8bb7dc9cfb085
chen yi wen <15068825070@163.com>
2025-08-26 12:28:28 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_sta_manager.h.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_sta_manager.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sub_manage/wifi_sta_manager.h
@@ -58,8 +58,7 @@ private:
     void PublishWifiOperateStateHiSysEvent(OperateResState state);
     void NotifyScanForStaConnChanged(OperateResState state, int networkId, int instId = 0);
     void DealInternetAccessChanged(int internetAccessStatus, int instId);
-    void DealSignalPollReport(const std::string &bssid, const int32_t signalLevel, const bool isAudioOn,
-        const int32_t instId = 0);
+    void DealSignalPollReport(const std::string &bssid, const int32_t signalLevel, const int32_t instId = 0);
 #ifndef OHOS_ARCH_LITE
     void DealOffScreenAudioBeaconLost(void);
 #endif

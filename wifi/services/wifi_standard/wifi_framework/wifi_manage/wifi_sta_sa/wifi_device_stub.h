8744751744c0da8fe6a0a2d3035e03148a16903f
TianYH <tianyuhang6@huawei.com>
2025-09-01 05:20:06 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_stub.h.
    Signed-off-by: TianYH <tianyuhang6@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_stub.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta_sa/wifi_device_stub.h
@@ -110,7 +110,7 @@ private:
     void OnSetVoWifiDetectPeriod(uint32_t code, MessageParcel &data, MessageParcel &reply);
     void OnGetVoWifiDetectPeriod(uint32_t code, MessageParcel &data, MessageParcel &reply);
     void OnIsRandomMacDisabled(uint32_t code, MessageParcel &data, MessageParcel &reply);
-    void OnSetRandomMacDisabled(uint32_t code, MessageParcel &data, MessageParcel &reply);   
+    void OnSetRandomMacDisabled(uint32_t code, MessageParcel &data, MessageParcel &reply);
 
 private:
     void ReadEapConfig(MessageParcel &data, WifiEapConfig &wifiEapConfig);

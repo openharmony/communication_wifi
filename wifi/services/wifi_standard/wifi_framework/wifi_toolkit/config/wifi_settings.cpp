20c8dffa5aa657026169968036a1d153493a9d63
chen yi wen <15068825070@163.com>
2025-09-04 11:03:29 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp.
    Signed-off-by: chen yi wen <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
@@ -815,7 +815,7 @@ void WifiSettings::SetKeyMgmtBitset(WifiDeviceConfig &config)
     }
     if (config.keyMgmt == KEY_MGMT_SAE) {
         index = FindKeyMgmtPosition(KEY_MGMT_WPA_PSK);
-        config.keyMgmtBitset |= (1 << index);
+        config.keyMgmtBitset |= (1 << uindex);
     }
 }
 

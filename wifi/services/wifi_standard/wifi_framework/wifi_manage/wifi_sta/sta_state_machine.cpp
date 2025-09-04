c608818263b78d05f74b0eda5578aee83c4e39f5
TianYH <tianyuhang6@huawei.com>
2025-09-04 11:40:19 +0000

    update wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp.
    Signed-off-by: TianYH <tianyuhang6@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.cpp
@@ -646,7 +646,8 @@ bool StaStateMachine::InitState::NotAllowConnectToNetwork(int networkId, const s
         return true;
     }
 
-    if (networkId == pStaStateMachine->linkedInfo.networkId && connTriggerMode != NETWORK_SELECTED_BY_SELFCURE) {
+    if (networkId == pStaStateMachine->linkedInfo.networkId && (connTriggerMode != NETWORK_SELECTED_BY_SELFCURE ||
+        connTriggerMode != NETWORK_SELECTED_BY_MDM) {
         WIFI_LOGI("This network is connected and does not need to be reconnected m_instId = %{public}d",
             pStaStateMachine->m_instId);
         return true;

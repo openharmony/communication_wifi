8e79048d303634bff8307aeb79046c532524666c
yunfengyu <huyu31@huawei.com>
2025-09-17 12:39:50 +0800

    filter dfs
    Signed-off-by: yunfengyu <huyu31@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p_sa/wifi_p2p_service_impl.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p_sa/wifi_p2p_service_impl.cpp
@@ -1350,7 +1350,7 @@ ErrCode WifiP2pServiceImpl::Hid2dGetChannelListFor5G(std::vector<int>& vecChanne
         WifiSettings::GetInstance().SetDefaultFrequenciesByCountryBand(BandType::BAND_5GHZ, tempFrequenciesList);
         TransformFrequencyIntoChannel(tempFrequenciesList, vecChannelList);
     }
-    WifiChannelHelper::GetInstance().FilterDfsChannel(vecChannelList);
+    WifiChannelHelper::GetInstance().FilterDfsChannel(vecChannelList, true);
     std::string strChannel;
     for (auto channel : vecChannelList) {
         strChannel += std::to_string(channel) + ",";

1594de0e77b24c716e1062ed16825bcf75fa8949
temptest_01 <15068825070@163.com>
2025-10-17 09:01:35 +0800

    update: 更新文件 sta_monitor.cpp
    Signed-off-by: temptest_01 <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_monitor.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_monitor.cpp
@@ -34,8 +34,8 @@ if the reject is caused by driver fail, we need set a delay time to reconnect to
 between this connection and other vap associations (like scan or p2p_enhance)
 */
 const int32_t CONNECT_REJECT_DELAY_TIME_MS = 2000;
-const int TWO = 2;
-const int THREE = 3;
+const int32_t TWO = 2;
+const int32_t THREE = 3;
 StaMonitor::StaMonitor(int instId) : pStaStateMachine(nullptr), m_instId(instId)
 {
     WIFI_LOGI("StaMonitor constuctor insId %{public}d", instId);

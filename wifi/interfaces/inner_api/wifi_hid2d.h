187a446204e656879223df3c6594192f2f9e4333
gcw_wlxBidWl <liusijie15@huawei.com>
2025-11-13 15:24:38 +0800

    update: 更新文件 wifi_hid2d.h fix 11ax
    Signed-off-by: gcw_wlxBidWl <liusijie15@huawei.com>

--- wifi/interfaces/inner_api/wifi_hid2d.h
+++ wifi/interfaces/inner_api/wifi_hid2d.h
@@ -150,6 +150,15 @@ public:
      */
     virtual ErrCode Hid2dIsWideBandwidthSupported(bool &isSupport) = 0;
 };
+
+    /**
+     * @Description Set p2p group type
+     *
+     * @param groupType - set group keepalive or not
+     * @return ErroCode - operation result
+     */
+    virtual ErrCode Hid2dSetGroupType(GroupLiveType groupType) = 0;
+
 }  // namespace Wifi
 }  // namespace OHOS
 #endif

2d76706b227cbe098c2a2f07675415cb87fe0c18
gcw_wlxBidWl <liusijie15@huawei.com>
2025-11-13 15:46:53 +0800

    update: 更新文件 p2p_define.h fix 11ax
    Signed-off-by: gcw_wlxBidWl <liusijie15@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/p2p_define.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/p2p_define.h
@@ -121,6 +121,7 @@ enum class P2P_STATE_MACHINE_CMD {
     CMD_SET_MIRACAST_SINK_CONFIG,
     CMD_NOTIFY_REMOTE_DIE,
     CMD_SET_P2P_HIGH_PERF,
+    CMD_HID2D_SET_GROUP_TYPE,
 
     /* monitor to state machine */
     WPA_CONNECTED_EVENT = 100,  // result of connect

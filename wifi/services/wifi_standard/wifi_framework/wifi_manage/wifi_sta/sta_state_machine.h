00b43ed821e2503e41955b76294620a031f8ecb5
temptest_01 <15068825070@163.com>
2025-11-19 17:07:40 +0800

    update: 更新文件 sta_state_machine.h
    Signed-off-by: temptest_01 <15068825070@163.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_sta/sta_state_machine.h
@@ -68,6 +68,7 @@ constexpr int CMD_AP_ROAMING_TIMEOUT_CHECK = 0X06;
 constexpr int CMD_LINK_SWITCH_DETECT_TIMEOUT = 0x07;
 constexpr int CMD_NO_INTERNET_TIMEOUT = 0x08;
 constexpr int CMD_IPV6_DELAY_TIMEOUT = 0x09;
+constexpr int CMD_SHOW_PORTAL_NOTIFICATION = 0x10;
 
 constexpr int STA_NETWORK_CONNECTTING_DELAY = 20 * 1000;
 constexpr int STA_SIGNAL_POLL_DELAY = 3 * 1000;
@@ -117,6 +118,8 @@ constexpr int RSSI_OFFSET_DEFAULT = 5;
 constexpr int RSSI_OFFSET_MAX = 10;
 
 constexpr unsigned int BIT_MLO_CONNECT = 0x80;
+
+constexpr int PORTAL_NOTIFICATION_TIMEOUT = 3 * 1000;
 
 #define DNS_IP_ADDR_LEN 15
 #define WIFI_FIRST_DNS_NAME "const.wifi.wifi_first_dns"

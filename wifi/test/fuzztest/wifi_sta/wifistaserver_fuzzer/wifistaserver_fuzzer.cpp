11a807b802a8d9351e38ee44acec65cf48340103
tradeWindToNorth <wangxu262@h-partners.com>
2025-11-21 15:17:02 +0800

    update: 更新文件 wifistaserver_fuzzer.cpp
    Signed-off-by: tradeWindToNorth <wangxu262@h-partners.com>

--- wifi/test/fuzztest/wifi_sta/wifistaserver_fuzzer/wifistaserver_fuzzer.cpp
+++ wifi/test/fuzztest/wifi_sta/wifistaserver_fuzzer/wifistaserver_fuzzer.cpp
@@ -648,7 +648,7 @@ extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
 
 /* Fuzzer entry point */
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
-{  
+{
     if ((data == nullptr) || (size <= OHOS::Wifi::U32_AT_SIZE_ZERO)) {
         return 0;
     }

2e90dd2c4adf6ac132b5aae1576d35e040058406
dongtingchi <dongtingchi@huawei.com>
2025-11-28 14:37:34 +0800

    更新注释
    Signed-off-by: dongtingchi <dongtingchi@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_state_machine.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_self_cure/self_cure_state_machine.cpp
@@ -2801,8 +2801,9 @@ bool SelfCureStateMachine::CheckSelfCureConnectState()
 {
     WifiLinkedInfo info;
     WifiConfigCenter::GetInstance().GetLinkedInfo(info);
+    // only effective in scenarios where the connection is interrupted
     if (info.detailedState == DetailedState::DISCONNECTED &&
-        info.disconnTriggerMode == DisconnState::DISCONNECTED) { // disconnTriggerMode is for wifi2wifi
+        info.disconnTriggerMode == DisconnState::DISCONNECTED) {
         HandleSelfCureException(SCE_WIFI_STATUS_FAIL);
         if (connectNetworkRetryCnt_ >= CONNECT_NETWORK_RETRY) {
             return false; // while reconnect fail, should not ignore sta connect event callback

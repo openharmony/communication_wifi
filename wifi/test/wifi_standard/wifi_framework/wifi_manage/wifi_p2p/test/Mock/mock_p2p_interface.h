10e45d3847d79398339dc970ce9d2ea01241429b
bruin06 <bruin2014@163.com>
2025-12-01 14:26:08 +0800

    update: 更新文件 mock_p2p_interface.h
    Signed-off-by: bruin06 <bruin2014@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/Mock/mock_p2p_interface.h
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_p2p/test/Mock/mock_p2p_interface.h
@@ -67,7 +67,7 @@ public:
     virtual ErrCode DisableRandomMac(int setmode);
     virtual ErrCode CreateRptGroup(const WifiP2pConfig &config);
     virtual ErrCode GetRptStationsList(std::vector<StationInfo> &result);
-    virtual void NotifyWscDialogConfirmResult(bool isAccept);
+    virtual void NotifyWscDialogConfirmResult(bool isAccept, const std::string& inputPincode);
     virtual void NotifyWscDisplayConfirmResult();
     virtual ErrCode SetMiracastSinkConfig(const std::string& config);
     virtual ErrCode NotifyRemoteDie(int uid);

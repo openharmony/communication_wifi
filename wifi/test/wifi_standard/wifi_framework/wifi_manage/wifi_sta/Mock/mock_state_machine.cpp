b6b5f7d1a24b2cd58bc83d2bd4f6c45debf531e6
temptest_01 <15068825070@163.com>
2025-12-15 14:14:54 +0800

    静态检查
    Signed-off-by: temptest_01 <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/Mock/mock_state_machine.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/Mock/mock_state_machine.cpp
@@ -200,8 +200,8 @@ void StateMachine::MessageExecutedLater(int msgName, int param1,
                                         int param2, const std::any &messageObj, int64_t delayTimeMs)
 {
     (void)messageObj;
-    WIFI_LOGD("MessageExecutedLater, msgName is %{public}d, param1 is %{public}d, param2 is %{public}d, delayTimeMs is %{public}d.",
-        msgName, param1, param2, delayTimeMs);
+    WIFI_LOGD("MessageExecutedLater, msgName is %{public}d, param1 is %{publicd, param2 is %{public}d, "
+              "delayTimeMs is %{public}d.", msgName, param1, param2, delayTimeMs);
 }
 void StateMachine::SendMessageAtFrontOfQueue(int msgName, int param1)
 {

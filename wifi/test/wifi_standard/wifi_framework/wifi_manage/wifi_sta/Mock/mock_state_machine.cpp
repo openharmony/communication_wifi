70560214a2d678631a833a271f46358d857681f7
temptest_01 <15068825070@163.com>
2025-12-15 15:28:44 +0800

    1
    Signed-off-by: temptest_01 <15068825070@163.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/Mock/mock_state_machine.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/Mock/mock_state_machine.cpp
@@ -182,8 +182,8 @@ void StateMachine::MessageExecutedLater(int msgName, int param1, int64_t delayTi
 }
 void StateMachine::MessageExecutedLater(int msgName, int param1, int param2, int64_t delayTimeMs)
 {
-    WIFI_LOGD("MessageExecutedLater, msgName is %{public}d, param1 is %{publicd, param2 is %{public}d, "
-        "delayTimeMs is %{public}d.", msgName, param1, param2, delayTimeMs);
+    WIFI_LOGD("MessageExecutedLater, msgName is %{public}d, param1 is %{publicd, param2 is %{public}d,
+        delayTimeMs is %{public}d.", msgName, param1, param2, delayTimeMs);
 }
 void StateMachine::MessageExecutedLater(InternalMessagePtr msg, int64_t delayTimeMs)
 {
@@ -200,8 +200,8 @@ void StateMachine::MessageExecutedLater(int msgName, int param1,
                                         int param2, const std::any &messageObj, int64_t delayTimeMs)
 {
     (void)messageObj;
-    WIFI_LOGD("MessageExecutedLater, msgName is %{public}d, param1 is %{publicd, param2 is %{public}d, "
-              "delayTimeMs is %{public}d.", msgName, param1, param2, delayTimeMs);
+    WIFI_LOGD("MessageExecutedLater, msgName is %{public}d, param1 is %{publicd, param2 is %{public}d,
+        delayTimeMs is %{public}d.", msgName, param1, param2, delayTimeMs);
 }
 void StateMachine::SendMessageAtFrontOfQueue(int msgName, int param1)
 {

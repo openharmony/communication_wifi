d9dbe8d0b4bb887d58e32621bda7010fb93a1174
daweq <dingyanwen1@huawei.com>
2026-01-21 09:13:07 +0800

    扫描不到GBK隐藏热点
    Signed-off-by: daweq <dingyanwen1@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/wifi_scan/scan_service.cpp
@@ -355,7 +355,7 @@ ErrCode ScanService::ScanWithParam(const WifiScanParams &params, ScanType scanTy
         if (!params.ssid.empty()) {
             scanConfig.hiddenNetworkSsid.push_back(params.ssid);
             // for gbk hiddenNetworkSsID
-            AddSsidToHiddenNetworkList(params.ssid, hiddenNetworkSsid);
+            AddSsidToHiddenNetworkList(params.ssid, scanConfig.hiddenNetworkSsid);
         } else if (!GetHiddenNetworkSsidList(scanConfig.hiddenNetworkSsid)) {
             /*
             * Invoke the interface provided by the configuration center to obtain the

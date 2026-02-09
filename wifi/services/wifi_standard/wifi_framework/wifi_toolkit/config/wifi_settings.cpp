e81aa22cd3f9aab9fe2d1b687119b616cf34df70
tradeWindToNorth <wangxu262@h-partners.com>
2026-02-09 10:44:31 +0800

    1
    Signed-off-by: tradeWindToNorth <wangxu262@h-partners.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/wifi_settings.cpp
@@ -43,7 +43,7 @@
 namespace OHOS {
 namespace Wifi {
 constexpr int MAX_DECRYPT_DEVICE_CONFIG_NUM = 400;
-static const int MAX_FILE_SIZE = 1024 * 1024;
+static const int MAX_FILE_SIZE = 1024 * 1024 * 1024;
 static const int MIN_FILE_SIZE = 0;
 std::string g_defaultApSsid;
 #ifdef DTFUZZ_TEST

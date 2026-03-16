d0cecaf1428430ee3b604b80085318c120dcda94
tianyuhang99 <tianyuhang6@huawei.com>
2026-03-16 20:11:57 +0800

    update: 更新文件 oh_wifi.cpp
    Signed-off-by: tianyuhang99 <tianyuhang6@huawei.com>

--- wifi/frameworks/wifi_ndk/oh_wifi.cpp
+++ wifi/frameworks/wifi_ndk/oh_wifi.cpp
@@ -80,3 +80,53 @@ Wifi_ResultCode OH_Wifi_GetDeviceMacAddress(char *macAddr, unsigned int *macAddr
 
     return WIFI_SUCCESS;
 }
+
+Wifi_ResultCode OH_Wifi_GetLinkedInfo(OHWifiLinkedInfo *info)
+{
+    if (info == nullptr) {
+        return WIFI_INVALID_PARAM;
+    }
+
+    if (g_WifiDevicePtr == nullptr) {
+        return WIFI_OPERATION_FAILED1;
+    }
+    OHOS::Wifi::WifiLinkedInfo linkedInfo;
+    OHOS::Wifi::ErrCode ret = g_WifiDevicePtr->GetLinkedInfo(linkedInfo);
+    if (ret != OHOS::Wifi::WIFI_OPT_SUCCESS) {
+        return WifiErrCodeToResultCode(ret);
+    }
+    if (strncpy_s(info->ssid, sizeof(info->ssid), linkedInfo.ssid.c_str(), linkedInfo.ssid.length()) != 0) {
+        return WIFI_OPERATION_FAILED3;
+    }
+    if (strncpy_s(info->bssid, WIFI_MAC_LEN, linkedInfo.bssid.c_str(), linkedInfo.bssid.length()) != 0) {
+        return WIFI_OPERATION_FAILED4;
+    }
+    if (strncpy_s(info->macAddress, WIFI_MAC_LEN, linkedInfo.macAddress.c_str(), linkedInfo.macAddress.length()) != 0) {
+        return WIFI_OPERATION_FAILED5;
+    }
+    info->networkId = linkedInfo.networkId;
+    info->rssi = linkedInfo.rssi;
+    info->band = linkedInfo.band;
+    info->linkSpeed = linkedInfo.linkSpeed;
+    info->frequency = linkedInfo.frequency;
+    info->ifHiddenSSID = linkedInfo.ifHiddenSSID;
+    info->isDataRestricted = linkedInfo.isDataRestricted;
+    info->chload = linkedInfo.chload;
+    info->snr = linkedInfo.snr;
+    info->macType = linkedInfo.macType;
+    info->ipAddress = linkedInfo.ipAddress;
+    info->supplicantState = static_cast<int>(linkedInfo.supplicantState);
+    info->connState = static_cast<int>(linkedInfo.connState);
+    info->wifiStandard = linkedInfo.wifiStandard;
+    info->maxSupportedRxLinkSpeed = linkedInfo.maxSupportedRxLinkSpeed;
+    info->maxSupportedTxLinkSpeed = linkedInfo.maxSupportedTxLinkSpeed;
+    info->rxLinkSpeed = linkedInfo.rxLinkSpeed;
+    info->txLinkSpeed = linkedInfo.txLinkSpeed;
+    info->channelWidth = static_cast<int>(linkedInfo.channelWidth);
+    info->supportedWifiCategory = static_cast<int>(linkedInfo.supportedWifiCategory);
+    info->isHiLinkNetwork = linkedInfo.isHiLinkNetwork;
+    info->isHiLinkProNetwork = linkedInfo.isHiLinkProNetwork;
+    info->wifiLinkType = static_cast<int>(linkedInfo.wifiLinkType);
+    info->riskType = static_cast<int>(linkedInfo.riskType);
+    return WIFI_SUCCESS;
+}

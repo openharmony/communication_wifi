3a827492d4c3c16375fb95658b339ae4deb48610
lx9512 <352753852@qq.com>
2026-04-02 11:28:11 +0800

    api 26版本接口适配number类型的错误码
    Signed-off-by: lx9512 <352753852@qq.com>

--- wifi/frameworks/js/napi/src/wifi_napi_device.cpp
+++ wifi/frameworks/js/napi/src/wifi_napi_device.cpp
@@ -963,6 +963,7 @@ NO_SANITIZE("cfi") napi_value ConnectToCandidateConfig(napi_env env, napi_callba
         int networkId = -1;
         napi_get_value_int32(env, argv[0], &networkId);
         ret = wifiDevicePtr->ConnectToNetwork(networkId, true);
+        WIFI_NAPI_RETURN(env, ret == WIFI_OPT_SUCCESS, ret, SYSCAP_WIFI_STA);
     } else if (valueType == napi_object) {
         ConnectSettings connectSettings;
         GetJsObjToConnectSettings(env, argv[0], connectSettings);
@@ -974,11 +975,10 @@ NO_SANITIZE("cfi") napi_value ConnectToCandidateConfig(napi_env env, napi_callba
             WIFI_NAPI_ASSERT(env, false, WIFI_OPT_INVALID_PARAM, SYSCAP_WIFI_STA);
         }
         ret = wifiDevicePtr->ConnectToNetwork(connectSettings.networkId, true, connectSettings.userActionTimeout);
+        WIFI_NAPI_RETURN_NUM_CODE(env, ret == WIFI_OPT_SUCCESS, ret, SYSCAP_WIFI_STA);
     } else {
         WIFI_NAPI_ASSERT(env, false, WIFI_OPT_INVALID_PARAM, SYSCAP_WIFI_STA);
     }
-
-    WIFI_NAPI_RETURN(env, ret == WIFI_OPT_SUCCESS, ret, SYSCAP_WIFI_STA);
 }
 
 static void CandidateConnectCallbackFunc(void *data)

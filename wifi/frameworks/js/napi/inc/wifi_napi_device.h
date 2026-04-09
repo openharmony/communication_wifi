264b4d36e540e8aa3884dff39434ebaadebed3a3
bjtu_wujun_transportation <wujun156@huawei.com>
2026-04-09 15:40:34 +0800

    adapter to taihe
    Signed-off-by: bjtu_wujun_transportation <wujun156@huawei.com>

--- wifi/frameworks/js/napi/inc/wifi_napi_device.h
+++ wifi/frameworks/js/napi/inc/wifi_napi_device.h
@@ -103,12 +103,14 @@ public:
     WifiDeviceConfig *config;
     int networkId;
     bool isCandidate;
+    int userActionTimeout;
 
     DeviceConfigContext(napi_env env, napi_async_work work = nullptr, napi_deferred deferred = nullptr) :
         AsyncContext(env, work, deferred) {
             config = nullptr;
             networkId = -1;
             isCandidate = false;
+            userActionTimeout = DEFAULT_DIALOG_TIMEOUT;
         }
 
     DeviceConfigContext() = delete;

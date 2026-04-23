b89790f12b49c91957e436869936b6a10f3a12ac
tianyuhang99 <tianyuhang6@huawei.com>
2026-04-23 14:09:25 +0800

    update: 更新文件 self_cure_state_machine_test.cpp
    Signed-off-by: tianyuhang99 <tianyuhang6@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/self_cure_state_machine_test.cpp
+++ wifi/test/wifi_standard/wifi_framework/wifi_manage/wifi_sta/self_cure_state_machine_test.cpp
@@ -2675,8 +2675,8 @@ HWTEST_F(SelfCureStateMachineTest, SetSelfCureFailInfoTest_InvalidCnt, TestSize.
 HWTEST_F(SelfCureStateMachineTest, SetSelfCureFailInfoTest, TestSize.Level1)
 {
     WifiSelfCureHistoryInfo info;
-    std::vector<std::string> histories = {"3", "4", "5", "6", "7",
-        "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"};
+    std::vector<std::string> histories = {"5", "6", "7",
+        "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"};
     int cnt = SELFCURE_FAIL_LENGTH;
     EXPECT_EQ(SelfCureUtils::GetInstance().SetSelfCureFailInfo(info, histories, cnt), 0);
     EXPECT_EQ(info.staticIpSelfCureFailedCnt, 5);

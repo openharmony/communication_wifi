4284eb64620137ee3a721f37b44664c1e768cd74
foredward <chensong60@huawei.com>
2026-07-07 15:43:06 +0800

    fix select net all no internet
    Signed-off-by: foredward <chensong60@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_manage/network_select/network_selection_utils.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_manage/network_select/network_selection_utils.h
@@ -107,5 +107,13 @@ public:
  
     static bool IsSameFreqAsP2p(const NetworkCandidate &networkCandidate);
 };
+
+/**
+    * check if the network has all NO_INTERNET history.
+    *
+    * @param networkCandidate candidate network.
+    * @return return true networkStatusHistory has 10 records all NO_INTERNET
+    */
+static bool IsAllNoInternetHistory(const NetworkCandidate &networkCandidate);
 }
 #endif

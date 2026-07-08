7c277cb5e327c7aafab247bdd2cd7d81f969b101
huyu23 <huyu31@huawei.com>
2026-07-08 18:02:38 +0800

    update: 更新文件 hid2d_service_registry_test.h
    Signed-off-by: huyu23 <huyu31@huawei.com>

--- wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/hid2d_service_registry_test.h
+++ wifi/test/wifi_standard/wifi_framework/wifi_toolkit/unittest/hid2d_service_registry_test.h
@@ -1,1 +1,47 @@
-/** */
+/*
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#ifndef OHOS_WIFI_HID2D_SERVICE_REGISTRY_TEST_H
+#define OHOS_WIFI_HID2D_SERVICE_REGISTRY_TEST_H
+ 
+#include <gtest/gtest.h>
+#include "hid2d_service_registry.h"
+ 
+namespace OHOS {
+namespace Wifi {
+ 
+class Hid2dServiceRegistryTest : public testing::Test {
+public:
+    static void SetUpTestCase()
+    {
+    }
+ 
+    static void TearDownTestCase()
+    {
+    }
+ 
+    virtual void SetUp()
+    {
+    }
+ 
+    virtual void TearDown()
+    {
+    }
+};
+ 
+}  // namespace Wifi
+}  // namespace OHOS
+ 
+#endif

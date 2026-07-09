052eba9118325e7779cf847aae3b3dabd3b8daee
huyu23 <huyu31@huawei.com>
2026-07-09 14:06:59 +0800

    update: 更新文件 hid2d_service_registry.h
    Signed-off-by: huyu23 <huyu31@huawei.com>

--- wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/hid2d_service_registry.h
+++ wifi/services/wifi_standard/wifi_framework/wifi_toolkit/config/hid2d_service_registry.h
@@ -19,8 +19,9 @@
 #include <string>
 #include <vector>
 #include "wifi_scan_control_msg.h"
- 
+
 #define SOFT_BUS_SERVICE_UID 1024
+#define SOFT_BUS_SERVER_SA_ID 4700
 #define CAST_ENGINE_SERVICE_UID 5526
 #define MIRACAST_SERVICE_UID 5529
 #define MIRACAST_SERVICE_SA_ID 5527
@@ -35,10 +36,10 @@
 #define CAST_ENGINE_SA_ID 65546
 #define SHARE_SERVICE_ID 2902
 #define MOUSE_CROSS_SERVICE_ID 65569
- 
+
 namespace OHOS {
 namespace Wifi {
- 
+
 struct Hid2dServiceEntry {
     int uid;
     std::string serviceName;
@@ -46,14 +47,14 @@ struct Hid2dServiceEntry {
     int systemAbilityId;
     bool allowLpScan;
 };
- 
+
 const std::vector<Hid2dServiceEntry>& GetHid2dServiceRegistry();
 bool IsHid2dServiceUid(int uid);
 bool IsHid2dServiceSaId(int saId);
 const Hid2dServiceEntry* FindServiceByUid(int uid);
 const Hid2dServiceEntry* FindServiceBySaId(int saId);
 const std::vector<int>& GetAllHid2dServiceUids();
- 
+
 } // namespace Wifi
 } // namespace OHOS
 #endif

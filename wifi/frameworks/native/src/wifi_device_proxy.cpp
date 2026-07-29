c3f01dc2507ca7d8bccba92631c09fd2c6201c2c
tradeWindToNorth <wangxu262@h-partners.com>
2026-07-29 11:20:47 +0800

    update: 更新文件 wifi_device_proxy.cpp
    Signed-off-by: tradeWindToNorth <wangxu262@h-partners.com>

--- wifi/frameworks/native/src/wifi_device_proxy.cpp
+++ wifi/frameworks/native/src/wifi_device_proxy.cpp
@@ -679,7 +679,6 @@ void WifiDeviceProxy::BigDataReadEapConfig(WifiEapConfig &wifiEapConfig, std::ve
     int pwdIdx = g_bigDataRecvLen++;
     wifiEapConfig.password = HexToString(tokens[pwdIdx]);
     std::fill(tokens[pwdIdx].begin(), tokens[pwdIdx].end(), 0);
-    wifiEapConfig.password = HexToString(tokens[g_bigDataRecvLen++]);
     wifiEapConfig.caCertPath = HexToString(tokens[g_bigDataRecvLen++]);
     wifiEapConfig.caCertAlias = HexToString(tokens[g_bigDataRecvLen++]);
     wifiEapConfig.clientCert = HexToString(tokens[g_bigDataRecvLen++]);

5c65717a9bde4f23d0ebe7014b399ff35f3ac60d
huyu23 <huyu31@huawei.com>
2026-08-04 20:16:32 +0800

    update: 更新文件 wifi_napi_p2p.cpp
    Signed-off-by: huyu23 <huyu31@huawei.com>

--- wifi/frameworks/js/napi/src/wifi_napi_p2p.cpp
+++ wifi/frameworks/js/napi/src/wifi_napi_p2p.cpp
@@ -95,6 +95,7 @@ static ErrCode GroupInfosToJs(const napi_env& env, const WifiP2pGroupInfo& group
         return WIFI_OPT_FAILED;
     }
     SetValueUtf8String(env, "goIpAddress", groupInfo.GetGoIpAddress().c_str(), result);
+    SetValueUtf8String(env, "gcIpAddress", groupInfo.GetGcIpAddress().c_str(), result);
     return WIFI_OPT_SUCCESS;
 }
 
